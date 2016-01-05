b6f884dfbfc892bfd79cd9ad7eefe76ca716df43
Chris Grandin <cgrandin@shaw.ca>
2016-01-05 13:09:37 -0800

    Fixed axes on catch plot, added more to the executive summary.

--- doc/r/catches.r
+++ doc/r/catches.r
@@ -47,24 +47,22 @@ make.catches.plot <- function(catches,
                               leg.y.loc = 430  ## Y-based location to place the legend
                               ){
   ## Plot the catches in a stacked-barplot with legend
-  ##catches <- catches[,c("Year","US_foreign","US_JV","atSea_US_MS","atSea_US_CP","US_shore",
-  ##                      "CAN_forgn","CAN_JV","CAN_Shoreside","CAN_FreezeTrawl")]
+  years <- catches$Year
   catches <- catches[,c("CAN_forgn","CAN_JV","CAN_Shoreside","CAN_FreezeTrawl","US_foreign","US_JV","atSea_US_MS","atSea_US_CP","US_shore")]
   cols <- c(rgb(0,0.8,0), rgb(0,0.6,0), rgb(0.8,0,0), rgb(0.4,0,0), rgb(0,0.2,0),
             rgb(0,0.4,0), rgb(0,0,0.7), rgb(0,0,0.4), rgb(0,0,1))
   legOrder <- c(6,5,2,1,4,3,NA,NA,9,8,7)
   oldpar <- par()
   par(las=1,mar=c(4, 4, 6, 2) + 0.1,cex.axis=0.9)
-  tmp <- barplot(t(as.matrix(catches))/1000,beside=F,names=catches[,1],
-        col=cols,xlab="Year",ylab="",cex.lab=1,xaxs="i",mgp=c(2.2,1,0))
-  axis(1,at=tmp,label=F,line=-0.12)
+  tmp <- barplot(t(as.matrix(catches))/1000, beside=FALSE, names=catches[,1],
+                 col=cols,xlab="Year", ylab="", cex.lab=1, xaxt="n", mgp=c(2.2,1,0))
+  axis(1,at=tmp, labels=years, line=-0.12)
   grid(NA,NULL,lty=1,lwd = 1)
   mtext("Catch ('000 mt)",side=2,line=2.8,las=0,cex=1.3)
-  barplot(t(as.matrix(catches))/1000,beside=F,names=catches[,1],
-          col=cols,
-          ##args.legend=list(x=60.5,y=430,bg="white",horiz=F,xpd=NA,cex=0.83,ncol=3,col=cols[legOrder]),
-          xlab="Year",ylab="",cex.lab=1,xaxs="i",add=T,mgp=c(2.2,1,0))
-  legend(x=0,y=leg.y.loc,
+  barplot(t(as.matrix(catches))/1000,beside=FALSE,names=catches[,1],
+          col=cols, xlab="Year",ylab="",cex.lab=1,xaxt="n",add=TRUE,mgp=c(2.2,1,0))
+
+legend(x=0,y=leg.y.loc,
          c("Canadian Foreign","Canadian Joint-Venture","Canadian Shoreside","Canadian Freezer Trawl",
            "U.S. Foreign","U.S. Joint-Venture","U.S. MS","U.S. CP","U.S. Shore-based")[legOrder],
          bg="white",horiz=F,xpd=NA,cex=1,ncol=3,fill=cols[legOrder],border=cols[legOrder],bty="n")
