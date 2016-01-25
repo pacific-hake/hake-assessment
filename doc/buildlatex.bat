7e25a5cef6133300034c594785740469f6553179
Chris Grandin <cgrandin@shaw.ca>
2016-01-25 09:02:01 -0800

    Fix recruitment deviations calculations in executive summary to reflect changes in the way they were loaded.

--- doc/buildlatex.bat
+++
@@ -1,1 +0,0 @@
-(@latex -synctex=1 "hake-assessment.tex" && bibtex "hake-assessment" && latex "hake-assessment.tex" && latex "hake-assessment.tex" && dvips "hake-assessment.dvi" && ps2pdf "hake-assessment.ps") 1> latexOutput.log 2>&1
