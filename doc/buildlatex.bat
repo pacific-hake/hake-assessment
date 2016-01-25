7631ade96e134cd294dbdc981788cae68ae08444
allanhicks <fishmanHicks@gmail.com>
2016-01-24 16:01:41 -0800

    Minor changes to git and compiling files
    modified .gitignore and added a buildlatex that doesn't call all.R. Not
    sure what synctex is

---
+++ doc/buildlatex.bat
@@ -0,0 +1,1 @@
+(@latex -synctex=1 "hake-assessment.tex" && bibtex "hake-assessment" && latex "hake-assessment.tex" && latex "hake-assessment.tex" && dvips "hake-assessment.dvi" && ps2pdf "hake-assessment.ps") 1> latexOutput.log 2>&1
