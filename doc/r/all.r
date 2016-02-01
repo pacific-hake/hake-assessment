cf0ae8d71b4e31109c98086aa019498f25e38bed
Chris Grandin <cgrandin@shaw.ca>
2016-02-01 08:18:21 -0800

    Changed number of lines to survey header in partest so that it works with model run 11. Once you merge this commit into yours, you need to re-source all.r, answering Y to all the questions. This is a change of base case so all models need to be loaded and calculations all have to be done again. Make sure you have the model folders 00-12 in your models directory and no non-model files or directories. I am also partway working out the overview map code. It isn't working yet, but shouldn't affect the loading routines.

--- doc/r/all.r
+++ doc/r/all.r
@@ -32,6 +32,7 @@ install.packages.if.needed("date", "date", github=FALSE)
 install.packages.if.needed("r4ss", "r4ss/r4ss", github=TRUE)
 install.packages.if.needed("xtable", "xtable", github=FALSE)
 install.packages.if.needed("PBSmapping", "PBSmapping", github=FALSE)
+install.packages.if.needed("maps", "maps", github=FALSE)
 install.packages.if.needed("dplyr", "dplyr", github = FALSE)
 ## install.packages.if.needed("gtools", "gtools", github = FALSE)
 
@@ -41,6 +42,7 @@ require(date)
 require(r4ss)
 require(xtable)
 require(PBSmapping)
+require(maps)
 require(dplyr)
 ## require(gtools)
 
@@ -55,6 +57,7 @@ source("figures-age-comps.r")
 source("figures-selex.r")
 source("figures-stock-recruitment.r")
 source("figures-mle-mcmc.r")
+## source("figures-overview-map.r")
 
 source("tables-timeseries.r")
 source("tables-reference-points.r")
@@ -89,7 +92,7 @@ nuisance.posteriors.file <- "nuisanceposteriors.csv"
 
 ## Index of the base model as found in the directory.
 ## i.e. 00_Modelname is index 1, 01_Modelname is index 2, etc.
-base.model.ind <- 7
+base.model.ind <- 12
 ## Last year's base model. This is used for the parameter estimates table which compares
 ##  last year's to this year's parameter estimates.
 last.year.base.model.ind <- 1
--- doc/r/utilities.r
+++ doc/r/utilities.r
@@ -3,7 +3,8 @@ run.partest.model <- function(model,
                               rows.to.skip.to.comps.header = 21,
                               rows.to.skip.to.likelihood = 90,
 ##                              rows.to.skip.to.survey.header = 1246){ ## end.yr = 2015
-                              rows.to.skip.to.survey.header = 1261){  ## end.yr = 2016, These numbers change when a new year is added
+##                              rows.to.skip.to.survey.header = 1261){  ## end.yr = 2016, These numbers change when a new year is added
+                              rows.to.skip.to.survey.header = 1266){  ## end.yr = 2016, These numbers change when a new year is added
   ## To ensure integration with the knitr loading step, you must
   ## run this from the Rgui (after you've got a base model loaded) like this:
   ##
@@ -552,3 +553,4 @@ biomass_fraction_plots <- function(replist, selected=FALSE){
          legend=c("Ages 4+","Ages 5+"),bg='white')
   title(main=ifelse(selected, "Estimated fractions of selected biomass", "Estimated fractions of total biomass"))
 }
+
