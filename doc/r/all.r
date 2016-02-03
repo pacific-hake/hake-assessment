0b7bdb2dfa38587589e699003cb28b046e41b2e7
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2016-02-03 13:13:54 -0800

    Minor continuing edits to Exec Summary - about to fix error in two tables.

--- doc/r/all.r
+++ doc/r/all.r
@@ -457,8 +457,9 @@ next.bio.lower.tac.based <- fmt0(fore.tac.mcmc$slower[names(fore.tac.mcmc$slower
 next.bio.median.tac.based <- fmt0(fore.tac.mcmc$smed[names(fore.tac.mcmc$smed) %in% (end.yr + 1)] * 100, 1)
 next.bio.upper.tac.based <- fmt0(fore.tac.mcmc$supper[names(fore.tac.mcmc$supper) %in% (end.yr + 1)] * 100, 1)
 
-## Calculations for exec summary and assessment-section.rnw; number of mcmc samples, minimum
-##  median biomass, years when fishing intensity > 1
+## Calculations for exec summary and assessment-section.rnw:
+##  number of mcmc samples, minimum median biomass,
+##  years when fishing intensity > 1
 num.mcmc.samples <- dim(base.model$mcmc)[1]
 median.bio.min  <- fmt0(min(base.model$mcmccalcs$smed), 3)  # min median biomass
 median.bio.min.year <- names(which.min(base.model$mcmccalcs$smed)) # year of min
