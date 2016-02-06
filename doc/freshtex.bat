c703dd94d10f55f8bc097fcff1e3d84e59e74375
allanhicks <fishmanHicks@gmail.com>
2016-02-06 01:12:37 -0800

    added freshtex.bat, which deletes knitr-cache as well

---
+++ doc/freshtex.bat
@@ -0,0 +1,17 @@
+# Clean up the directory after a LaTeX build. Windows version
+del *.aux
+del *.bbl
+del *.blg
+del *.dvi
+del *.log
+del *.lof
+del *.lot
+del *.ps
+del *.toc
+del *.txt
+del *.out
+del *.synctex.gz
+del hake-assessment.tex
+del hake-assessment.pdf
+rmdir /S /Q knitr-cache
+hake-assessment.synctex.gz(busy)
