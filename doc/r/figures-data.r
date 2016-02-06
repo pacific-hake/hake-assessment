eb666dca516086bfc51f16ce2cf344ffc45dfd4a
allanhicks <fishmanHicks@gmail.com>
2016-02-06 02:58:08 -0800

    Added argument to plot only one data plot
    Make sure to update r4ss
    devtools::install_github("r4ss/r4ss")

--- doc/r/figures-data.r
+++ doc/r/figures-data.r
@@ -1,4 +1,4 @@
 make.data.overview.plot <- function(model){ ## model contains the output of SS_output
   ## Make a plot of the data used in the assessment
-  SSplotData(model)
+  SSplotData(model,both=F)
 }
