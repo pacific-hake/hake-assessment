08e3194dac9591eba4fcf0a9cb377895f09dea82
Ian Taylor <ian.taylor@noaa.gov>
2017-01-25 17:27:16 -0800

    changes related to maturity ogives table
    this should complete issue #175

--- doc/r/r-functions/tables-maturity.r
+++ doc/r/r-functions/tables-maturity.r
@@ -40,3 +40,40 @@ make.maturity.samples.table <- function(ovary.samples,
                size = size.string))
 }
 
+
+
+make.maturity.ogives.table <- function(maturity.ogives,
+                                       interval = 2, ## use display 2cm values (vs 1cm)
+                                       xcaption = "default", ## Caption to use
+                                       xlabel   = "default", ## Latex label to use
+                                       font.size = 10,       ## Size of the font for the table
+                                       space.size = 10       ## Size of the spaces for the table
+                                       ){
+  ## This function based on make.maturity.samples.table above, but created by
+  ## Ian who doesn't have much experience with xtable so may contain errors
+  ## or silly-looking code
+  ##
+  ## Returns an xtable
+
+  tab <- maturity.ogives
+  # subset to values evenly divisible by the 'interval' input (e.g. 2cm instead of 1cm)
+  tab <- tab[tab$length.cm %% interval == 0,]
+
+  ## The next line formats all non-year-column values with a thousands seperator
+  colnames(tab) <- c("\\textbf{Length (cm)}",
+                     # note: could be a ref but don't know how to do that in a table
+                     "\\specialcell{\\textbf{Dorn and }\\\\\\textbf{Saunders (1997)}}",
+                     "\\specialcell{\\textbf{2015}\\\\\\textbf{assessment}}")
+  ## Make the size string for font and space size
+  size.string <- paste0("\\fontsize{", font.size, "}{", space.size, "}\\selectfont")
+
+  return(print(xtable(tab, caption=xcaption,
+                      label = xlabel,
+                      align = get.align(ncol(tab), first.left = FALSE, just = "c"),
+                      digits = c(0, 0, 2, 2)),
+               caption.placement = "top",
+               include.rownames = FALSE,
+               table.placement = "H",
+               sanitize.text.function = function(x){x},
+               size = size.string))
+}
