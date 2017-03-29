c85a298ee77366ae0e0adc523d0dbcd595bd03b4
Chris Grandin <cgrandin@shaw.ca>
2017-03-29 11:23:20 -0700

    Sampling history table updated to include new latex methods.

--- doc/r/r-functions/tables-sampling.r
+++ doc/r/r-functions/tables-sampling.r
@@ -1,47 +1,98 @@
 make.sampling.history.table <- function(dat,
-                                        xcaption = "default", ## Caption to use
-                                        xlabel   = "default", ## Latex label to use
-                                        font.size = 9,        ## Size of the font for the table
-                                        space.size = 10){       ## Size of the spaces for the table
+                                        xcaption = "default",
+                                        xlabel   = "default",
+                                        font.size = 9,
+                                        space.size = 10){
+  ## Returns an xtable in the proper format for the sampling effort
   ## dat is a data frame containing the sampling history
+  ## xcaption - caption to appear in the calling document
+  ## xlabel - the label used to reference the table in latex
+  ## font.size - size of the font for the table
+  ## space.size - size of the vertical spaces for the table
 
   dat[is.na(dat)] <- "--"
 
-  ## Add extra header top part of which is spanning multiple columns
   addtorow <- list()
   addtorow$pos <- list()
   addtorow$pos[[1]] <- -1
   addtorow$pos[[2]] <- nrow(dat)
-  addtorow$command <- c(paste0("\\hline & \\multicolumn{6}{c|}{\\textbf{U.S.}} & \\multicolumn{4}{c|}{\\textbf{Canada}} \\\\ ",
-                               " \\hline \\multirow{-2}{*}{\\textbf{Year}}",
-                               " & \\specialcell{\\textbf{Foreign}\\\\\\textbf{(hauls)}}",
-                               " & \\specialcell{\\textbf{Joint-}\\\\\\textbf{Venture}\\\\\\textbf{(hauls)}}",
-                               " & \\specialcell{\\textbf{Mother-}\\\\\\textbf{ship}\\\\\\textbf{(hauls)}}",
-                               " & \\specialcell{\\textbf{Combined}\\\\\\textbf{Mother-}\\\\\\textbf{ship}\\\\\\textbf{Catcher-}\\\\\\textbf{processor}\\\\\\textbf{(hauls)}}",
-                               " & \\specialcell{\\textbf{Catcher-}\\\\\\textbf{processor}\\\\\\textbf{(hauls)}}",
-                               " & \\specialcell{\\textbf{Shore-}\\\\\\textbf{based}\\\\\\textbf{(trips)}}",
-                               " & \\textbf{Foreign}",
-                               " & \\specialcell{\\textbf{Joint-}\\\\\\textbf{Venture}\\\\\\textbf{(hauls)}}",
-                               " & \\specialcell{\\textbf{Shoreside}\\\\\\textbf{(trips)}}",
-                               " & \\specialcell{\\textbf{Freezer-}\\\\\\textbf{trawl}\\\\\\textbf{(hauls)}} \\\\ \\hline "),
-                        "\\hline ")
+  addtorow$command <-
+    c(paste0(latex.hline,
+             latex.amp(),
+             latex.mcol(6, "c|", latex.bold("U.S.")),
+             latex.amp(),
+             latex.mcol(4, "c|", latex.bold("Canada")),
+             latex.nline,
+             latex.hline,
+             latex.mrow(-2, "*", latex.bold("Year")),
+             latex.amp(),
+             latex.mlc(c("Foreign",
+                         "(hauls)")),
+             latex.amp(),
+             latex.mlc(c("Joint-",
+                         "Venture",
+                         "(hauls)")),
+             latex.amp(),
+             latex.mlc(c("Mother-",
+                         "ship",
+                         "(hauls)")),
+             latex.amp(),
+             latex.mlc(c("Combined",
+                         "Mother-",
+                         "ship",
+                         "Catcher-",
+                         "processor",
+                         "(hauls)")),
+             latex.amp(),
+             latex.mlc(c("Catcher-",
+                         "processor",
+                         "(hauls)")),
+             latex.amp(),
+             latex.mlc(c("Shore-",
+                         "based",
+                         "(trips)")),
+             latex.amp(),
+             latex.mlc(c("Foreign",
+                         "(hauls)")),
+             latex.amp(),
+             latex.mlc(c("Joint-",
+                         "Venture",
+                         "(hauls)")),
+             latex.amp(),
+             latex.mlc(c("Shoreside",
+                         "(trips)")),
+             latex.amp(),
+             latex.mlc(c("Freezer",
+                         "Trawlers",
+                         "(hauls)")),
+             latex.nline,
+             latex.hline),
+      latex.hline)
 
-  ## Make the size string for font and space size
-  size.string <- paste0("\\fontsize{",font.size,"}{",space.size,"}\\selectfont")
-  return(print(xtable(dat,
-                      caption = xcaption,
-                      label = xlabel,
-                      align = c("c", "|Y", "|Y", "Y", "Y", "Y", "Y", "Y|", "Y", "Y", "Y", "Y|")),
-                      # align = c("c", "|r", "|r", "r", "r", "r", "r", "r|", "r", "r", "r", "r|")),    # Andy trying unsuccessfully to right justify columns
-                      # align = get.align(ncol(dat))), # cannot use since need |
-               caption.placement = "top",
-               add.to.row = addtorow,
-               include.rownames = FALSE,
-               include.colnames = FALSE,
-               table.placement="H",
-               tabular.environment = "tabularx",
-               width = "\\textwidth",
-               sanitize.text.function=function(x){x},
-               size=size.string,
-               hline.after = NULL))
+  size.string <- latex.size.str(font.size, space.size)
+  print(xtable(dat,
+               caption = xcaption,
+               label = xlabel,
+               align = c("c",
+                         "|Y",
+                         "|Y",
+                         "Y",
+                         "Y",
+                         "Y",
+                         "Y",
+                         "Y|",
+                         "Y",
+                         "Y",
+                         "Y",
+                         "Y|")),
+        caption.placement = "top",
+        add.to.row = addtorow,
+        include.rownames = FALSE,
+        include.colnames = FALSE,
+        table.placement="H",
+        tabular.environment = "tabularx",
+        width = "\\textwidth",
+        sanitize.text.function=function(x){x},
+        size=size.string,
+        hline.after = NULL)
 }
