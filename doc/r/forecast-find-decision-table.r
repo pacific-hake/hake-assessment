ee2306fbb77c50deeafa9be729e7e8aa8ec0410a
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2018-02-06 14:25:30 -0800

    Extra instructions (to document what I'm doing), and updating some of the forecast values.

--- doc/r/forecast-find-decision-table.r
+++ doc/r/forecast-find-decision-table.r
@@ -2,25 +2,36 @@
 #  automatically run by other code. It's run once by someone
 #  to get the catch levels to put into forecast-catch-levels.r.
 # Aaron updated numbers 1/23/17
+# Andy updating numbers 2/6/18.
 
-SSdir <- "C:/Users/Aaron.Berger/Documents/GitHub/hake-assessment/models"
-
-baseModel <- "45_BasePreSRG_v4_mcmc_forecasts"
+# SSdir <- "C:/Users/Aaron.Berger/Documents/GitHub/hake-assessment/models"
+SSdir <- "../../models"
+baseModel <- "2018.40_base_model"
 
 ######################################
 # Forecasts
-# Figure out median recommended (default) catches for 2017-2019
+# Calculate the median recommended (default) catches for 2018-2020
+# Copy  baseModel/mcmc/derived_posteriors.sso, forecast.ss, starter.ss,
+#  2018hake_control.ss, 2018hake_data.ss,
+# into new DecisionTable/defaultHR/
 out <- read.table(file.path(SSdir,baseModel,
                             "DecisionTable/defaultHR/derived_posteriors.sso"),
                   header=T)
-# no catches input into forecast.ss file to determine median 2017 catch
-median(out$ForeCatch_2017)   #969840 1/23/17
-# now fix this catch for 2017 in the forecast.ss file, run mceval, and find
-#  median 2018 catch
+# no catches input into forecast.ss file to determine median 2018 catch (?)
+median(out$ForeCatch_2018)   # 725983.5  2/6/18
+# now fix this catch for 2018 near the end of the forecast.ss file:
+## _Yr Seas Fleet Catch(or_F)
+# 2018 1 1 725983.5
+
+# run mceval (ss3 -mceval), and find
+#  median 2019 catch
 out <- read.table(file.path(SSdir,baseModel,
                             "DecisionTable/defaultHR/derived_posteriors.sso"),
                   header=T)
-median(out$ForeCatch_2018)   #843566  1/23/17
+median(out$ForeCatch_2019)   #843566  1/23/17
+
+updated to here
+
 # now fix this catch for 2018 in the forecast.ss file, run mceval, and find
 #  median 2019 catch
 out <- read.table(file.path(SSdir,baseModel,
