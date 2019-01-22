484ce0584a0ace14aac7e1bf74ab3d47b96d47b2
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-01-22 14:12:54 -0800

    Update bridging model definitions for 2019 base model.

--- R/model-setup.R
+++ R/model-setup.R
@@ -150,10 +150,30 @@ if(verbose){
 ## Bridge models group 1
 ## -----------------------------------------------------------------------------
 bridge.model.dir.names.1 <- c(last.yr.base.model.dir.name,
-                              "2018.40.25_alternativeCatch")
+                              "2019.02.40_update_historic_catch",
+                              "2019.02.41_add_2018_catch",
+                              "2019.02.42_wtatage_2018",
+                              "2019.02.43_h_INIT_095")
 bridge.model.names.1 <- c(last.yr.base.model.name,
-                          "Alternative catch stream")
-bridge.model.end.yr.1 <- end.yr - 1 # all models end 1 year earlier
+                          "Corrected historic catch",
+                          "Add 2018 total catch",
+                          "Add 2018 weight-at-age",
+                          "Change initial steepness")
+bridge.model.end.yr.1 <- end.yr - c(1, 1, 0, 0, 0) # subtract 1 year from first two models
+
+
+## -----------------------------------------------------------------------------
+## Bridge models group 2
+## -----------------------------------------------------------------------------
+bridge.model.dir.names.2 <- c(last.yr.base.model.dir.name,
+                              "2019.02.43_h_INIT_095",
+                              "2019.02.44_proj_wtatage",
+                              "2019.02.45_fecundity_time_varying")
+bridge.model.names.2 <- c(last.yr.base.model.name,
+                          "Result of first bridging model set",
+                          "Use 2018 wtatage for projections",
+                          "Time-varying fecundity")
+bridge.model.end.yr.2 <- end.yr - c(1, 0, 0, 0) # subtract 1 year from first 2 models
 
 ## -----------------------------------------------------------------------------
 ## Sensitivity models group 1
