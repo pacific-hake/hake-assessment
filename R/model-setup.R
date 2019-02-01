3358e3fa03ca1a02028300272185b5fb988769d2
Chris Grandin <cgrandin@shaw.ca>
2019-02-01 11:53:24 -0800

    Change fecundity sensitivity descriptions to numbers to ensure plot legends are legible.

--- R/model-setup.R
+++ R/model-setup.R
@@ -234,13 +234,20 @@ sens.model.dir.names.5 <- c("2019.03.52_fecundity",
                             "2019.03.56_fecundity",
                             "2019.03.57_fecundity",
                             "2019.03.58_fecundity")
-sens.model.names.5 <- c("Early weight-age 1975-2018 mean, late is 2016-2018 mean",             #52
-                        "Early weight-age 1975-2018 mean, late is 1975-2018 mean",             #53
-                        "TV Fecund, early weight-age 1975-2018 mean, late is 2016-2018 mean",  #54
-                        "TV Fecund, early weight-age 1975-1979 mean, late is 1975-2018 mean*", #55
-                        "Early weight-age 1975-1979 mean, late is 2016-2018 mean",             #56
-                        "Early weight-age 1975-1979 mean, late is 1975-2018 mean*",            #57
-                        "TV Fecund, early weight-age 1975-1979 mean, late is 2016-2018 mean")  #58
+sens.model.names.5 <- c("52",
+                        "53",
+                        "54",
+                        "55",
+                        "56",
+                        "57",
+                        "58")
+## sens.model.names.5 <- c("Early weight-age 1975-2018 mean, late is 2016-2018 mean",             #52
+##                         "Early weight-age 1975-2018 mean, late is 1975-2018 mean",             #53
+##                         "TV Fecund, early weight-age 1975-2018 mean, late is 2016-2018 mean",  #54
+##                         "TV Fecund, early weight-age 1975-1979 mean, late is 1975-2018 mean*", #55
+##                         "Early weight-age 1975-1979 mean, late is 2016-2018 mean",             #56
+##                         "Early weight-age 1975-1979 mean, late is 1975-2018 mean*",            #57
+##                         "TV Fecund, early weight-age 1975-1979 mean, late is 2016-2018 mean")  #58
 
 ## This function must be called from within the first knitr code chunk
 ## in the document. It is defined here so that it is in the same place
