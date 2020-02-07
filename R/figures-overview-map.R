293a7cef1f99af4efc95fbae31a12e0b03376840
Chris Grandin <chris.grandin@gmail.com>
2020-02-07 10:04:30 -0800

    Forced to stage commits that weren't mine

--- R/figures-overview-map.R
+++ R/figures-overview-map.R
@@ -15,29 +15,15 @@ make.overview.map.plot <- function(path,
   
   ports <- read_csv(ports_file) %>% 
     mutate(lon = -abs(lon))
-  ports_left_north <- ports %>% 
-    filter(pos == "left",
-           north_south == "north")
-  ports_left_south <- ports %>% 
-    filter(pos == "left",
-           north_south == "south")
-  ports_right_north <- ports %>% 
-    filter(pos == "right",
-           north_south == "north")
-  ports_right_south <- ports %>% 
-    filter(pos == "right",
-           north_south == "south")
+  ports_left <- ports %>% 
+    filter(pos == "left")
+  ports_right <- ports %>% 
+    filter(pos == "right")
   
-  ports_left_north_sf <- ports_left_north %>% 
+  ports_left_sf <- ports_left %>% 
     st_as_sf(coords = c("lon", "lat"), crs = crs) %>% 
     `st_crs<-`(crs)
-  ports_left_south_sf <- ports_left_south %>% 
-    st_as_sf(coords = c("lon", "lat"), crs = crs) %>% 
-    `st_crs<-`(crs)
-  ports_right_north_sf <- ports_right_north %>% 
-    st_as_sf(coords = c("lon", "lat"), crs = crs) %>% 
-    `st_crs<-`(crs)
-  ports_right_south_sf <- ports_right_south %>% 
+  ports_right_sf <- ports_right %>% 
     st_as_sf(coords = c("lon", "lat"), crs = crs) %>% 
     `st_crs<-`(crs)
   
@@ -55,10 +41,8 @@ make.overview.map.plot <- function(path,
   
   g <- ggplot(data = coast) +
     geom_sf(color = "black", fill = "grey") +
-    geom_sf(data = ports_left_north_sf, color = "red", size = 2,  show.legend = FALSE) +
-    geom_sf(data = ports_left_south_sf, color = "red", size = 2,  show.legend = FALSE) +
-    geom_sf(data = ports_right_north_sf, color = "red", size = 2,  show.legend = FALSE) +
-    geom_sf(data = ports_right_south_sf, color = "red", size = 2,  show.legend = FALSE) +
+    geom_sf(data = ports_left_sf, color = "red", size = 2,  show.legend = FALSE) +
+    geom_sf(data = ports_right_sf, color = "red", size = 2,  show.legend = FALSE) +
     geom_text_repel(data = states,
                      aes(x = lon,
                          y = lat,
@@ -74,32 +58,18 @@ make.overview.map.plot <- function(path,
                     color = "black",
                     segment.size = 1,
                     arrow = arrow(length = unit(0.02, "npc"))) +
-    geom_text_repel(data = ports_left_north,
-                    aes(x = lon,
-                        y = lat,
-                        label = name),
-                    xlim = c(NA, -133),
-                    segment.size = 0.8,
-                    arrow = arrow(length = unit(0.01, "npc"))) +
-    geom_text_repel(data = ports_left_south,
-                    aes(x = lon,
-                        y = lat,
-                        label = name),
+    geom_text_repel(data = ports_left,
+                     aes(x = lon,
+                         y = lat,
+                         label = name),
                     xlim = c(NA, -125),
                     segment.size = 0.8,
                     arrow = arrow(length = unit(0.01, "npc"))) +
-    geom_text_repel(data = ports_right_north,
-                    aes(x = lon,
-                        y = lat,
-                        label = name),
-                    xlim = c(-129, NA),
-                    segment.size = 0.8,
-                    arrow = arrow(length = unit(0.01, "npc"))) +
-    geom_text_repel(data = ports_right_south,
-                    aes(x = lon,
-                        y = lat,
-                        label = name),
-                    xlim = c(-122, NA),
+    geom_text_repel(data = ports_right,
+                     aes(x = lon,
+                         y = lat,
+                         label = name),
+                     xlim = c(-122, NA),
                     segment.size = 0.8,
                     arrow = arrow(length = unit(0.01, "npc"))) +
     coord_sf(xlim = extents[[1]],
