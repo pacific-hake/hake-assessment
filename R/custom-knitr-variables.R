f580dc142c22feac50623b3db8030e2d2639ac6e
Chris Grandin <chris.grandin@gmail.com>
2020-02-11 21:30:26 -0800

    Add more slides in Estimated Qunatities section of assessment presentation

--- R/custom-knitr-variables.R
+++ R/custom-knitr-variables.R
@@ -355,6 +355,7 @@ years.since.2010.recruitment.med.below.mean <- names(recruitment.med.since.2010[
 recruitment.med.in.2014 <- f(base.model$mcmccalcs$rmed["2014"], 3)
 last.assess.recruitment.med.in.2014 <- f(last.yr.base.model$mcmccalcs$rmed["2014"], 3)
 prob.percent.2014.rec.gt.2010.rec <- f(mean(base.model$mcmc$Recr_2014 > base.model$mcmc$Recr_2010) * 100, 1)
+prob.percent.2016.rec.gt.2010.rec <- f(mean(base.model$mcmc$Recr_2016 > base.model$mcmc$Recr_2010) * 100, 1)
 recruitment.lower.in.2016 <- f(base.model$mcmccalcs$rlower["2016"], 3)
 recruitment.med.in.2016 <- f(base.model$mcmccalcs$rmed["2016"], 3)
 recruitment.upper.in.2016 <- f(base.model$mcmccalcs$rupper["2016"], 3)
