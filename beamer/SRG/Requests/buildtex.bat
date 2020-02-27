c9602e169ecbda5175eb6ad4d94e33923d791404
Chris Grandin <chris.grandin@gmail.com>
2020-02-27 08:29:31 -0800

    Change build script to day 2

--- beamer/SRG/Requests/buildtex.bat
+++ beamer/SRG/Requests/buildtex.bat
@@ -1,3 +1,3 @@
-Rscript -e "library(knitr);knit('./beamer-hake-requests.rnw')" 1> knitrOutput.log 2>&1
+Rscript -e "library(knitr);knit('./beamer-hake-requests-day-2.rnw')" 1> knitrOutput.log 2>&1
 
-(@latex -synctex=1 "beamer-hake-requests.tex" && latex "beamer-hake-requests.tex" && latex "beamer-hake-requests.tex" && dvips "beamer-hake-requests.dvi" && ps2pdf "beamer-hake-requests.ps") 1> latexOutput.log 2>&1
+(@latex -synctex=1 "beamer-hake-requests-day-2.tex" && latex "beamer-hake-requests-day-2.tex" && latex "beamer-hake-requests-day-2.tex" && dvips "beamer-hake-requests-day-2.dvi" && ps2pdf "beamer-hake-requests-day-2.ps") 1> latexOutput.log 2>&1
