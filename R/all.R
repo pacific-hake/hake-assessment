e5c2c206187dfdc82c50807b3e77efd5a2f492cb
Chris Grandin <chris.grandin@gmail.com>
2020-12-18 11:03:17 -0800

    Clean up knitr alttext build process and fix some docs

--- R/all.R
+++ R/all.R
@@ -88,9 +88,11 @@ show_ss_output <- FALSE
 
 source(file.path(rootd.R, "utilities.R"))
 source(file.path(rootd.R, "add-alt-text.R"))
+source(file.path(rootd.R, "knit-alt-text.R"))
 source(file.path(rootd.R, "catches.R"))
 source(file.path(rootd.R, "run-catch-levels.R"))
 source(file.path(rootd.R, "create-rds-file.R"))
+source(file.path(rootd.R, "build-doc.R"))
 source(file.path(rootd.R, "delete-files.R"))
 source(file.path(rootd.R, "extra-mcmc.R"))
 source(file.path(rootd.R, "retrospectives.R"))
