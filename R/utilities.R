3f38da85f5bb3d6588cc0618a312efbe05af1177
Chris Grandin <chris.grandin@gmail.com>
2021-12-04 02:46:17 -0800

    Add robust model executable checking to adnuts * No pull request yet, added locally * Added .sample_adbb() to overwrite the adnuts one

--- R/utilities.R
+++ R/utilities.R
@@ -1029,7 +1029,7 @@ calc_quantiles <- function(df = NULL,
                            col = NULL,
                            probs = c(0.05, 0.25, 0.5, 0.75, 0.95),
                            include_mean = TRUE){
-  
+
   stopifnot(col %in% names(df))
   stopifnot(class(df[[col]]) == "numeric")
   col_sym <- sym(col)
@@ -1038,7 +1038,7 @@ calc_quantiles <- function(df = NULL,
                       map(probs,
                           ~partial(quantile, probs = .x, na.rm = TRUE)) %>%
                         set_names(probs))
-  
+
   if(include_mean){
     out <- out %>%
       mutate(avg = mean(df[[col]]))
@@ -1098,15 +1098,15 @@ calc_quantiles_by_group <- function(df = NULL,
                                     grp_names = grp_col,
                                     probs = c(0.05, 0.25, 0.5, 0.75, 0.95),
                                     include_mean = TRUE){
-  
+
   stopifnot(grp_col %in% names(df))
   stopifnot(col %in% names(df))
-  
+
   grp_col_sym <- sym(grp_col)
   grp_names_sym <- sym(grp_names)
   col_sym <- sym(col)
   grp_vals <- unique(df[[grp_names]])
-  
+
   df %>%
     group_by(!!grp_col_sym) %>%
     group_map(~ calc_quantiles(.x, col = col, probs = probs, include_mean = include_mean)) %>%
@@ -1114,4 +1114,119 @@ calc_quantiles_by_group <- function(df = NULL,
     mutate(!!grp_names_sym := grp_vals) %>%
     select(!!grp_names_sym, everything()) %>%
     ungroup()
+}
+
+#' Return the Operating System name in lower case
+#'
+#' @return A character string describing the OS, e.g. 'windows', 'linux', or 'osx'
+#' @export
+get_os <- function(){
+  sysinf <- Sys.info()
+  if(!is.null(sysinf)){
+    os <- sysinf["sysname"]
+    if(os == "Darwin")
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
+#' Find the model executable name on your filesystem
+#'
+#' @description
+#' Find the model executable name in the local directory (`loc_dir`). If not there, search the PATH.
+#' If found more than once in the PATH, the first one will be used and a warning issued.
+#' If not on the PATH, throw an error and stop code execution.
+#' If using Linux or OSX and the file permissions do not allow the user to execute,
+#' throw an error and stop code execution.
+#'
+#' @param model The name of the executable (without any extension)
+#' @param loc_dir The path to check for existence of the executable before checking the path
+#' @param path_only If `TRUE`, ignore `loc_dir` and search the PATH only
+#'
+#' @return The executable as it should be called in the OS. It is either just as it was given if on the PATH
+#' or in the `loc_dir` directory on a Windows machine, or prepended with './' if in the `loc_dir` directory on a
+#' non-Windows machine. If not found anywhere, an error will be thrown and code execution will stop
+#' @export
+get_model_executable <- function(model, loc_dir = getwd(), path_only = FALSE){
+
+  check_dot <- grep("\\.", model)
+  if(length(check_dot)){
+    stop("model name cannot include a '.', Do not include extension in model name", call. = FALSE)
+  }
+
+  os <- get_os()
+  wch <- ifelse(os == "windows", "where", "which")
+  loc_dir_files <- dir(loc_dir)
+  model_regex <- gsub("\\+", "\\\\+", model)
+  model_name <- grep(paste0("^", model_regex, ifelse(os == "windows", ".exe", ""), "$"), loc_dir_files, value = TRUE)
+  if(!path_only && length(model_name)){
+    if(os != "windows"){
+      model <- paste0("./", model)
+    }
+    found_model_loc <- "loc_dir"
+  }else{
+    # No local executable, check PATH for one by using which or where
+
+    # Check that which or where are installed on the OS
+    tryCatch(system(wch, intern = TRUE),
+             warning = function(w){},
+             error = function(e){
+               stop("The '", wch, "' command was not found on your OS and is necessary ",
+                    "to search the PATH for your model executable.\n",
+                    "Install it and try again or place executable in the local directory.",
+                    call. = FALSE)
+             })
+
+    model_path <- tryCatch(system(paste(wch, model), intern = TRUE),
+                           warning = function(w){
+                             NA
+                           })
+    if(is.na(model_path[1])){
+      stop(model, " not found locally or on the PATH.", call. = FALSE)
+    }
+    if(length(model_path) > 1){
+      warning("The model executable ", model, " was found in more than one place on your PATH.\n",
+              "Using the first one found (", model_path[1], ").")
+    }
+    found_model_loc <- "path"
+    model <- model_path[1]
+  }
+  # Check to see if the file is executable (LInux and OSX only)
+  if(os != "windows"){
+    stat <- system(paste("stat", model), intern = TRUE)
+    perms <- grep("^Access:.*Uid.*$", stat, value = TRUE)
+    perms <- gsub("\\).*", "", perms)
+    perms <- gsub(".*/","",perms)
+    first_perm <- substr(perms, 1, 1)
+    user_exec_perm <- substr(perms, 4, 4)
+    if(first_perm == "d"){
+      stop("The model name given (", model, ") is a directory and not executable.",
+           call. = FALSE)
+    }else if(first_perm == "p"){
+      stop("The model name given (", model, ") is a named pipe and not executable.",
+           call. = FALSE)
+    }else if(first_perm == "s"){
+      stop("The model name given (", model, ") is a socket and not executable.",
+           call. = FALSE)
+    }else if(first_perm %in% c("b", "c")){
+      stop("The model name given (", model, ") is a device block and not executable.",
+           call. = FALSE)
+    }else if(user_exec_perm != "x"){
+      stop("The file found (", model, ") is not executable.\nCheck name and use chmod to ",
+           "change permissions if name is correct.",
+           call. = FALSE)
+    }
+  }
+  if(found_model_loc == "loc_dir"){
+    message("Using model found in the local directory given by the 'loc_dir' argument: ", model)
+  }else if(found_model_loc == "path"){
+    message("Using model executable found in the PATH: ", model)
+  }
+  model
 }
