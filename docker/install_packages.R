b0c8a681e27c3f8fe2b90f7d770b255cb970a28b
Chris Grandin <chris.grandin@gmail.com>
2021-12-06 23:26:02 -0800

    Add docker files to repo * Used to make docker image used on AWS cloud

---
+++ docker/install_packages.R
@@ -0,0 +1,24 @@
+install.packages(c(
+  "cowplot", "date", "furrr", "ggpubr",
+  "ggrepel", "here", "maps", "maptools", "matrixcalc",
+  "rgeos", "rnaturalearth", "rstan", "shinystan", "snowfall",
+  "tictoc"))
+
+devtools::install_github("r4ss/r4ss@bioscale")
+devtools::install_github("ropensci/rnaturalearthhires")
+# 2021 assessment SHA for adnuts (Jan 12, 2021 - base run was on
+# Jan 14 so this was the most recent)
+devtools::install_github("cgrandin/adnuts@hake2021")
+
+tinytex::tlmgr_install(c(
+  "amsmath", "adjustbox", "algorithmicx", "algorithms", "appendix",
+  "booktabs", "caption", "chngcntr", "cite", "collectbox", "courier",
+  "datetime", "datetime2", "dvips", "ec", "enumitem", "epstopdf-pkg",
+  "etex-pkg", "etoolbox", "fancyhdr", "fancyvrb", "float",
+  "fmtcount", "framed", "geometry", "hyperref", "iftex",
+  "import", "infwarerr", "kvoptions", "lastpage", "linegoal",
+  "lineno", "marginnote", "multirow", "natbib", "ncctools",
+  "nowidow", "pdflscape", "pdfcomment", "pdftexcmds", "psnfss",
+  "rsfs", "soulpos", "soulutf8", "subfig", "times",
+  "tocloft", "tracklang", "zref"))
+
