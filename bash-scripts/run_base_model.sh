5d889c60c45ce995815eef4bc7bf31080b271cc1
Chris Grandin <chris.grandin@gmail.com>
2021-12-13 01:02:34 -0800

    Add source all.R to the bash shell script

--- bash-scripts/run_base_model.sh
+++ bash-scripts/run_base_model.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); run_adnuts('$MODEL_DIR/2021.00.04_base_v1', adapt_delta = 0.9)" > adnuts.log 2>&1)
+(trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); run_adnuts('$MODEL_DIR/2021.00.04_base_v1', adapt_delta = 0.9)" > adnuts.log 2>&1)
