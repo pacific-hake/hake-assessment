5d6d00699e425906358ebe930c8c188fc4cd22c6
Chris Grandin <chris.grandin@gmail.com>
2021-12-18 02:29:22 -0800

    Ensure docker starts and hakestore drive mount hapens at every startup from 'stopped'

--- aws/install_hake.sh
+++ aws/install_hake.sh
@@ -27,18 +27,29 @@ chmod 640 /etc/passwd-s3fs
 # Set Fuse to allow all users access to the newly mounted drives
 echo "user_allow_other" | sudo tee -a /etc/fuse.conf
 
+# Add users
+# useradd -m cgrandin
+# passwd -d cgrandin
+# git clone https://github.com/pacific-hake/hake-assessment
+# chmod -R 777 hake-assessment
+# Create the new directory to mount the S3 drive on
+# mkdir -p /home/cgrandin/hake-assessment/hakestore
+
 # Set up directory structure like this /home/ec2-user/hake-assessment/hakedrave/models-2021/...
 # Pull the hake repository and make it writable for user ec2-user
 cd /home/ec2-user
 git clone https://github.com/pacific-hake/hake-assessment
 chmod -R 777 hake-assessment
-
 # Create the new directory to mount the S3 drive on
 mkdir -p /home/ec2-user/hake-assessment/hakestore
-# Mount it at /home/ec2-user/hake-assessment/hakestore using S3 fuse
-s3fs hakestore -o use_cache=/tmp -o allow_other -o umask=0000 /home/ec2-user/hake-assessment/hakestore
 
-# Install htop which is a CPU process viewer
+# This is how you would mount the S3 drive manually from the command line
+#  at /home/ec2-user/hake-assessment/hakestore using S3 fuse:
+# s3fs hakestore -o use_cache=/tmp -o allow_other -o umask=0000 /home/ec2-user/hake-assessment/hakestore
+# Place the s3fs mounting into fstab
+echo "s3fs#hakestore /home/ec2-user/hake-assessment/hakestore fuse _netdev,allow_other,use_cache=/tmp,umask=0000" | tee -a /etc/fstab
+
+# Install htop, a CPU process viewer
 yum -y install htop
 
 # Set the ssh server up to send timeout checks to the clients to make sure they are still connected
@@ -49,20 +60,24 @@ echo "ClientAliveInterval 0" | tee -a /etc/ssh/sshd_config
 # Install and run docker
 amazon-linux-extras enable docker
 yum -y install docker
-# Adds user ec2-user to the docker group so you don't need to prefix with sudo
 service docker start
+# Adds user ec2-user to the docker group so you don't need to prefix with sudo
 usermod -a -G docker ec2-user
 chmod 666 /var/run/docker.sock
-docker pull cgrandin/hake
+docker pull cgrandin/hake-noextramcmc
 cd /home/ec2-user
 docker run -d -p 8787:8787 -e USER=rstudio -e PASSWORD=a \
-       -e MODEL_DIR=models --name=hake \
-       --mount type=bind,source="$(pwd)",target=/home/rstudio cgrandin/hake
+       -e MODEL_DIR=models --name=hake --restart always \
+       --mount type=bind,source="$(pwd)",target=/home/rstudio cgrandin/hake-noextramcmc
+# These two commands ensure the docker service starts when the machine starts after being stopped
+systemctl enable docker.service
+systemctl enable containerd.service
 
 # Add rstudio user to sudoers so they can run mkdir command
 echo "rstudio ALL=NOPASSWD: /usr/bin/mkdir" | tee -a /etc/sudoers
 echo "rstudio ALL=NOPASSWD: /usr/bin/rmdir" | tee -a /etc/sudoers
-#echo "rstudio ALL=(ALL) NOPASSWD:ALL" | tee -a /etc/sudoers
+# To give rstudio FULL permissions use next line
+# echo "rstudio ALL=(ALL) NOPASSWD:ALL" | tee -a /etc/sudoers
 
 # Start in hake-assessment directory
 echo "if(interactive()) setwd('hake-assessment')" | tee -a /home/ec2-user/.Rprofile
@@ -70,19 +85,8 @@ echo "if(interactive()) setwd('hake-assessment')" | tee -a /home/ec2-user/.Rprof
 echo "if(interactive()) source('R/all.R')" | tee -a /home/ec2-user/.Rprofile
 echo "if(interactive()) copy_dirfiles('hakestore/models-2021-ss-input-files', 'models')" | tee -a /home/ec2-user/.Rprofile
 echo "if(interactive()) system_('chmod -R 777 models')" | tee -a /home/ec2-user/.Rprofile
-#echo "if(interactive()) system_('umask 0000')" | tee -a /home/ec2-user/.Rprofile
-
-# Append the docker stuff to rc.local, so that the docker container is started at EVERY
-# start, not just on instance creation as is with User Data.
-echo "service docker start" | tee -a /etc/rc.local
-echo "usermod -a -G docker ec2-user" | tee -a /etc/rc.local
-echo "cd /home/ec2-user" | tee -a /etc/rc.local
-echo "docker run -d -p 8787:8787 -e USER=rstudio -e PASSWORD=a \
-                 -e MODEL_DIR=models --name=hake \
-                 --mount type=bind,source="$(pwd)",target=/home/rstudio cgrandin/hake" | tee -a /etc/rc.local
 
 # Open in a web browser using instance IP:8787 like this example, 3.96.123.102:8787
 # Rstudio login: rstudio (USER above)
 # Password: a (PASSWORD above)
-
 
