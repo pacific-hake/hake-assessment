008437ea1502569e3ae6ef518cea0d395ab06058
Chris Grandin <chris.grandin@gmail.com>
2021-12-24 01:14:53 -0800

    Add model copy command to bash shell script

--- bash-scripts/run_base_model.sh
+++ bash-scripts/run_base_model.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); run_adnuts('$MODEL_DIR/2021.00.04_base_v1', adapt_delta = 0.9)" > adnuts.log 2>&1)
+cp -R /home/ec2-user/hake-assessment/hakestore/2021.00.04_base_v1 /home/ec2-user/hake-assessment/models
