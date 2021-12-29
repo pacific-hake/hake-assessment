edc262b334ff18d8e5e782664602a12c2237134a
Chris Grandin <chris.grandin@gmail.com>
2021-12-29 10:36:16 -0800

    Pipe output foir bash script to /dev/null * Was adnuts.log which broke r4ss::SS_output()

--- bash-scripts/run_base_model.sh
+++ bash-scripts/run_base_model.sh
@@ -4,7 +4,7 @@
 # Environment variable $MODEL_DIR is set in aws/install_hake.sh
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
 run_adnuts('$MODEL_DIR/$BASE_MODEL', adapt_delta = 0.9)" \
-> adnuts.log 2>&1)
+> /dev/null 2>&1)
 
 # Change a filename so that it works with hake-assessment code.
 # R4ss breaks if this is not done.
@@ -16,7 +16,7 @@ mv ~/hake-assessment/$MODEL_DIR/$BASE_MODEL/data_echo.ss_new \
 # Environment variable $BASE_MODEL is set in R/model-setup.R
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
 build_rds('$BASE_MODEL', run_catch_levels = TRUE, run_forecasts = TRUE)" \
-> adnuts.log 2>&1)
+> /dev/null 2>&1)
 
 # Copy all the output for the base model to the persistent S3 drive 'hakestore'
 DATE_STR="_dec29"
