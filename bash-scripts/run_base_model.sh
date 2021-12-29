0cb0ae1bb6f555ac5c1c98fa27f6cf9ce8e12343
Chris Grandin <chris.grandin@gmail.com>
2021-12-29 00:10:43 -0800

    Move a opy command in AWS script so that build_rds() works

--- bash-scripts/run_base_model.sh
+++ bash-scripts/run_base_model.sh
@@ -6,14 +6,14 @@
 run_adnuts('$MODEL_DIR/$BASE_MODEL', adapt_delta = 0.9)" \
 > adnuts.log 2>&1)
 
+# Change a filename so that it works with hake-assessment code. r4ss breaks if this is not done
+mv ~/hake-assessment/$MODEL_DIR/$BASE_MODEL/data_echo.ss_new ~/hake-assessment/$MODEL_DIR/$BASE_MODEL/data.ss_new
+
 # Run the base models forecasting and retrospectives, and build the RDS file
 # Environment variable $BASE_MODEL is set in R/model-setup.R
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
 build_rds('$BASE_MODEL', run_catch_levels = TRUE, run_forecasts = TRUE)" \
 > adnuts.log 2>&1)
-
-# Change a filename so that it works with hake-assessment code. r4ss breaks if this is not done
-mv ~/hake-assessment/$MODEL_DIR/$BASE_MODEL/data_echo.ss_new ~/hake-assessment/$MODEL_DIR/$BASE_MODEL/data.ss_new
 
 # Copy all the output for the base model to the persistent S3 drive 'hakestore'
 DATE_STR="_dec29"
