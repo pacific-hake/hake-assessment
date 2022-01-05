610e100c4b3d7f92295c81b9388ae51763e69ea6
Chris Grandin <chris.grandin@gmail.com>
2022-01-05 02:23:34 -0800

    Add progress messages to the base model run script

--- bash-scripts/run_base_model.sh
+++ bash-scripts/run_base_model.sh
@@ -3,21 +3,28 @@
 # Run base model MCMC using adnuts on AWS
 # Environment variable $MODEL_DIR is set in aws/install_hake.sh
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_adnuts('$MODEL_DIR/$BASE_MODEL', adapt_delta = 0.9)" \
-> /dev/null 2>&1)
+run_adnuts('$MODEL_DIR/$BASE_MODEL', adapt_delta = 0.99)" \
+> /dev/null 2>&1; echo "Base model MCMC complete")
 
 # Change a filename so that it works with hake-assessment code.
 # R4ss breaks if this is not done.
 mv ~/hake-assessment/$MODEL_DIR/$BASE_MODEL/data_echo.ss_new \
 ~/hake-assessment/$MODEL_DIR/$BASE_MODEL/data.ss_new
 
-# Run the base models catch-level calculations and forecasting
+# Run the base models catch-level calculations
 # Environment variable $BASE_MODEL is set in R/model-setup.R
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-build_rds('$BASE_MODEL', run_catch_levels = TRUE, run_forecasts = TRUE, build_file = FALSE)" \
-> /dev/null 2>&1)
+build_rds('$BASE_MODEL', run_catch_levels = TRUE, build_file = FALSE)" \
+> /dev/null 2>&1; echo "Base model catch level calculations complete")
+
+# Run the base models forecasts
+# Environment variable $BASE_MODEL is set in R/model-setup.R
+(trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
+build_rds('$BASE_MODEL', run_catch_levels = FALSE, run_forecasts = TRUE, build_file = FALSE)" \
+> /dev/null 2>&1; echo "Base model forecasts complete")
 
 # Copy all the output for the base model to the persistent S3 drive 'hakestore'
 DATE_STR="_jan04_2022"
 cp -R ~/hake-assessment/$MODEL_DIR/$BASE_MODEL \
 ~/hake-assessment/hakestore/$BASE_MODEL$DATE_STR
+echo "Copied Base model output to S3 storage"
