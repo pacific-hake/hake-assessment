eeecba740043c860c02f91b4aa21a5ff62fb7f58
Chris Grandin <chris.grandin@gmail.com>
2022-01-14 12:39:47 -0800

    Add no-age-1 bash scripts and generalize copy command explanation

--- bash-scripts/run_retrospectives_1_to_6_noage1.sh
+++ bash-scripts/run_retrospectives_1_to_6_noage1.sh
@@ -2,35 +2,36 @@
 
 # Run the base model retrospectives 1 through 6. Each one needs 16 CPUs to run at
 # optimal speed so they need 96 CPUS
-# Environment variable $BASE_MODEL is set in R/model-setup.R
+# Environment variable $NO_AGE1_MODEL is set in R/model-setup.R
 
 # Required the MLE run in the root part of the model directory have all it files present
-# cp hakestore/models/$BASE_MODEL/* models/$BASE_MODEL
+# cp hakestore/models/$NO_AGE1_MODEL/* models/$NO_AGE1_MODEL
 
+NO_AGE1_MODEL=2022.01.23_age1Survey
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_retrospectives('$MODELS_DIR/$BASE_MODEL', retrospective_yrs = 1, n_cores = 16)" \
+run_retrospectives('$MODELS_DIR/$NO_AGE1_MODEL', retrospective_yrs = 1, n_cores = 16)" \
 > /dev/null 2>&1; echo "Restrospective - 1 complete") &
 
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_retrospectives('$MODELS_DIR/$BASE_MODEL', retrospective_yrs = 2, n_cores = 16)" \
+run_retrospectives('$MODELS_DIR/$NO_AGE1_MODEL', retrospective_yrs = 2, n_cores = 16)" \
 > /dev/null 2>&1; echo "Restrospective - 2 complete") &
 
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_retrospectives('$MODELS_DIR/$BASE_MODEL', retrospective_yrs = 3, n_cores = 16)" \
+run_retrospectives('$MODELS_DIR/$NO_AGE1_MODEL', retrospective_yrs = 3, n_cores = 16)" \
 > /dev/null 2>&1; echo "Restrospective - 3 complete") &
 
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_retrospectives('$MODELS_DIR/$BASE_MODEL', retrospective_yrs = 4, n_cores = 16)" \
+run_retrospectives('$MODELS_DIR/$NO_AGE1_MODEL', retrospective_yrs = 4, n_cores = 16)" \
 > /dev/null 2>&1; echo "Restrospective - 4 complete") &
 
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_retrospectives('$MODELS_DIR/$BASE_MODEL', retrospective_yrs = 5, n_cores = 16)" \
+run_retrospectives('$MODELS_DIR/$NO_AGE1_MODEL', retrospective_yrs = 5, n_cores = 16)" \
 > /dev/null 2>&1; echo "Restrospective - 5 complete") &
 
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_retrospectives('$MODELS_DIR/$BASE_MODEL', retrospective_yrs = 6, n_cores = 16)" \
+run_retrospectives('$MODELS_DIR/$NO_AGE1_MODEL', retrospective_yrs = 6, n_cores = 16)" \
 > /dev/null 2>&1; echo "Restrospective - 6 complete")
 
-cp -R ~/hake-assessment/$MODELS_DIR/$BASE_MODEL/retrospectives/retro-0[1-6] \
-~/hake-assessment/hakestore/$BASE_MODEL/retrospectives
+cp -R ~/hake-assessment/$MODELS_DIR/$NO_AGE1_MODEL/retrospectives/retro-0[1-6] \
+~/hake-assessment/hakestore/$NO_AGE1_MODEL/retrospectives
 echo "Copied Base model retrospectives 1-6 to S3 storage"
--- bash-scripts/run_retrospectives_7_to_10_noage1.sh
+++ bash-scripts/run_retrospectives_7_to_10_noage1.sh
@@ -2,29 +2,30 @@
 
 # Run the base model retrospectives 7 through 10. Each one needs 16 CPUs to run at
 # optimal speed so they need 64 CPUS
-# Environment variable $BASE_MODEL is set in R/model-setup.R
+# Environment variable $NO_AGE1_MODEL is set in R/model-setup.R
 
 # Required the MLE run in the root part of the model directory have all it files present
-# cp hakestore/models/$BASE_MODEL/* models/$BASE_MODEL
+# cp hakestore/models/$NO_AGE1_MODEL/* models/$NO_AGE1_MODEL
 
+NO_AGE1_MODEL=2022.01.23_age1Survey
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_retrospectives('$MODELS_DIR/$BASE_MODEL', retrospective_yrs = 7, n_cores = 16)" \
+run_retrospectives('$MODELS_DIR/$NO_AGE1_MODEL', retrospective_yrs = 7, n_cores = 16)" \
 > /dev/null 2>&1; echo "Restrospective - 7 complete") &
 
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_retrospectives('$MODELS_DIR/$BASE_MODEL', retrospective_yrs = 8, n_cores = 16)" \
+run_retrospectives('$MODELS_DIR/$NO_AGE1_MODEL', retrospective_yrs = 8, n_cores = 16)" \
 > /dev/null 2>&1; echo "Restrospective - 8 complete") &
 
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_retrospectives('$MODELS_DIR/$BASE_MODEL', retrospective_yrs = 9, n_cores = 16)" \
+run_retrospectives('$MODELS_DIR/$NO_AGE1_MODEL', retrospective_yrs = 9, n_cores = 16)" \
 > /dev/null 2>&1; echo "Restrospective - 9 complete") &
 
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_retrospectives('$MODELS_DIR/$BASE_MODEL', retrospective_yrs = 10, n_cores = 16)" \
+run_retrospectives('$MODELS_DIR/$NO_AGE1_MODEL', retrospective_yrs = 10, n_cores = 16)" \
 > /dev/null 2>&1; echo "Restrospective - 10 complete")
 
-cp -R ~/hake-assessment/$MODELS_DIR/$BASE_MODEL/retrospectives/retro-0[7-9] \
-~/hake-assessment/hakestore/$BASE_MODEL/retrospectives
-cp -R ~/hake-assessment/$MODELS_DIR/$BASE_MODEL/retrospectives/retro-10 \
-~/hake-assessment/hakestore/$BASE_MODEL/retrospectives
+cp -R ~/hake-assessment/$MODELS_DIR/$NO_AGE1_MODEL/retrospectives/retro-0[7-9] \
+~/hake-assessment/hakestore/$NO_AGE1_MODEL/retrospectives
+cp -R ~/hake-assessment/$MODELS_DIR/$NO_AGE1_MODEL/retrospectives/retro-10 \
+~/hake-assessment/hakestore/$NO_AGE1_MODEL/retrospectives
 echo "Copied Base model retrospectives 7-10 to S3 storage"
