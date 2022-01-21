43ba8f8e1e4340936c7a2ab35e62ab072698fd56
Chris Grandin <chris.grandin@gmail.com>
2022-01-20 16:28:27 -0800

    Add docker container naming to AWS script * Add ADMB path and files to SS docker image

--- aws/install_hake.sh
+++ aws/install_hake.sh
@@ -3,9 +3,20 @@
 # Keys -------------------------------------------------------------------------
 # *Remove these keys before pushing to GitHub*
 
+# Get this public key by running ssh-keygen on the private key file:
+# ssh-keygen -y -f ~/.ssh/hake.pem
+ssh_key=""
+# If this access key goes stale or becomes comprimised, create a new key/secret in the
+# IAM Management Console, Users/Administrators/Administrator,
+# Security credentials tab. Download the CSV and paste the values here with a colon in between.
+access_key=""
+
 # Bash environment variables ---------------------------------------------------
 models_dir=models
 base_model_dir=2022.01.10_base
+docker_container=cgrandin/hake_hess_step
+#docker_container=cgrandin/hake_2021
+#docker_container=cgrandin/hake_admb_ss
 
 # Users ------------------------------------------------------------------------
 # Add user cgrandin
@@ -174,36 +185,36 @@ usermod -a -G docker aberger
 usermod -a -G docker kjohnson
 usermod -a -G docker koken
 chmod 666 /var/run/docker.sock
-docker pull cgrandin/hake
+docker pull $docker_container
 cd /home/ec2-user
 docker run -d -p 8787:8787 -e USER=rstudio -e PASSWORD=a \
        -e MODELS_DIR=$models_dir -e BASE_MODEL=$base_model_dir --name=hake-rstudio --restart always \
-       --mount type=bind,source="$(pwd)",target=/home/rstudio cgrandin/hake
+       --mount type=bind,source="$(pwd)",target=/home/rstudio $docker_container
 # Run docker container for user cgrandin
 cd /home/cgrandin
 docker run -d -p 8780:8780 -e USER=cgrandin -e PASSWORD=a \
        -e MODELS_DIR=$models_dir -e BASE_MODEL=$base_model_dir --name=hake-cgrandin --restart always \
-       --mount type=bind,source="$(pwd)",target=/home/cgrandin cgrandin/hake
+       --mount type=bind,source="$(pwd)",target=/home/cgrandin $docker_container
 # Run docker container for user aedwards
 cd /home/aedwards
 docker run -d -p 8781:8781 -e USER=aedwards -e PASSWORD=a \
        -e MODELS_DIR=$models_dir -e BASE_MODEL=$base_model_dir --name=hake-aedwards --restart always \
-       --mount type=bind,source="$(pwd)",target=/home/aedwards cgrandin/hake
+       --mount type=bind,source="$(pwd)",target=/home/aedwards $docker_container
 # Run docker container for user aberger
 cd /home/aberger
 docker run -d -p 8782:8782 -e USER=aberger -e PASSWORD=a \
        -e MODELS_DIR=$models_dir -e BASE_MODEL=$base_model_dir --name=hake-aberger --restart always \
-       --mount type=bind,source="$(pwd)",target=/home/aberger cgrandin/hake
+       --mount type=bind,source="$(pwd)",target=/home/aberger $docker_container
 # Run docker container for user kjohnson
 cd /home/kjohnson
 docker run -d -p 8783:8783 -e USER=kjohnson -e PASSWORD=a \
        -e MODELS_DIR=$models_dir -e BASE_MODEL=$base_model_dir --name=hake-kjohnson --restart always \
-       --mount type=bind,source="$(pwd)",target=/home/kjohnson cgrandin/hake
+       --mount type=bind,source="$(pwd)",target=/home/kjohnson $docker_container
 # Run docker container for user koken
 cd /home/koken
 docker run -d -p 8784:8784 -e USER=koken -e PASSWORD=a \
        -e MODELS_DIR=$models_dir -e BASE_MODEL=$base_model_dir --name=hake-koken --restart always \
-       --mount type=bind,source="$(pwd)",target=/home/koken cgrandin/hake
+       --mount type=bind,source="$(pwd)",target=/home/koken $docker_container
 
 # These two commands ensure the docker service starts when the machine starts after being stopped
 systemctl enable docker.service
