d1f402c54d9706f7979f12b73bc001f40f79e79a
Chris Grandin <chris.grandin@gmail.com>
2022-01-26 11:40:22 -0800

    Update forecast comparison figure code

--- R/figures-compare-forecasts.R
+++ R/figures-compare-forecasts.R
@@ -62,24 +62,29 @@ make.forecast.catch.posterior.plot <- function(model,         ## model is the mo
   }
 }
 
-make.forecast.depletion.comparison.plot <- function(model,        ## model is the model for which mcmc runs with different forecasts
-                                                                  ## has been made
-                                                    models.inds,  ## A vector of model indices for the forecast models
-                                                                  ## you want to compare. These numbers will reference
-                                                                  ## list members of the base.model$forecasts$outputs list
-                                                    models.names, ## a vector of names for the models (used in legend)
-                                                    start.yr,     ## Year to start the plot on
-                                                    model.end.yr, ## The model end year, not including forecasting.
-                                                                  ## This will be pasted to Bratio_ for densitynames
-                                                                  ## argument to SSplotComparisons
-                                                    end.yr,       ## Year to end the plot on
+#' Plots several forecasts against each other
+#'
+#' @param model The model for which mcmc runs with different forecasts have been made
+#' @param models.inds A vector of model indices for the forecast models you want to
+#' compare These numbers will reference list members of the `model$forecasts$outputs` list
+#' @param models.names A vector of names for the models (used in legend)
+#' @param start.yr Year to start the plot on
+#' @param model.end.yr The model end year, not including forecasting. This will be
+#' pasted to `Bratio_ `for `densitynames` argument to [r4ss::SSplotComparisons()]
+#' @param end.yr Year to end the plot on
+#' @param legend.loc Legend location
+#' @param ... Extra arguments to pass to [r4ss::SSplotComparisons()]
+#'
+#' @return The plot
+#' @export
+make.forecast.depletion.comparison.plot <- function(model,
+                                                    models.inds,
+                                                    models.names,
+                                                    start.yr,
+                                                    model.end.yr,
+                                                    end.yr,
                                                     legend.loc = "topleft",
-                                                    ...
-                                                    ){
-  ## Plots several forecasts against each other. model is typically the
-  ## base model and the forecasts to be plotted are referenced by model.inds
-  ## of the forecasts$outputs list. See Readme.md for reference on the
-  ## model list strucure.
+                                                    ...){
 
   oldpar <- par()
   par(mar = c(4.5, 4, 1, 1))
@@ -92,12 +97,12 @@ make.forecast.depletion.comparison.plot <- function(model,        ## model is th
   SSplotComparisons(compare.summary,
                     legendlabels = models.names,
                     endyr = end.yr,
-                    densitynames = c(paste0("Bratio_",model.end.yr)),
+                    densitynames = c(paste0("Bratio_", model.end.yr)),
                     new = FALSE,
                     minbthresh = 0,
                     subplots = 4,
                     plot = TRUE,
-                    mcmc = rep(TRUE, length(model.list)),
+                    mcmcVec = rep(TRUE, length(model.list)),
                     xlim = c(start.yr,end.yr),
                     legendloc = legend.loc,
                     labels = c("Year",
@@ -115,9 +120,9 @@ make.forecast.depletion.comparison.plot <- function(model,        ## model is th
                     staggerpoints = 1990,
                     spacepoints=200,
                     ...)
-  abline(h=c(0.1,0.4),lty=2,col="grey")
-  axis(2,at=c(0.1,0.4),las=1,cex.axis=0.8)
-  axis(1,at=seq(start.yr,end.yr,2))
+  abline(h = c(0.1, 0.4), lty = 2, col =" grey")
+  axis(2, at = c(0.1, 0.4), las = 1, cex.axis = 0.8)
+  axis(1, at = seq(start.yr, end.yr, 2))
   par <- oldpar
 }
 
