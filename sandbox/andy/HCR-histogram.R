ad7052cfa1dcfdf3a19910bc28aa2c18f87ba37f
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2022-02-15 10:55:15 -0800

    Sandbox directory - each person can have a folder for playing around in.

---
+++ sandbox/andy/HCR-histogram.R
@@ -0,0 +1,177 @@
+
+##' Trying a histogram to replace the overly smoothed density plot of HCR catch
+##' ##'
+##' @param model
+##' @return
+##' @export
+##' @author Andrew Edwards
+##' @examples
+##' @ontrun{
+##' @
+##' @}
+make.forecast.catch.posterior.hist <- function(model,         ## model is the model for which mcmc runs with different forecasts
+                                                              ## has been made
+                                               fore.yr,       ## Forecast year to make the plot with
+                                               xmax = 4000,   ## max x value (could be more dynamic)
+                                               do.plot = TRUE, ## If FALSE, no
+                                               ## plot will be drawn, but the
+                                               ## return values will be returned
+                                               num.bins.in.cred = 30
+                                               ){
+  if(do.plot){
+    oldpar <- par()
+  }
+  dat <- model$mcmc
+  dat <- eval(parse(text = paste0("dat$ForeCatch_", fore.yr))) / 1000.0
+  med.catch <- median(dat)
+
+  cred.interval <- quantile(dat, probs = c(0.025, 0.975))
+  bin.width <- diff(cred.interval)/num.bins.in.cred
+
+  min.bin.break = cred.interval[1] - ceiling(cred.interval[1]/bin.width) * bin.width
+    # First bin will start <0 and sometimes not be empty, if min(dat) is within
+    #  first bin). May want to fix properly if fig looks funny.
+  max.bin.break = cred.interval[2] +
+    ceiling((max(dat) - cred.interval[2])/bin.width) * bin.width
+  bin.breaks = seq(min.bin.break,
+                    max.bin.break,
+                   by = bin.width)
+  par(mfrow = c(2,1))
+  hist(dat,
+       breaks = bin.breaks)
+
+  hist(dat[dat<cred.interval[1]],
+       breaks = bin.breaks,
+       add = TRUE,
+       col = "red")
+  hist(dat[dat>cred.interval[2]],
+       breaks = bin.breaks,
+       add = TRUE,
+       col = "red")
+
+  hpdi <- HDInterval::hdi(dat)
+  bin.breaks.hpdi =
+#DO A FUNCTION FOR THE BIN BREAKS (Above) if needed more
+#  HERE
+
+  br <- base.model$mcmc[["Bratio_2022"]]
+(list "" '(("object" . "") ("..." . "") ("digits" . "") ("quantile.type" . "7")) '("object" "..." "test.statistic" "univariate" "multivariate" "p.adjust.method" "intercept" "split" "expand.split" "keep.zero.df" "parm" "high.moments" "extremes" "all" "full" "maxsum" "digits" "quantile.type" "dispersion" "correlation" "symbolic.cor" "test" "tol" "quantiles" "ny" "names" "verbosity" "loadings" "cutoff" "max_frames" "dir" "srcrefs" "maxp4s" "showValues" "useSource"))
+> > quantile(br, probs = c(0.025, 0.975))
+     2.5%     97.5%
+0.3105285 1.3473200
+> HDInterval::hdi(br)
+   lower    upper
+0.261039 1.233090
+attr(,"credMass")
+[1] 0.95
+  >
+
+B2022 <- base.model$mcmc[["SSB_2022"]]
+> quantile(B2022, probs = c(0.025, 0.975))
+     2.5%     97.5%
+ 583631.8 2584659.0
+> f(quantile(B2022, probs = c(0.025, 0.975)))
+       2.5%       97.5%
+"  583,632" "2,584,659"
+> f(HDInterval::hdi(B2022))
+      lower       upper
+"  469,149" "2,298,790"
+  >
+
+  > br <- base.model$mcmc[["Bratio_2022"]]
+> > quantile(br, probs = c(0.025, 0.975))
+     2.5%     97.5%
+0.3105285 1.3473200
+> HDInterval::hdi(br)
+   lower    upper
+0.261039 1.233090
+attr(,"credMass")
+[1] 0.95
+
+  R_2010 <- base.model$mcmc[["Recr_2010"]]
+> (list "" '(("x" . "") ("dec.points" . "0")) '("x" "dec.points" "dll" "lib" "env" "nativeRoutines"))
+> f(quantile(R_2010, probs = c(0.025, 0.975)))
+        2.5%        97.5%
+"11,200,560" "32,817,810"
+> f(HDInterval::hdi(R_2010))
+       lower        upper
+"10,122,800" "29,072,100"
+>
+
+  This doesn't change so much because it's presumably more symmetric:
+R2014overR2010 <- base.model$mcmc[["Recr_2014"]] / base.model$mcmc[["Recr_2010"]]
+> quantile(R2014overR2010, probs = c(0.025, 0.975))
+     2.5%     97.5%
+0.4504541 0.6674952
+> HDInterval::hdi(R2014overR2010)
+    lower     upper
+0.4415496 0.6541714
+attr(,"credMass")
+[1] 0.95
+
+hist(R2014overR2010, breaks = 50)
+
+R2020overR2010 <- base.model$mcmc[["Recr_2020"]] / base.model$mcmc[["Recr_2010"]]
+sum(R2020overR2010 > 1)/length(R2020overR2010)
+(list "" '(("x" . "")) '("x" "..."))
+ 0.04897959
+
+
+> R2022overR2010 <- base.model$mcmc[["Recr_2022"]] / base.model$mcmc[["Recr_2010"]]
+> sum(R2022overR2010 > 1)/length(R2022overR2010)
+[1] 0.02865473
+
+check 2017 fishing intensity interval. - still overlaps 1 (1.072 vs 1.057 for
+                                                           HDI) - since
+                                                           it's a ratio I think it's less likely to change much since more symmetric of a distribution.
+#  dens <- density(dat, from = min(dat))
+#  dens.orig <- dens
+  if(do.plot){
+    plot(dens,
+       yaxt = "n",
+       ylab = "",
+       xlab = paste0("Projected ",fore.yr," catch based on the default harvest policy (1,000 t)"),
+       type = "l",
+       lty = 1,
+       pch = 16,
+       xlim = c(0, xmax),
+       xaxs = "i",
+       yaxs = "i",
+       ylim = c(0, max(dens$y) * 1.02), lwd = 3, main="", xaxt = "n")
+
+    axis(1, at = seq(0, xmax, 200), lab=rep("", length(seq(0, xmax, 200))))
+    axis(1, at = seq(0, xmax, 400))
+    mtext("Density", side = 2, line = 0.5, las = 0)
+  }
+  yy <- dens
+  # vector of x and y values associated with output from "density"
+  yy$x <- c(min(dens$x), dens$x, max(dens$x))
+  yy$y <- c(-1, dens$y, -1)
+  if(do.plot){
+    polygon(yy$x, yy$y, col = gray(0.9), lwd = 3)
+  }
+  # subsetting values for those within the 95% interval
+  ind <- dens$x >= quantile(dat, 0.025) & dens$x <= quantile(dat, 0.975)
+  dens$x <- dens$x[ind]
+  dens$y <- dens$y[ind]
+  yy$x <- c(min(dens$x), dens$x, max(dens$x))
+  yy$y <- c(-1, dens$y, -1)
+  if(do.plot){
+    polygon(yy$x, yy$y, col = rgb(0, 0, 1, 0.3), lty = 0)
+    lines(dens.orig, lwd = 3)
+  }
+  if(do.plot){
+    tmpy <- dens$y[min(abs(dens$x - med.catch)) == abs(dens$x - med.catch)]
+    lines(c(med.catch, med.catch), c(0, tmpy), lwd = 2)
+    text(med.catch, mean(c(0, tmpy)), paste0("Median = ", round(med.catch,3)), srt = 90, adj = c(0.5, -0.5))
+    box()
+    par <- oldpar
+  }
+  if(!do.plot){
+    # return interval (but exact, not rounded to nearest value of the
+    # 512 x-values used by "density"
+    ret.vec <-c(quantile(dat, 0.025), med.catch, quantile(dat, 0.975))
+    names(ret.vec) <- c("lower", "median", "upper")
+    return(ret.vec)
+  }
+}
---
+++ sandbox/andy/hdi-calcs.R
@@ -0,0 +1,80 @@
+##' See HCR-histogram.R for running it for now.
+##'
+##' Calculate the 95% credible interval of a vector of MCMC samples of a
+##'  quantity in two different ways, and optionally plot histogram
+##'
+##' Calculate the 95% credible interval of the vector as either the highest
+##'  posterior density interval (hpdi)
+##'  or the usual, and simpler, interval between the 2.5% and 97.5%
+##'  quantiles. The hpdi is narrower (or equal for a symmetric distribution)
+##'  and has several advantages, particularly as a decision tool
+##'  (e.g. https://www.sciencedirect.com/topics/mathematics/highest-density-interval).
+##'  Optionally return a histogram showing the samples within and outside the
+##'  credible interval (by calculating bin breaks to exactly to include the
+##'  bounds of the interval); this is preferable to showing a density type plot
+##'  as that requires some smoothing.
+##'
+##' @param vec a vector of MCMC samples of a quantity
+##' @param credible.mass a scalar [0,1] specifying the mass within the credible
+##'   interval (gets passed to `HDIinterval::hdi()` as `credMass`)
+##' @param num.bins.in.cred number of bins to show within the credible interval
+##'   for the histogram.
+##' @param interval.type "hpdi" for the highest posterior density interval
+##'   (calculated using `HDIinterval::hdi()`, or "equal" for equal tailed, as
+##'   traditionally done in the hake assessment).
+##' @param do.plot plot the histogram and return the results, else just return
+##'   the results.
+##' @param ... Further arguments passed to `hist()`.
+##' @return **
+##' @export
+##' @author Andrew Edwards
+##' @examples
+##' @dontrun{
+##' @
+##' @}
+make.posterior.intervals <- function(vec,
+                                     credible.mass = 0.95,
+                                     num.bins.in.cred = 20,
+                                     interval.type = "hpdi",
+                                     do.plot = TRUE,
+                                     ...){
+  stopifnot(interval.type %in% c("hpdi", "equal"))
+
+  if(interval.type == "hpdi"){
+    cred.interval <- HDInterval::hdi(vec,
+                                     credMass = credible.mass)
+    main.title = "Highest density interval"
+  } else {
+    cred.interval <- quantile(vec,
+                              probs = c((1 - credible.mass)/2,
+                                        1 - (1 - credible.mass)/2))
+    main.title = "Equal-tailed interval"
+    #names(cred.interval) = c("lower",
+    #                         "upper")  # to match hpdi names
+  }
+
+  bin.width <- diff(cred.interval)/num.bins.in.cred
+
+  min.bin.break = cred.interval[1] - ceiling(cred.interval[1]/bin.width) * bin.width
+    # First bin will start <0 and sometimes not be empty, if min(vec) is within
+    #  first bin). May want to fix properly if fig looks funny.
+  max.bin.break = cred.interval[2] +
+    ceiling((max(vec) - cred.interval[2])/bin.width) * bin.width
+  bin.breaks = seq(min.bin.break,
+                   max.bin.break,
+                   by = bin.width)
+  hist(vec,
+       breaks = bin.breaks,
+       main = main.title)
+
+  hist(vec[vec<cred.interval[1]],
+       breaks = bin.breaks,
+       add = TRUE,
+       col = "red")
+  hist(vec[vec>cred.interval[2]],
+       breaks = bin.breaks,
+       add = TRUE,
+       col = "red")
+
+  # TODO return
+}
