eab9ab3c4f2c98d15a63aecc5d23b253fb0ae528
Chris Grandin <chris.grandin@gmail.com>
2022-12-17 15:37:23 -0800

    Modify base model running bash script

--- bash-scripts/run-base-model.sh
+++ bash-scripts/run-base-model.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 
-# Run base model MCMC using adnuts on AWS
+# Run base model MCMC using ADNUTS
+
+#project_path=`Rscript -e "cat(dirname(here::here()))"`
+project_path="/srv/hake"
+models_path="models"
+year_path=2023
+version_path="01-version"
+type_path="01-base_models"
+model_name="01-base"
+
+n_cores=16
+n_final=8000
+warmup_final=250
+adapt_delta=0.95
+extra_mcmc=FALSE
+
+model_path=$project_path/$models_path/$year_path/$version_path/$type_path/$model_name
+[ -d "$model_path" ] && echo "Directory $model_path exists, starting run loop." || \
+  { echo "Error: Directory $model_path does not exist, bailing out." ; exit 1; }
+
 # Environment variable $MODELS_DIR is set in aws/install_hake.sh
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-run_adnuts('$MODELS_DIR/$BASE_MODEL', adapt_delta = 0.95, extra_mcmc = TRUE, n_cores = 16)" \
+run_adnuts('$model_path', adapt_delta = $adapt_delta, extra_mcmc = $extra_mcmc, n_cores = n_cores)" \
 > /dev/null 2>&1; echo "Base model MCMC complete")
-
-# Change a filename so that it works with hake-assessment code.
-# R4ss breaks if this is not done.
-mv ~/hake-assessment/$MODELS_DIR/$BASE_MODEL/data_echo.ss_new \
-~/hake-assessment/$MODELS_DIR/$BASE_MODEL/data.ss_new
 
 # Run the base models catch-level calculations
 # Environment variable $BASE_MODEL is set in R/model-setup.R
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-build_rds('$BASE_MODEL', run_catch_levels = TRUE, build_file = FALSE)" \
+build_rds('$model_path', run_catch_levels = TRUE, build_file = FALSE)" \
 > /dev/null 2>&1; echo "Base model catch level calculations complete")
 
 # Delete all files except the forecast.ss files
-find ~/hake-assessment/$MODELS_DIR/$BASE_MODEL/catch-levels -type f \
+find ~/hake-assessment/$model_path/catch-levels -type f \
  ! -name 'forecast.ss' -delete
 
 # Run the base models forecasts
 # Environment variable $BASE_MODEL is set in R/model-setup.R
 (trap 'kill 0' SIGINT; Rscript -e "setwd(here::here()); source('R/all.R'); \
-build_rds('$BASE_MODEL', run_catch_levels = FALSE, run_forecasts = TRUE, build_file = FALSE)" \
+build_rds('model_name', run_catch_levels = FALSE, run_forecasts = TRUE, build_file = FALSE)" \
 > /dev/null 2>&1; echo "Base model forecasts complete")
 
 # Delete the unnecessary files. Some are huge (eg. echoinput.sso can 3GB)
-find ~/hake-assessment/$MODELS_DIR/$BASE_MODEL/forecasts -type f \
+find ~/hake-assessment/$model_path/forecasts -type f \
  ! \( -name 'posteriors.sso' -o -name 'derived_posteriors.sso' -o -name 'forecast.ss' \) -delete
 
-# Copy all the output for the base model to the persistent S3 drive 'hakestore'
-rm -rf ~/hake-assessment/hakestore/$MODELS_DIR/$BASE_MODEL
-cp -R ~/hake-assessment/$MODELS_DIR/$BASE_MODEL \
-~/hake-assessment/hakestore/$MODELS_DIR/
-echo "Copied Base model output to S3 storage"
