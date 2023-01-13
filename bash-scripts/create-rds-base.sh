3407dad72a577c949c4f595bb3a4df54f43b1e25
Chris Grandin <chris.grandin@gmail.com>
2023-01-13 09:10:07 -0800

    Add bash script to create RDS files

---
+++ bash-scripts/create-rds-base.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Build RDS file for Brgie models
+#
+models=(01-base)
+
+#project  _path=`Rscript -e "cat(dirname(here::here()))"`
+project_path="/srv/hake"
+type_path="01-base-models"
+
+. ./generic-create-rds.sh
---
+++ bash-scripts/generic-create-rds.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script must be called from another script where the following variables have been set
+[[ -z $project_path ]] && { echo "Variable 'project_path' has not been set, bailing out." ; exit 1; }
+[[ -z $type_path ]] && { echo "Variable 'type_path' has not been set, bailing out." ; exit 1; }
+[[ -z $models ]] &&{ echo "Variable 'models' has not been set, bailing out." ; exit 1; }
+
+repo_path=`Rscript -e "cat(here::here())"`
+models_path="models"
+year_path=2023
+version_path="01-version"
+
+models_path=$project_path/$models_path/$year_path/$version_path/$type_path
+for model in ${models[@]}; do
+  (trap 'kill 0' SIGINT; \
+  model_path=$models_path/$model; \
+  echo "Building RDS for $model in a subshell"; \
+  echo; \
+  Rscript -e " \
+  setwd('$repo_path'); \
+  source('R/all.R'); \
+  create_rds_file('$model_path')" > /dev/null 2>&1; \
+  echo; \
+  echo "$model RDS complete") &
+done
