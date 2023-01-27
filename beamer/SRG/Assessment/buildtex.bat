5a1b3fd5777dd2aec5b906b955fb5cf9486d504a
Chris Grandin <chris.grandin@gmail.com>
2023-01-27 11:05:15 -0800

    Start assessment presentation

--- beamer/SRG/Assessment/buildtex.bat
+++
@@ -1,3 +0,0 @@
-Rscript -e "library(knitr);knit('./beamer-hake-assessment.rnw')" 1> knitrOutput.log 2>&1
-
-(@latex -synctex=1 "beamer-hake-assessment.tex" && latex "beamer-hake-assessment.tex" && latex "beamer-hake-assessment.tex" && dvips "beamer-hake-assessment.dvi" && ps2pdf "beamer-hake-assessment.ps") 1> latexOutput.log 2>&1
