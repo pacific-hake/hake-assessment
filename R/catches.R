0148ac6811055450549b2f753582d844b7b8db93
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2023-01-27 08:47:58 -0800

    Fix Table a by not making it a longtable (can combing make_catch_table_es() and make_catch_table() later).

--- R/catches.R
+++ R/catches.R
@@ -152,6 +152,130 @@ make_catch_table <- function(ct,
         hline.after = NULL)
 }
 
+#' Make summary of catch for given country for Executive Summary (can combine
+#' with above one at some point, just need the options)
+#'
+#' @param ct Output from [load_catches()]
+#' @param country 1 for Canada, 2 for US, anything else for both
+#' @param start_yr Start year in table
+#' @param end_yr End year in table
+#' @param xcaption Latex caption
+#' @param xlabel Latex label
+#' @param font_size Font size
+#' @param space_size Between-letter space size
+#' @param table_placement Latex for where to put table (H, tbp, etc.)
+#' @param tabular_environment longtable or tabular
+#' @return An [xtable::xtable()]
+#' @export
+make_catch_table_es <- function(ct,
+                             country = 3,
+                             start_yr,
+                             end_yr,
+                             xcaption = "default",
+                             xlabel   = "default",
+                             font_size = 9,
+                             space_size = 10,
+                             table_placement = "H",
+                             tabular_environment = "longtable",
+                             hline_after = NULL){
+
+  ct <- as_tibble(ct)
+  yrs <- ct$year
+  start_yr <- ifelse(start_yr < min(yrs), min(yrs), start_yr)
+  end_yr <- ifelse(end_yr > max(yrs) | end_yr < start_yr, max(yrs), end_yr)
+  yrs <- start_yr:end_yr
+
+  if(tabular_environment == "tabular"){
+    hline_after <- length(yrs)
+  }
+
+  if(country == 1){
+    ct <- ct %>%
+      select(year, can_foreign_xx, can_jv_xx, can_shore_xx, can_freeze_xx, can_catch)
+    ct_names <- c(latex.bold("Year"),
+                  latex.bold("Foreign"),
+                  latex.bold("JV"),
+                  latex.bold("Shoreside"),
+                  latex.bold("Freezer-Trawler"),
+                  latex.bold("Total"))
+  }else if(country == 2){
+    ct <- ct %>%
+      select(year, us_foreign_xx, us_jv_xx, us_ms_xx, us_cp_xx, us_shore_xx, us_research_xx, us_catch)
+    ct_names <- c(latex.bold("Year"),
+                  latex.bold("Foreign"),
+                  latex.bold("JV"),
+                  latex.bold("Mothership"),
+                  latex.bold("Catcher-Processor"),
+                  latex.bold("Shore-based"),
+                  latex.bold("Research"),
+                  latex.bold("Total"))
+  }else{
+    ct <- ct %>%
+      select(year, us_ms_xx, us_cp_xx, us_shore_xx, us_research_xx, us_catch,
+             can_jv_xx, can_shore_xx, can_freeze_xx, can_catch, tot_catch)
+    ct_names <- c(latex.bold("Year"),
+                  latex.mlc(c("US",
+                              "Mother-",
+                              "ship")),
+                  latex.mlc(c("US",
+                              "Catcher-",
+                              "Processor")),
+                  latex.mlc(c("US",
+                              "Shore-",
+                              "Based")),
+                  latex.mlc(c("US",
+                              "Research")),
+                  latex.mlc(c("US",
+                              "Total")),
+                  latex.mlc(c("CAN",
+                              "Joint-",
+                              "Venture")),
+                  latex.mlc(c("CAN",
+                              "Shoreside")),
+                  latex.mlc(c("CAN",
+                              "Freezer-",
+                              "Trawler")),
+                  latex.mlc(c("CAN",
+                              "Total")),
+                  latex.bold("Total"))
+  }
+
+  ct <- ct %>%
+    filter(year %in% yrs) %>%
+    mutate(year = as.character(year)) %>%
+    mutate_at(.vars = vars(-year), ~{f(.x, 0)})
+
+  addtorow <- list()
+  addtorow$pos <- list()
+  addtorow$pos[[1]] <- -1
+
+  addtorow$command <-
+    paste0(latex.hline,
+           paste(ct_names, collapse = latex.amp()),
+           latex.nline,
+           latex.hline)
+
+  if(tabular_environment == "longtable"){
+    addtorow$command <- paste0(addtorow$command,
+                               latex_continue(ncol(ct), addtorow$command))
+  }
+
+  size_string <- latex.size.str(font_size, space_size)
+  print(xtable(ct,
+               caption = xcaption,
+               label = xlabel,
+               align = get.align(ncol(ct))),
+        caption.placement = "top",
+        include.rownames = FALSE,
+        include.colnames = FALSE,
+        add.to.row = addtorow,
+        table.placement = table_placement,
+        tabular.environment = tabular_environment,
+        sanitize.text.function = function(x){x},
+        size = size_string,
+        hline.after = hline_after)
+}
+
 #' Make a summary table of landings and TACS
 #'
 #' @param d Data frame as output from [load_catches()]
