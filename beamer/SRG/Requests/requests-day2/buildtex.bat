8520909187465bfc3d88d9764d77c41c466045d6
Chris Grandin <chris.grandin@gmail.com>
2023-01-30 01:03:30 -0800

    Request presentation template made to work for this year * Removed old documents from 2022

--- beamer/SRG/Requests/requests-day2/buildtex.bat
+++
@@ -1,3 +0,0 @@
-Rscript -e "library(knitr);knit('./beamer-hake-requests-day2.rnw')" 1> knitrOutput.log 2>&1
-
-(@latex -synctex=1 "beamer-hake-requests-day2.tex" && latex "beamer-hake-requests-day2.tex" && latex "beamer-hake-requests-day2.tex" && dvips "beamer-hake-requests-day2.dvi" && ps2pdf "beamer-hake-requests-day2.ps") 1> latexOutput.log 2>&1
