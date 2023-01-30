d804a183e3ea95005ea5e683265a64deaf110064
Chris Grandin <chris.grandin@gmail.com>
2023-01-30 00:39:11 -0800

    Management presentation compiles * Added clean script

--- beamer/SRG/Management/buildtex.bat
+++
@@ -1,3 +0,0 @@
-Rscript -e "library(knitr);knit('./beamer-hake-management.rnw')" 1> knitrOutput.log 2>&1
-
-(@latex -synctex=1 "beamer-hake-management.tex" && latex "beamer-hake-management.tex" && latex "beamer-hake-management.tex" && dvips "beamer-hake-management.dvi" && ps2pdf "beamer-hake-management.ps") 1> latexOutput.log 2>&1
