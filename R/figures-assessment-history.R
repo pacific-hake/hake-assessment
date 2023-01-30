8ab446f02a33938e2700fa2b292658cfea4908e7
Aaron Berger <7946234+aaronmberger-nwfsc@users.noreply.github.com>
2023-01-30 14:12:43 -0800

    small tweak to assessment history figure

--- R/figures-assessment-history.R
+++ R/figures-assessment-history.R
@@ -155,7 +155,7 @@ make.assessment.history.disp.plot <- function(base,
   par(mfrow = c(1, 2), mar=c(4, 4, 1, 1), oma = c(1,1, 0, 0))
 
   plot(xx$InterQuartileRange~xx$Assessment_Year,type="o",ylim=c(0,2000),
-     ylab="Interquartile Range of Spawning Biomass Estimate ('000s)",
+     ylab="Interquartile Range of Spawning Biomass ('000s)",
      xlab="Assessment Year",
      cex.lab=0.85,
      col="black",
