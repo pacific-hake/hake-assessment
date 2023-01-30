09145d4036a4b55695a77c73cd1ced4493615124
Chris Grandin <chris.grandin@gmail.com>
2023-01-30 00:58:22 -0800

    Sensitivities presentation compiles * lots of missing slides * Removed all base/age 1 model comparisons from last year

--- beamer/SRG/Sensitivities/buildtex.bat
+++
@@ -1,3 +0,0 @@
-Rscript -e "library(knitr);knit('./beamer-hake-sensitivity.rnw')" 1> knitrOutput.log 2>&1
-
-(@latex -synctex=1 "beamer-hake-sensitivity.tex" && latex "beamer-hake-sensitivity.tex" && latex "beamer-hake-sensitivity.tex" && dvips "beamer-hake-sensitivity.dvi" && ps2pdf "beamer-hake-sensitivity.ps") 1> latexOutput.log 2>&1
