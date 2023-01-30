2aaeae20d6da618e5d7a89071b3de529f0635e95
Chris Grandin <chris.grandin@gmail.com>
2023-01-30 00:40:59 -0800

    Add clean script for Data presentation

--- beamer/SRG/Data/buildtex.bat
+++
@@ -1,3 +0,0 @@
-Rscript -e "library(knitr);knit('./beamer-hake-data.rnw')" 1> knitrOutput.log 2>&1
-
-(@latex -synctex=1 "beamer-hake-data.tex" && latex "beamer-hake-data.tex" && latex "beamer-hake-data.tex" && dvips "beamer-hake-data.dvi" && ps2pdf "beamer-hake-data.ps") 1> latexOutput.log 2>&1
