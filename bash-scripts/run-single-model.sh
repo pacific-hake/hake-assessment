0b82eb12714b1013ea49d2ac2039d070a8922cb4
Chris Grandin <chris.grandin@gmail.com>
2023-02-02 09:57:44 -0800

    Add run single model script

---
+++ bash-scripts/run-single-model.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Run a single model MCMC using ADNUTS
+# To fix the permissions for the hake group:
+# chmod -R u+rwx,g+rwx,o= 01-base-models
+
+# Start timer
+SECONDS=0
+
+repo_path=`Rscript -e "cat(here::here())"`
+# If running on a local machine and the model folder is in your
+# repo root, uncomment the next line and comment the line after it
+#models+path=$repo_path/models
+models_path="/srv/hake/models"
+year_path=2023
+version_path="01-version"
+type_path="05-test-models"
+model_name="01-model-name"
+
+num_chains=16
+num_samples=8000
+num_warmup_samples=250
+adapt_delta=0.95
+run_extra_mcmc=TRUE
+
+model_path=$models_path/$year_path/$version_path/$type_path/$model_name
+[[ ! -d $model_path ]] && { echo "Error: Directory $model_path does not exist, bailing out." ; exit 1; }
+
+(trap 'kill 0' SIGINT; Rscript -e "setwd('$repo_path'); source('R/all.R'); \
+run_adnuts_timed('$model_path', adapt_delta = $adapt_delta, run_extra_mcmc = $run_extra_mcmc, \
+                 num_chains = $num_chains, num_samples = $num_samples, \
+                 num_warmup_samples = $num_warmup_samples)" \
+  > /dev/null 2>&1; echo "Base model MCMC complete")
+
