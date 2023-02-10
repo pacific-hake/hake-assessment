1497b3e0607b7ad17c524d2dd27e2e54b104077c
Chris Grandin <chris.grandin@gmail.com>
2023-02-09 23:06:05 -0800

    Finish selectivity uncertainty plot * Can use any number of columns and it draws properly * Border lines around columns and panels can be shown or not * More fancy parameters

--- R/plot-multiple-tv-selex-unc.R
+++ R/plot-multiple-tv-selex-unc.R
@@ -2,8 +2,7 @@
 #'
 #' @details
 #' The panels show the medians and the credible interval as
-#' calculated in [load_extra_mcmc()]. The column borders only work
-#' right for `n_col` = 2 or 3
+#' calculated in [load_extra_mcmc()].
 #'
 #' @param model A model list, as created by [create_rds_file()]
 #' @param yr_lim A vector of two values representing the minimum and
@@ -13,6 +12,10 @@
 #' @param rev Logical. If `TRUE`, reverse the order of the years
 #' @param axis_title_font_size Size of the font for the X and Y axis labels
 #' @param axis_tick_font_size Size of the font for the X and Y axis tick labels
+#' @param border_width Thickness of the border line. If `NULL` no border will
+#' be shown
+#' @param border_color Color of the column borders
+#' @param border_linetype Line type of the column borders
 #' @param label_loc A vector of two (x, y) describing the label location inside
 #' the panels
 #' @param label_font_size The labels font size
@@ -24,10 +27,14 @@ plot_multiple_tv_selex_unc <- function(model,
                                        ages = 1:8,
                                        n_col = 1,
                                        rev = FALSE,
-                                       point_size = 2,
-                                       point_fatten = 1,
+                                       point_size = 1,
+                                       point_fatten = 2,
                                        axis_title_font_size = 14,
                                        axis_tick_font_size = 12,
+                                       border_width = 2,
+                                       border_color = "black",
+                                       border_linetype = "solid",
+                                       show_panel_borders = FALSE,
                                        label_loc = c(0.8, 0.8),
                                        label_font_size = 4){
 
@@ -81,15 +88,29 @@ plot_multiple_tv_selex_unc <- function(model,
                  lower = NA_real_,
                  upper = NA_real_)
 
-  brk <- length(yr_vec) %/% n_col
   if(length(yr_vec) %% n_col != 0){
-    brk <- brk + (n_col - length(yr_vec) %% n_col)
+    extras <- length(yr_vec) %% n_col
+    full_cols_len <- length(yr_vec) - extras
+    num_each_col <- full_cols_len / n_col
+    col_lengths <- rep(num_each_col, n_col)
+    for(i in 1:extras){
+      col_lengths[i] <- col_lengths[i] + 1
+    }
+    if(sum(col_lengths) != length(yr_vec)){
+      stop("Sanity check failed, column algorithm error",
+           call. = FALSE)
+    }
+  }else{
+    col_lengths <- rep(length(yr_vec) / n_col, n_col)
   }
 
-  yr_lst <- map(0:(n_col - 1), ~{
-    yr_vec[seq((.x * brk) + 1, ((.x + 1) * brk))]
+  start_of_col <- 1
+  yr_lst <- map(col_lengths, ~{
+    ret <- yr_vec[start_of_col:(start_of_col + .x - 1)]
+    start_of_col <<- start_of_col + .x
+    ret
   })
-browser()
+
   yr_vec <- map(seq_along(yr_lst[[1]]), function(yr_ind){
       map_dbl(yr_lst, function(lst_elem){
         lst_elem[yr_ind]
@@ -122,41 +143,48 @@ browser()
                                       size = axis_title_font_size),
           axis.title.y = element_text(color = "grey20",
                                       size = axis_title_font_size),
-          axis.ticks.length = unit(0.15, "cm"),
-          panel.border = element_blank()) +
+          axis.ticks.length = unit(0.15, "cm")) +
     ylab("Selectivity by year") +
     xlab("Age")
 
-  gr <- ggplotGrob(g)
-
-  # Add basic black rectangle round the columns
-  g_lst <- map(seq_len(n_col), ~{
-    rectGrob(gp = gpar(col = "black",
-                       lwd = 3,
-                       fill = NA))
-  })
-
-  yr_col_lengths <- yr_lst |> map_dbl(~{length(!.x[!is.na(.x)])})
-  t_extent <- 7
-  l_extent <- map_dbl(seq_len(n_col), ~{
-    5 + (.x - 1) * 4
-  })
-
-  bot <- gr$layout |> filter(name == "ylab-l") |> pull(b)
-  b_extent <- bot
-  if(n_col > 1){
-    tmp <- map_dbl(seq_len(n_col - 1), ~{
-      bot - ifelse(yr_col_lengths[.x + 1] < yr_col_lengths[1], 1, 0)
-    })
-    b_extent <- c(b_extent, tmp)
+  if(!show_panel_borders){
+    g <- g +
+      theme(panel.border = element_blank())
   }
-  gt <- gtable::gtable_add_grob(gr,
-                                grobs = g_lst,
-                                t = 7,
-                                b = b_extent,
-                                l = l_extent)
-  grid.newpage()
-  grid.draw(gt)
+  if(!is.null(border_width)){
+    gr <- ggplotGrob(g)
 
+    # Add basic black rectangle round the columns
+    g_lst <- map(seq_len(n_col), ~{
+      rectGrob(gp = gpar(col = border_color,
+                         lty = border_linetype,
+                         lwd = border_width,
+                         fill = NA))
+    })
+
+    yr_col_lengths <- yr_lst |> map_dbl(~{length(!.x[!is.na(.x)])})
+    t_extent <- 7
+    l_extent <- map_dbl(seq_len(n_col), ~{
+      5 + (.x - 1) * 4
+    })
+
+    bot <- gr$layout |> filter(name == "ylab-l") |> pull(b)
+    b_extent <- bot
+    if(n_col > 1){
+      tmp <- map_dbl(seq_len(n_col - 1), ~{
+        bot - ifelse(yr_col_lengths[.x + 1] < yr_col_lengths[1], 1, 0)
+      })
+      b_extent <- c(b_extent, tmp)
+    }
+    gt <- gtable::gtable_add_grob(gr,
+                                  grobs = g_lst,
+                                  t = 7,
+                                  b = b_extent,
+                                  l = l_extent)
+    grid.newpage()
+    return(grid.draw(gt))
+  }
+
+  g
 }
 
