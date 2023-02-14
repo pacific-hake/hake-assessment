10b6c99f599ae80caf0d2a89396fe7d6d38cea59
Chris Grandin <chris.grandin@gmail.com>
2023-02-13 21:12:55 -0800

    Fix end year in retrospective models so that the parameter estimation table works right

--- R/load-retrospectives.R
+++ R/load-retrospectives.R
@@ -3,6 +3,9 @@
 #'
 #' @param retro_path The path in which the retrospective directories reside
 #' @param retrospective_yrs A vector of years for the retrospectives
+#' @param end_yr The end year of the model. Retrospectives will have
+#' their `endyr` element set to this minus the numbers of years of the
+#' retorspective
 #' @return The list of retrospective outputs
 #' @export
 load_retrospectives <- function(retro_path,
@@ -11,12 +14,13 @@ load_retrospectives <- function(retro_path,
 
   message("\nLoading retrospectives from ", retro_path)
   plan("multisession", workers = length(retro_yrs))
-  retros_list <- future_map(retro_yrs, function(x, ...){
+  retros_list <- future_imap(retro_yrs, function(x, y, ...){
     retro_sub <- paste0("retro-", pad.num(x, 2))
     retro_dir <- file.path(retro_path, retro_sub)
     message("Loading from ", retro_dir)
     model <- load_ss_files(retro_dir, ...)
     model$extra.mcmc <- load_extra_mcmc(model, small = TRUE, ...)
+    model$endyr <- model$endyr - y
     model
   }, ...)
   plan()
