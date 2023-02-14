0f2bcc43921b6961dc3908d3323d2cddf077869a
Chris Grandin <chris.grandin@gmail.com>
2023-02-14 10:54:54 -0800

    JMC-US building

--- beamer/JMC/presentation-us/buildtex.bat
+++
@@ -1,3 +0,0 @@
-Rscript -e "library(knitr);knit('./beamer-hake-JMC-Canada.rnw')" 1> knitrOutput.log 2>&1
-
-(@latex -synctex=1 "beamer-hake-JMC-Canada.tex" && latex "beamer-hake-JMC-Canada.tex" && latex "beamer-hake-JMC-Canada.tex" && dvips "beamer-hake-JMC-Canada.dvi" && ps2pdf "beamer-hake-JMC-Canada.ps") 1> latexOutput.log 2>&1
