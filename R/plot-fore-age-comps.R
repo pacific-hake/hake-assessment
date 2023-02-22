b9921266a80dbc1a206c002b48ef17865451250d
Chris Grandin <chris.grandin@gmail.com>
2023-02-22 02:25:21 -0800

    Replace age comps forecast plot with new code

---
+++ R/plot-fore-age-comps.R
@@ -0,0 +1,63 @@
+#' Create a two-panel plot showing the forecasted age compositions by
+#' numbers and weight for the first forecast year (`endyr` + 1)
+#'
+#' @param model  A model object, created by [create_rds_file()]
+#' @param x_lim A vector of 2 representing the minimum and maximum x-axis
+#' values to plot
+#' @param bar_alpha The transparency of the bars
+#'
+#' @return A [cowplot::plot_grid()] object
+#' @importFrom cowplot plot_grid
+#' @export
+plot_fore_age_comps <- function(model,
+                                x_lim = c(1, 15),
+                                x_breaks = seq(x_lim[1], x_lim[2], 2),
+                                bar_alpha = 0.3){
+
+  natsel_prop <- model$extra.mcmc$natsel.prop
+  natselwt_prop <- model$extra.mcmc$natselwt.prop
+
+  reformat <- function(df){
+    df |>
+      apply(2, quantile, probs = c(0.025, 0.5, 0.975)) |>
+      as_tibble(rownames = "quant") |>
+      mutate(quant = ifelse(quant == "2.5%",
+                            "lower",
+                            ifelse(quant == "97.5%",
+                                   "upper",
+                                   "med"))) |>
+      pivot_longer(-quant, names_to = "age", values_to = "prop") |>
+      pivot_wider(names_from = "quant", values_from = "prop") |>
+      #mutate(age = factor(age, levels = age))
+      mutate(age = as.numeric(age))
+  }
+
+  by_nums <- reformat(natsel_prop)
+  by_weight <- reformat(natselwt_prop)
+
+  plist <- NULL
+  plist[[1]] <- ggplot(by_nums,
+                       aes(x = age, y = med)) +
+    geom_bar(stat = "identity", alpha = bar_alpha) +
+    geom_errorbar(aes(ymin = lower, ymax = upper),
+                  width = 0.5) +
+    geom_point() +
+    scale_x_continuous(breaks = x_breaks, labels = x_breaks) +
+    scale_y_continuous(expand = c(0, 0)) +
+    coord_cartesian(xlim = x_lim, expand = FALSE) +
+    xlab("Age") +
+    ylab(paste0("Expected proportion in ",
+                model$endyr + 1,
+                " catch"))
+  plist[[2]] <- ggplot(by_weight,
+                       aes(x = age, y = med)) +
+    geom_bar(stat = "identity", alpha = bar_alpha) +
+    geom_errorbar(aes(ymin = lower, ymax = upper),
+                  width = 0.5) +
+    geom_point() +
+    scale_x_continuous(breaks = x_breaks, labels = x_breaks) +
+    scale_y_continuous(expand = c(0, 0)) +
+    coord_cartesian(xlim = x_lim)
+
+  plot_grid(plotlist = plist, nrow = 1)
+}
