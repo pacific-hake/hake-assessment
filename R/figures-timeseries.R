57c184352b37d7b8e18325826de9dff7de2ff7ec
Chris Grandin <chris.grandin@gmail.com>
2023-03-20 03:06:59 -0700

    Add exploitation fraction plot

--- R/figures-timeseries.R
+++ R/figures-timeseries.R
@@ -32,37 +32,6 @@ make.fishing.intensity.plot <- function(model,            ## model is an mcmc ru
   par <- oldpar
 }
 
-make.exploitation.fraction.plot <- function(model,            ## model is an mcmc run and is the output of the r4ss package's function SSgetMCMC
-                                            start_yr,         ## Year the timeseries starts (i.e. first year in model)
-                                            end_yr,           ## Year the timeseries ends (i.e. last year that values are required for; note that SS calculates an exploitation rate for the final year of the model, but the number is meaningless because that year has no catch).
-                                            color = "blue",
-                                            upper.lim = 0.4){
-  ## Plots the exploitation fraction for the mcmc given by model
-  oldpar <- par()
-
-  flower <- model$mcmccalcs$flower
-  fmed <- model$mcmccalcs$fmed
-  fupper <- model$mcmccalcs$fupper
-
-  yrs <- start_yr:end_yr
-
-  ## Only include start year to end year
-  flower <- flower[(names(flower) %in% yrs)]
-  fmed <- fmed[(names(fmed) %in% yrs)]
-  fupper <- fupper[(names(fupper) %in% yrs)]
-
-  y <- data.frame(value=fmed,lo=flower,hi=fupper)
-
-  par(mfrow = c(1,1), las = 1, mar = c(3.5,4.5,1,1), oma = c(0,1,0,0))
-  plotBars.fn(yrs, y, ylim = c(0,upper.lim), pch = 20, xlab = "Year", ylab = "",
-              cex = 0.8, las = 1, gap = 0.005, xaxt = "n", ciLwd = 1, ciCol = rgb(0,0,1,0.5),
-              mgp = c(2.3,1,0), xlim = range(yrs), yaxs="i")
-  axis(1, at = big_ticks)
-  axis(1, at = small_ticks, lab = rep("",length(small_ticks)), tcl = -0.3)
-  mtext("Exploitation fraction", side=2, line=3, las=0)
-  par <- oldpar
-}
-
 make.phase.plot <- function(model,            ## model is an mcmc run and is the output of the r4ss package's function SSgetMCMC
                             start_yr,         ## Year the timeseries starts (i.e. first year in model)
                             end_yr,           ## Year the timeseries ends (i.e. last year in model)
