b8dacbe5c67f80a4b25612ad3d423141d89b9cb7
Chris Grandin <chris.grandin@gmail.com>
2023-03-25 21:25:27 -0700

    Make build_doc() work using new method * Works for lualatex

--- R/build-doc.R
+++ R/build-doc.R
@@ -1,108 +1,47 @@
-#' Build the assessment document entirely from within an R session
+#' Build the Hake assessment document
 #'
-#' @details Make sure you have created the .rds files by running [build_rds()] in the appropriate manner.
-#' Once you have done that and run this function once within an R session, it will be a little quicker since the RDS
-#' file contents will have already been loaded into the R session.
+#' @param tag Logical. If `TRUE`, add tagging commands to the
+#' tex file so that a tagged, web-accessible document is built.
+#' If `TRUE`, it takes longer for the `lualatex` step so typically this
+#' is only use for the final build (or testing along the way)
 #'
-#' @param doc_name What to name the document (no extension)
-#' @param doc_dir Directory the main document RNW file resides in
-#' @param alt_text The knitting process creates the global `alt_fig_text`, but only if the figures were not cached.
-#' This must only be set to `TRUE` when building from scratch, without any cached figures
-#' @param knit_only Only knit the code, do not run latex engine.
-#' @param make_pdf Logical. `TRUE` to make the pdf, if `FALSE` it will only go as far as postscript. If `png_figs`
-#' is set to `TRUE`, this argument will have no effect, a PDF will be built anyway
-#' @param make_bib Logical. Run bibtex
-#' @param ... Arguments to pass to [add_alt_text()]
-#'
-#' @return [base::invisible()]
+#' @return Nothing
 #' @export
-build_doc <- function(doc_name = "hake-assessment",
-                      doc_dir = here::here("doc"),
-                      alt_text = FALSE,
-                      knit_only = FALSE,
-                      make_pdf = TRUE,
-                      make_bib = TRUE,
-                      ...){
+build_doc <- function(tag = FALSE, ...){
 
-  curr_dir <- getwd()
-  on.exit(setwd(curr_dir))
-  setwd(doc_dir)
-
-  latex_command <- "lualatex"
-
-  knit(paste0(doc_name, ".rnw"))
-  # The knitting process creates the global `alt_fig_text`, but only if the figures were not cached
-  if(alt_text){
-    add_alt_text(paste0(doc_name, ".tex"), alt_fig_text, ...)
-  }
-
-  if(!knit_only){
-    system(paste0(latex_command, " ", doc_name, ".tex"), wait = TRUE)
-    system(paste0(latex_command, " ", doc_name, ".tex"), wait = TRUE)
-    if(make_bib){
-      system(paste0("bibtex ", doc_name), wait = TRUE)
-    }
-    system(paste0(latex_command, " ", doc_name, ".tex"), wait = TRUE)
-    system(paste0(latex_command, " ", doc_name, ".tex"), wait = TRUE)
-  }
-  invisible()
-}
-
-#' Build a pared-down version of the assessment. Typically used to compile figures section or
-#' tables section only. Citations and other references will not be compiled properly since
-#' the latex engine, e.g., `lualatex`, is only called once
-#'
-#' @param doc_name What to name the document (no extension needed)
-#'
-#' @return [base::invisible()]
-#' @export
-build_test <- function(doc_name = "hake-assessment-test"){
-
-  curr_dir <- getwd()
-  on.exit(setwd(curr_dir))
   setwd(here::here("doc"))
-  knit(paste0(doc_name, ".rnw"))
-  # The knitting process creates the global `alt_fig_text`
-  #add_alt_text(paste0(doc_name, ".tex"), alt_fig_text)
-  system(paste0("lualatex ", doc_name, ".tex"), wait = TRUE)
-  invisible()
-}
 
-#' Build the executive summary section only
-#'
-#' @return [base::invisible()]
-#' @export
-build_es <- function(){
-  build_test(doc_name = "hake-assessment-es-only")
-}
-
-#' Build the ADNUT diagnostics document entirely from within an R session
-#'
-#' @details Make sure you have created the .rds files by running [build_rds()] in the appropriate manner.
-#' Once you have done that and run this function once within an R session, it will be a little quicker since the RDS
-#' file contents will have already been loaded into the R session. Only has the
-#' PNG option since EPS files produce a huge final PDF; no need for bibtex
-#'   since no references. If those are needed then just adapt `build_doc` again.
-#'
-#' @param knit_only Only knit the code, do not run latex engine
-#' @param doc_name What to name the document (no extension)
-#'
-#' @return [base::invisible()]
-#' @export
-build_adnuts_doc <- function(knit_only = FALSE,
-                             doc_name = "adnuts-diagnostics"){
-
-  latex_command <- "lualatex"
-  curr_path <- getwd()
-  setwd(here::here("doc-adnuts-diagnostics"))
-
-  knit(paste0(doc_name, ".rnw"))
-
-  if(!knit_only){
-    system(paste0(latex_command, " ", doc_name, ".tex"), wait = TRUE)
-    system(paste0(latex_command, " ", doc_name, ".tex"), wait = TRUE)
-    # system(paste0(latex_command, " ", doc_name, ".tex"), wait = TRUE)  # two should be enough
+  bookdown::render_book("000-launcher.rmd",
+                        output_dir = here::here("doc"),
+                        output_format = "bookdown::pdf_document2",
+                        envir = parent.frame(),
+                        ...)
+  # Get filename from _bookdown file
+  if(!file.exists("_bookdown.yml")){
+    stop("File `_bookdown.yml` does not exist in the doc directory",
+         call. = FALSE)
   }
-  setwd(curr_path)
-  invisible()
+
+  # Construct tex filename
+  bd <- readLines("_bookdown.yml")
+  pat <- '^ *book_filename: +\\"(\\w+)\\"'
+  gr <- grep(pat, bd)
+  fn <- gsub('^ *book_filename: +\\"(\\w+)\\"', "\\1", bd[gr])
+  fn <- paste0(fn, ".tex")
+  if(!file.exists(fn)){
+    stop("File `", fn, "` does not exist. This filename was built using the ",
+         "value for `book_filename:` in the `_bookdown.yml` file with ",
+         "`.tex` appended",
+         call. = FALSE)
+  }
+  # Add table of contents, center section headers, modify figure placements
+  # and many other things to make the document presentable
+  post_process(fn, accessibility = tag)
+  # Run lualatex twice to ensure references are populated correctly
+  system(paste0("lualatex -interaction=nonstopmode ", fn),
+         intern = TRUE,
+         wait = TRUE)
+  system(paste0("lualatex -interaction=nonstopmode ", fn),
+         intern = TRUE,
+         wait = TRUE)
 }
