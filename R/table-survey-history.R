791db4506f72f4e111132bc0ed03ba75ce0b7f40
Chris Grandin <chris.grandin@gmail.com>
2023-04-11 22:29:36 -0700

    Add table for survey by country

--- R/table-survey-history.R
+++ R/table-survey-history.R
@@ -9,10 +9,14 @@
 #' @param d A data frame containing the survey history as read in from
 #' the file `survey-history.csv`
 #' @param digits Number of decimal places
-#' @param xcaption Caption to appear in the calling document
-#' @param xlabel The label used to reference the table in latex
-#' @param font.size Size of the font for the table
-#' @param space.size Size of the vertical spaces for the table
+#' @param font_size The table data and header font size in points
+#' @param header_font_size The font size for the headers only. If `NULL`,
+#' the headers will have the same font size as the table cell data
+#' @param vert_spacing The vertical spacing between newlines for this font.
+#' If `NULL` this will be calculated as `header_font_size * header_vert_scale`
+#' @param header_vert_scale Scale factor to create the vertical spacing value.
+#' See `header_vert_spacing`
+#' @param ... Arguments passed to [knitr::kable()]
 #'
 #' @return A [knitr::kable()] object
 #' @export
