be241ff558eff094add9314019a1d97feeb5177e
Chris Grandin <chris.grandin@gmail.com>
2023-04-20 10:03:11 -0700

    Update overview map

--- R/plot-overview-map.R
+++ R/plot-overview-map.R
@@ -1,64 +1,88 @@
+#' Create a map of the west coast of North America showing places of interest
+#'
+#' @param ports_df A data frame as read in by [readr::read_csv()]
+#' @param states_df A data frame as read in by [readr::read_csv()]
+#' @param crs Coordinate Reference System (CRS) number. Default is 4326 which
+#' is WGS84: See [Epsg.org](https://epsg.org/home.html) for details. Click
+#' `Text search` tab and look at the `code` column for valid crs numbers
+#' @param extents The extents of the map as a two-column data frame with
+#' columns `latitude` and `longitude`. Change this to make the map larger
+#' or smaller, i.e. to zoom map in/out
+#'
+#' @return A [ggplot2::ggplot()] object
+#' @export
+plot_overview_map <- function(
+    ports_df,
+    states_df,
+    crs = 4326,
+    extents = tibble(lon = c(-140, -113),
+                     lat = c(33, 58))){
 
-make.overview.map.plot <- function(path,
-                                   crs = 4326,
-                                   extents = data.frame(lon = c(-140, -113),
-                                                        lat = c(33, 58)),
-                                   contour_depths = c(100, 200, 400, 1000, 1500, 2000),
-                                   contour_color = "lightblue",
-                                   contour_thickness = 0.25){
 
-  ports_file <- file.path(path, "port-locations.csv")
-  states_file <- file.path(path, "state-locations.csv")
-
-  coast <- ne_states(country = c("United States of America", "Canada", "Mexico"),
+  coast <- ne_states(country = c("United States of America",
+                                 "Canada",
+                                 "Mexico"),
                      returnclass = "sf")
 
-  ports <- read_csv(ports_file) %>%
+  ports <- ports_df |>
     mutate(lon = -abs(lon))
-  ports_left_north <- ports %>%
+  ports_left_north <- ports |>
     filter(pos == "left",
            north_south == "north")
-  ports_left_south <- ports %>%
+  ports_left_south <- ports |>
     filter(pos == "left",
            north_south == "south")
-  ports_right_north <- ports %>%
+  ports_right_north <- ports |>
     filter(pos == "right",
            north_south == "north")
-  ports_right_south <- ports %>%
+  ports_right_south <- ports |>
     filter(pos == "right",
            north_south == "south")
 
-  ports_left_north_sf <- ports_left_north %>%
-    st_as_sf(coords = c("lon", "lat"), crs = crs) %>%
+  ports_left_north_sf <- ports_left_north |>
+    st_as_sf(coords = c("lon", "lat"), crs = crs) |>
     `st_crs<-`(crs)
-  ports_left_south_sf <- ports_left_south %>%
-    st_as_sf(coords = c("lon", "lat"), crs = crs) %>%
+  ports_left_south_sf <- ports_left_south |>
+    st_as_sf(coords = c("lon", "lat"), crs = crs) |>
     `st_crs<-`(crs)
-  ports_right_north_sf <- ports_right_north %>%
-    st_as_sf(coords = c("lon", "lat"), crs = crs) %>%
+  ports_right_north_sf <- ports_right_north |>
+    st_as_sf(coords = c("lon", "lat"), crs = crs) |>
     `st_crs<-`(crs)
-  ports_right_south_sf <- ports_right_south %>%
-    st_as_sf(coords = c("lon", "lat"), crs = crs) %>%
+  ports_right_south_sf <- ports_right_south |>
+    st_as_sf(coords = c("lon", "lat"), crs = crs) |>
     `st_crs<-`(crs)
 
-  states <- read_csv(states_file) %>%
+  states <- states_df |>
     mutate(lon = -abs(lon))
-  se_alaska <- states %>%
+  se_alaska <- states |>
     filter(name == "Southeast Alaska")
-  states <- states %>%
+  states <- states |>
     filter(name != "Southeast Alaska")
 
-  extents <- st_as_sf(extents, coords = c("lon", "lat")) %>%
-    `st_crs<-`(crs) %>%
-    st_coordinates() %>%
+  extents <- st_as_sf(extents, coords = c("lon", "lat")) |>
+    `st_crs<-`(crs) |>
+    st_coordinates() |>
     as_tibble()
 
   g <- ggplot(data = coast) +
-    geom_sf(color = "black", fill = "grey") +
-    geom_sf(data = ports_left_north_sf, color = "red", size = 2,  show.legend = FALSE) +
-    geom_sf(data = ports_left_south_sf, color = "red", size = 2,  show.legend = FALSE) +
-    geom_sf(data = ports_right_north_sf, color = "red", size = 2,  show.legend = FALSE) +
-    geom_sf(data = ports_right_south_sf, color = "red", size = 2,  show.legend = FALSE) +
+    geom_sf(color = "black",
+            fill = "grey") +
+    geom_sf(data = ports_left_north_sf,
+            color = "red",
+            size = 2,
+            show.legend = FALSE) +
+    geom_sf(data = ports_left_south_sf,
+            color = "red",
+            size = 2,
+            show.legend = FALSE) +
+    geom_sf(data = ports_right_north_sf,
+            color = "red",
+            size = 2,
+            show.legend = FALSE) +
+    geom_sf(data = ports_right_south_sf,
+            color = "red",
+            size = 2,
+            show.legend = FALSE) +
     geom_text_repel(data = states,
                      aes(x = lon,
                          y = lat,
@@ -110,5 +134,6 @@ make.overview.map.plot <- function(path,
              ylim = extents[[2]]) +
     ylab(paste0("Latitude (", intToUtf8(176), ")")) +
     xlab(paste0("Longitude (", intToUtf8(176), ")"))
+
     g
 }
