25b1332f7c8a4365fb218fba85eff5010f61ff42
Chris Grandin <chris.grandin@gmail.com>
2023-05-03 01:35:20 -0700

    Add bind_cols_quiet()

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,14 @@
+#' Wrapper for [dplyr::bind_cols()] but silences the New names... messages
+#' that appear when binding
+#'
+#' @param ... Arguments passed to [dplyr::bind_cols()]
+#'
+#' @return The output data frame from [dplyr::bind_cols()]
+#' @export
+bind_cols_quiet <- function(...){
+  bind_cols(..., .name_repair = ~vec_as_names(..., quiet = TRUE))
+}
+
 #' Calculate the standard deviation of recruitment deviations in an MCMC
 #' data frame
 #'
