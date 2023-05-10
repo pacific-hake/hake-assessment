3da6024f0a82102b300326aa8451485c777e1542
Chris Grandin <chris.grandin@gmail.com>
2023-05-10 00:44:20 -0700

    Less plots made during building RDS process

--- R/plot-during-loading.R
+++ R/plot-during-loading.R
@@ -1,15 +1,9 @@
-#' Create plots suring the loading phase, and return a list of them to be
+#' Create plots during the loading phase, and return a list of them to be
 #' attached to the model object for saving in a model RDS file
 #'
 #' @details
-#' Assumes that many global variables exist. While this is usually bad form,
-#' this one function is only to be called from within [create_rds_file()]
-#' at a point when all those variables exist. If they do not, the Rmd files
-#' doc/002- through 005-* must be sourced prior to calling this.
 #' Uses the [probs] vector which is included in the package data for
 #' this package
-#'
-
 #'
 #' @param model A model, created by [create_rds_file()]
 #'
@@ -30,178 +24,6 @@ plot_during_loading <- function(model){
                                              n_posts = 1000,
                                              ylim = c(0, 10))
 
-  out$overview_map <- plot_overview_map(ports_df, states_df)
-
-  out$catches <- plot_catches(ct, xlim = c(start_yr, last_data_yr))
-
-  out$us_depths <- plot_depth_2_panel(us_atsea_fishing_depth_df,
-                                      us_atsea_bottom_depth_df,
-                                      yrs = (last_data_yr - 4):last_data_yr)
-
-  out$can_depths <- plot_depth_4_panel(can_ft_gear_depth_df,
-                                       can_ft_bottom_depth_df,
-                                       can_ss_gear_depth_df,
-                                       can_ss_bottom_depth_df,
-                                       fleet_1_name = "Freezer trawlers",
-                                       fleet_2_name = "Shoreside",
-                                       yrs = (last_data_yr - 4):last_data_yr)
-
-  out$data_overview <- plot_data_summary(model)
-
-  out$age_comp_bubbles <-
-    {
-      p_lst <- NULL
-      p_lst[[1]] <- plot_age_comp_bubbles(model,
-                                          type = "fishery",
-                                          leg_pos = "top",
-                                          alpha = 0.4)
-      p_lst[[2]] <- plot_age_comp_bubbles(model,
-                                          type = "survey",
-                                          alpha = 0.4)
-      suppressWarnings(plot_grid(plotlist = p_lst, nrow = 2))
-    }
-
-  out$survey_extrap_biomass <-
-    plot_survey_biomass(model, index = "age2")
-
-  out$survey_age1 <- plot_survey_biomass(model,
-                                         index = "age1",
-                                         y_lim = c(0, 16))
-  out$maturity_ogive <-
-    {
-      p_lst <- list()
-      p_lst[[1]] <- plot_maturity_ogives(model, maturity_samples_df)
-      p_lst[[2]] <- plot_fecundity(model,
-                                   d = maturity_samples_df,
-                                   yrs = start_yr_age_comps:(end_yr - 1))
-      plot_grid(plotlist = p_lst, ncol = 1)
-    }
-
-  out$weight_at_age <- plot_heatmap_weight_at_age(
-    model,
-    cell_font_size = 3,
-    sample_size_df = weight_age_sample_sizes_df,
-    pre_yrs = start_yr_age_comps:model$endyr,
-    pre_func = mean,
-    post_yrs = (model$endyr - 4):model$endyr,
-    post_func = mean,
-    cols = c("red",
-             "yellow",
-             "green",
-             "dodgerblue"))
-
-  out$sample_size_weight_at_age <- plot_heatmap_sample_size_weight_at_age(
-    model,
-    cell_font_size = 3,
-    sample_size_df = weight_age_sample_sizes_df,
-    pre_yrs = start_yr_age_comps:model$endyr,
-    pre_func = mean,
-    post_yrs = (model$endyr - 4):model$endyr,
-    post_func = mean,
-    cols = c("purple",
-             "lightgreen",
-             "yellow",
-             "dodgerblue"))
-
-  out$weight_at_age_lines <- plot_weight_at_age(wt_at_age,
-                                                ages = 2:10,
-                                                bold_ages = 5,
-                                                cols = c("purple",
-                                                         "darkblue",
-                                                         "yellow",
-                                                         "darkgreen"))
-  out$bridge_summary <-
-    {
-      plist <- list()
-      plist[[1]] <-
-        plot_rel_biomass(d_obj = d_obj_bridge_rel_biomass[[1]],
-                         ylim = c(0, 2.5),
-                         x_labs_mod = 10,
-                         clip_cover = 0.35,
-                         wrap_y_label = TRUE,
-                         axis_title_font_size = 12,
-                         leg_pos = "none")
-      plist[[2]] <-
-        plot_recdevs(d_obj = d_obj_bridge_recdev[[1]],
-                     leg_pos = "none",
-                     x_labs_mod = 10,
-                     clip_cover = 0.25,
-                     axis_title_font_size = 12,
-                     line_width = 0.25)
-
-      plist[[3]] <-
-        plot_survey_index_fits(d_obj = d_obj_bridge_age2_index[[1]],
-                               survey_type = "age2",
-                               rev_colors = FALSE,
-                               clip_cover = 0.25,
-                               axis_title_font_size = 12,
-                               leg_pos = "none")
-
-      plist[[4]] <-
-        plot_survey_index_fits(d_obj = d_obj_bridge_age1_index[[1]],
-                               survey_type = "age1",
-                               xlim = c(1995, 2022),
-                               ylim = c(0, 11),
-                               y_breaks = seq(0, 11, by = 2),
-                               clip_cover = 0.25,
-                               axis_title_font_size = 12,
-                               rev_colors = FALSE,
-                               leg_pos = "none")
-
-      # Grid of 2 rows and 1 column with one plot at the top (biomass)
-      # and 4 plots embedded in another grid (quad_grid) at the bottom
-      quad_grid <- cowplot::plot_grid(plotlist = plist,
-                                      nrow = 2,
-                                      ncol = 2)
-      full_plist <- list()
-      full_plist[[1]] <-
-        plot_biomass(d_obj = d_obj_bridge_biomass[[1]],
-                     leg_ncol = 2,
-                     leg_font_size = 7,
-                     wrap_y_label = TRUE,
-                     axis_title_font_size = 12,
-                     leg_pos = c(0.72, 0.82))
-
-      full_plist[[2]] <- quad_grid
-
-      cowplot::plot_grid(plotlist = full_plist,
-                         nrow = 2,
-                         ncol = 1,
-                         rel_heights = c(0.33, 0.67))
-    }
-
-  out$age_comp_fits <-
-    {
-      plist <- NULL
-      plist[[1]] <-
-        plot_age_comp_fit(base_model,
-                          label_font_size = 3,
-                          n_col = 4)
-      plist[[2]] <-
-        plot_age_comp_fit(base_model,
-                          n_col = 1,
-                          type = "survey",
-                          x_breaks = 2:15,
-                          label_font_size = 3,
-                          label_loc = c(14, 0.45))
-      plot_grid(plotlist = plist, nrow = 2)
-    }
-
-  out$age_comp_pearson <-
-    {
-      plist <- NULL
-      plist[[1]] <-
-        plot_pearson_bubbles(base_model,
-                             type = "fishery",
-                             legend.position = "top",
-                             alpha = 0.7)
-      plist[[2]] <-
-        plot_pearson_bubbles(base_model,
-                             type = "survey",
-                             alpha = 0.7)
-      plot_grid(plotlist = plist, ncol = 1)
-    }
-
   # out$prior_posterior <- plot_priors_vs_posts(base_model,
   #                                             key_posteriors,
   #                                             titles = key_posteriors_titles,
@@ -209,7 +31,6 @@ plot_during_loading <- function(model){
   #                                             ncol = 2,
   #                                             nrow = 3,
   #                                             labeller = label_parsed_space)
-
 
   out
 }
