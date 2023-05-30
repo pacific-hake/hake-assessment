5c15446e461d0b39f5076af0c946f6ce3a030776
Chris Grandin <chris.grandin@gmail.com>
2023-05-30 15:20:51 -0700

    Add font size etc. to the hake theme

--- R/utils-theme.R
+++ R/utils-theme.R
@@ -2,11 +2,38 @@
 #'
 #' @export
 hake_theme <- function(){
+
+  axis_title_font_size <- 14
+  axis_tick_font_size <- 11
+  axis_label_color <- "black"
+
   theme_bw() +
     theme(legend.key = element_blank(),
           legend.text.align = 1,
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           legend.background = element_rect(fill = "transparent"),
-          plot.margin = unit(c(0.1, 0.6, 0.1, 0.1), "lines"))
+          plot.margin = margin(6, 6, 6, 6),
+
+          axis.text.x = element_text(color = axis_label_color,
+                                     size = axis_tick_font_size,
+                                     angle = 0,
+                                     hjust = 0.5,
+                                     vjust = -0.25,
+                                     face = "plain"),
+          axis.text.y = element_text(color = axis_label_color,
+                                     size = axis_tick_font_size,
+                                     hjust = 1,
+                                     vjust = 0.5,
+                                     face = "plain"),
+          axis.title.x = element_text(color = axis_label_color,
+                                      size = axis_title_font_size,
+                                      angle = 0,
+                                      vjust = 0,
+                                      face = "plain"),
+          axis.title.y = element_text(color = axis_label_color,
+                                      size = axis_title_font_size,
+                                      angle = 90,
+                                      face = "plain"),
+          axis.ticks.length = unit(0.15, "cm"))
 }
