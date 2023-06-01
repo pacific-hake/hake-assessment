d0354f64fbc60c18a2621609a4d7adf6ea2c8c17
Chris Grandin <chris.grandin@gmail.com>
2023-06-01 01:34:39 -0700

    Work on histograms for MCMC diagnostics

--- R/plot-mcmc-histogram.R
+++ R/plot-mcmc-histogram.R
@@ -13,6 +13,14 @@
 #' `barplot` is `TRUE`. If `NULL`, the data will be plotted as-is
 #' @param show_ro Logical. If `TRUE`, make a label with arrow showing which
 #' bin the R0 parameter is in
+#' @param ro_arrow_length The length in count units (y-axis units) of the
+#' arrow pointing to the bin that the R0 parameter is in. Only used if
+#' `show_ro` is `TRUE`
+#' @param bar_label_limit The number in a bin (count - y-axis), above which
+#' the label of the count for the bar is in white in the middle of the bar
+#' vertically. Below this value, the bar value will be above the bar. To
+#' have all value labels above the bars, set this to a value larger than the
+#' highest count bin
 #' @param x_lim A vector of two values. The min and max to show on the
 #' x-axis. If `NULL`, values will be calculated from the input values
 #' @param y_lim A vector of two values. The min and max to show on the
@@ -41,6 +49,9 @@ plot_mcmc_histogram <- function(d,
                                 lvls = NULL,
                                 scale_effn = 1e3,
                                 show_ro = FALSE,
+                                ro_arrow_length = 20,
+                                bar_label_limit = 10,
+                                bar_text_color = "navyblue",
                                 x_lim = NULL,
                                 y_lim = NULL,
                                 x_breaks = NULL,
@@ -61,10 +72,6 @@ plot_mcmc_histogram <- function(d,
   d <- d |>
     select(!!col_sym) |>
     rename(x = !!col_sym)
-
-  ro_val <- d |>
-    slice(ro_ind) |> pull()
-  ro_val <- ro_val * scale_effn
 
   if(barplot){
     # This is the count data frame, which we use directly to plot instead of
@@ -117,6 +124,10 @@ plot_mcmc_histogram <- function(d,
     return(g)
   }
 
+  ro_val <- d |>
+    slice(ro_ind) |> pull()
+  ro_val <- ro_val * scale_effn
+
   # Histograms only, barplots returned above
   d <- d |>
     mutate(x = as.numeric(x))
@@ -141,6 +152,30 @@ plot_mcmc_histogram <- function(d,
   if(is.null(y_breaks[1])){
     y_breaks <- seq(y_lim[1], y_lim[2], by = y_brk)
   }
+  gr <- ggplot_build(g)$data[[1]]
+  wch_less <- which(gr$xmin < ro_val / scale_effn)
+  wch_more <- which(gr$xmin >= ro_val / scale_effn)
+  if(tail(wch_less, 1) + 1 != head(wch_more, 1)){
+    stop("The R0 value does not seem to fit into a bin on the histogram",
+         call. = FALSE)
+  }
+  ro_xmin <- gr |>
+    slice(tail(wch_less, 1)) |>
+    pull(xmin)
+  ro_xmax <- gr |>
+    slice(head(wch_more, 1)) |>
+    pull(xmin)
+  ro_x <- (ro_xmin + ro_xmax) / 2
+  ro_y0 <- (gr |>
+             slice(tail(wch_less, 1)) |>
+             pull(y))
+  ro_y1 <- ro_y0 + ro_arrow_length
+  # Arrow data frame
+  lbl <- tibble(x = ro_x,
+                xend = ro_x,
+                y = ro_y0,
+                yend = ro_y1,
+                label = paste0("R0 (", ro_val, ")"))
 
   g <- g +
     scale_x_continuous(breaks = x_breaks,
@@ -154,20 +189,38 @@ plot_mcmc_histogram <- function(d,
     xlab(x_lab) +
     ylab(y_lab) +
     stat_bin(aes(x = x,
-                 label = after_stat(ifelse(count > 50, count, NA))),
-             color = "white",
+                 label = after_stat(ifelse(count > bar_label_limit,
+                                           count,
+                                           NA))),
+             color = bar_text_color,
              binwidth = x_brk,
              geom = "text",
              position = position_stack(vjust = 0.5)) +
     stat_bin(aes(x = x,
-                 label = after_stat(ifelse(count <= 50, count, NA))),
+                 label = after_stat(ifelse(count <= bar_label_limit,
+                                           count,
+                                           NA))),
+             color = bar_text_color,
              binwidth = x_brk,
              geom = "text",
              vjust = -0.5) +
-    annotate("text",
-             x = ro_val / scale_effn,
-             y = (y_lim[2] - y_lim[1]) / 2,
-             label = paste0("R0 (", ro_val, ")"))
+    geom_segment(data = lbl,
+              aes(x = x,
+                  xend = xend,
+                  y = y,
+                  yend = yend),
+              arrow = arrow(type = "closed",
+                            ends = "first",
+                            length = unit(2.5, 'mm'))) +
+    geom_text(data = lbl,
+              aes(x = x,
+                  y = yend + 0.1 * yend,
+                  label = label))
+
+    # annotate("text",
+    #          x = ro_x,
+    #          y = ro_y1,
+    #          label = paste0("R0 (", ro_val, ")"))
 
   g
 }
