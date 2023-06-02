1db13a75c074560de853071623bceaba28e15aa1
Chris Grandin <chris.grandin@gmail.com>
2023-06-02 01:53:39 -0700

    Improve vec2df

--- R/utils-vec2df.R
+++ R/utils-vec2df.R
@@ -1,15 +1,13 @@
-#' Create a one-row data.frame from a vector of values
+#' Create a one-row data frame from a vector of values
 #'
 #' @details
 #' If `nms` is not given, the names of the vector (if `vec` is a named vector)
 #' will be used for the column names
-#' This function should not be used for massive amounts of conversions in
-#' a loop as it has a bad time complexity
 #'
 #' @param vec A vector
 #' @param nms Optional column names for the new data frame
 #'
-#' @return A [tibble::tibble()] with one row
+#' @return A one row [tibble::tibble()]
 #' @export
 vec2df <- function(vec, nms = NULL){
 
@@ -19,19 +17,10 @@ vec2df <- function(vec, nms = NULL){
     names(df) <- nms
   }
 
-  df <- vec |>
-    enframe(name = NULL) |>
-    t() |>
-    as.data.frame() |>
-    as_tibble()
-
-  if(is.null(nms)){
-    if(!is.null(names(vec)[1])){
-      names(df) <- names(vec)
-    }
-  }else{
-    names(df) <- nms
+  d <- as_tibble(t(vec))
+  if(!is.null(nms)){
+    names(d) <- nms
   }
 
-  df
+  d
 }
