200b5fab8fd1d25fc81b7a355cc7d7dc5f618770
Chris Grandin <chris.grandin@gmail.com>
2023-06-08 15:51:02 -0700

    Add glossary appendix

--- R/plot-spr-illustration.R
+++ R/plot-spr-illustration.R
@@ -1,219 +1,213 @@
+#' Create a three-panel plot illustrating how the SPR is calculated.
+#'
+#' @param model A model object, , created by [create_rds_file()]
+#' @param yrs A vector of years to include in the calculation. If `NULL`,
+#' the years found in the weight-at-age data frame (`model$wtatage`)
+#' will be used
+#' @param color The color to use for the bars
+#' @param show_legend Logical. Whether or not to show the text 'legend'
+#'
+#' @return A [ggplot2::ggplot()] object
+#' @export
 plot_spr_illustration <- function(model,
                                   yrs = NULL,
-                                  show.fished = TRUE,
-                                  show.legend = TRUE){
-  # graphic for appendix to illustrate how SPR is calculated
-  col1 <- 'grey20'
-  col2 <- 'grey70'
+                                  color = "royalblue",
+                                  show_legend = TRUE){
 
-  # ages to show in the plot
-  ages <- 0:20
-  xlim <- c(0,20)
-  x.legend <- 6 # x-value associated with left side of legend
-  x.legend2 <- 12 # x-value associated with left side of legend
-
-  cols <- as.character(ages)
-
-  if(is.null(yrs)){
-    yrs <- unique(model$wtatage$Yr)
+  # Ages to show in the plot
+  ages_chr <- grep("\\d+", names(model$ageselex), value = TRUE)
+  if(!length(ages_chr)){
+    stop("Problem with object `model$ageselex`, no ages were found as ",
+         "column names in the data frame. Check to make sure they are ",
+         "there and not prepended by some characters, e.g. 'a15'",
+         call. = FALSE)
   }
+  ages <- as.numeric(ages_chr)
+  yrs <- yrs %||% unique(model$wtatage$Yr)
 
-  # average maturity * fecundity
-  matfec.vec <- model$wtatage %>%
+  # Average maturity * fecundity
+  matfec_vec <- model$wtatage |>
     filter(Fleet == -2,
-           Yr %in% yrs) %>%
-    select(cols) %>%
-    summarize_all(mean)
-  # average weight at age
-  wt.vec.avg <- model$wtatage %>%
+           Yr %in% yrs) |>
+    select(all_of(ages_chr)) |>
+    summarize_all(mean) |>
+    unlist()
+  # Average weight at age
+  meanwt_vec <- model$wtatage |>
     filter(Fleet == -1,
-           Yr %in% yrs) %>%
-    select(cols) %>%
-    summarize_all(mean)
+           Yr %in% yrs) |>
+    select(all_of(ages_chr)) |>
+    summarize_all(mean) |>
+    unlist()
 
   # numbers at age in equilibrium
-  N_at_age.equil <- as.numeric(model$natage[model$natage$Era=="VIRG" &
-                                              model$natage$"Beg/Mid"=="B",
-                                            cols])
+  n_at_age_equil <- model$natage |>
+    filter(Era == "VIRG",
+           `Beg/Mid` == "B") |>
+    select(all_of(ages_chr)) |>
+    unlist()
 
-  # M and Z at age for final year
-  M_at_age.endyr <- as.numeric(model$Natural_Mortality[model$Natural_Mortality$Yr == model$endyr,
-                                              cols])
-  F_at_age.endyr <- as.numeric(model$fatage[model$fatage$Yr == model$endyr,
-                                              cols])
+  m_at_age_endyr <- model$Natural_Mortality |>
+    filter(Yr == model$endyr) |>
+    select(all_of(ages_chr)) |>
+    unlist()
 
-  Z_at_age.endyr <- F_at_age.endyr + M_at_age.endyr
+  f_at_age_endyr <- model$fatage |>
+    filter(Yr == model$endyr) |>
+    select(all_of(ages_chr)) |>
+    unlist()
 
-  # calculate numbers at age with and without fishing based on 1 at age 0
-  N_at_age.M <- rep(1, 21)
-  N_at_age.Z <- rep(1, 21)
-  for(iage in 1:20){
-    N_at_age.M[iage+1] <- N_at_age.M[iage]*exp(-M_at_age.endyr[iage])
-    N_at_age.Z[iage+1] <- N_at_age.Z[iage]*exp(-Z_at_age.endyr[iage])
+  z_at_age_endyr <- f_at_age_endyr + m_at_age_endyr
+
+  # Calculate numbers-at-age with and without fishing based on 1 at age 0
+  # This assumes the `ages` vector starts at zero
+  n_at_age_m <- rep(1, length(ages))
+  n_at_age_z <- rep(1, length(ages))
+  for(iage in ages[-1]){
+    n_at_age_m[iage + 1] <- n_at_age_m[iage] * exp(-m_at_age_endyr[iage])
+    n_at_age_z[iage + 1] <- n_at_age_z[iage] * exp(-z_at_age_endyr[iage])
   }
 
-  # set up 3-panel plot and adjust margins
-  par(mfrow = c(3, 1),
-      mar = c(1, 4, 3, 1),
-      oma = c(3, 0, 0, 0),
-      cex = 1)
+  d <- tibble(age = ages,
+              natage_m = n_at_age_m,
+              natage_z = n_at_age_z,
+              # Biomass-per-recruit unfished
+              biomass_pr_unfished = n_at_age_m * meanwt_vec,
+              # Biomass-per-recruit fished
+              biomass_pr_fished = n_at_age_z * meanwt_vec,
+              # Female spawning biomass per recruit unfished
+              fsbiomass_pr_unfished = 0.5 * n_at_age_m * matfec_vec,
+              # Female spawning biomass per recruit fished
+              fsbiomass_pr_fished = 0.5 * n_at_age_z * matfec_vec)
 
-  # make plot of numbers per recruit
-  plot(0,
-       type = 'n',
-       xlim = xlim,
-       ylim = c(0, 1.1),
-       xaxs = 'r',
-       yaxs  ='i',
-       xlab = "",
-       ylab = "",
-       axes = FALSE)
-  points(ages,
-         N_at_age.M,
-         type = 'h',
-         lwd = 30,
-         lend = 1,
-         col = col2)
-  if(show.fished){
-    points(ages,
-           N_at_age.Z,
-           type = 'h',
-           lwd = 30,
-           lend = 1,
-           col = col1)
-  }
-  axis(1, at = -1:22)
-  axis(2, las = 1)
-  mtext("Numbers per recruit",
-        font = 2,
-        cex = 1,
-        at = 0,
-        adj = 0)
-  if(show.legend){
-    legend(x = x.legend,
-           y = 1,
-           fill = c(col2, col1),
-           border = NA,
-           legend = c("No fishing","With fishing"),
-           bty = 'n')
-  }
+  p <- list()
+  p[[1]] <- ggplot(d) +
+    geom_bar(aes(x = age,
+                 y = natage_m),
+             stat = "identity",
+             fill = color,
+             alpha = 0.5) +
+    geom_bar(aes(x = age,
+                 y = natage_z),
+             stat = "identity",
+             fill = color,
+             alpha = 1) +
+    xlab("") +
+    ylab("") +
+    ggtitle("Numbers per recruit") +
+    theme(plot.margin = margin(c(6, 0, 0, 0))) +
+    scale_y_continuous(labels = ~{f(.x, 2)})
 
-  # make plot of biomass per recruit
-  plot(0,
-       type='n',
-       xlim = xlim,
-       ylim = c(0, 0.215),
-       xaxs = 'r',
-       yaxs = 'i',
-       xlab = "",
-       ylab = "",
-       axes = FALSE)
-  points(ages,
-         N_at_age.M * wt.vec.avg,
-         type = 'h',
-         lwd = 30,
-         lend = 1,
-         col = col2)
-  if(show.fished){
-    points(ages,
-           N_at_age.Z * wt.vec.avg,
-           type = 'h',
-           lwd = 30,
-           lend = 1,
-           col = col1)
-  }
-  axis(1, at = -1:22)
-  axis(2, las = 1)
-  mtext("Biomass per recruit",
-        font = 2,
-        cex = 1,
-        at = 0,
-        adj = 0)
+  p[[2]] <- ggplot(d) +
+    geom_bar(aes(x = age,
+                 y = biomass_pr_unfished),
+             stat = "identity",
+             fill = color,
+             alpha = 0.5) +
+    geom_bar(aes(x = age,
+                 y = biomass_pr_fished),
+             stat = "identity",
+             fill = color,
+             alpha = 1) +
+    xlab("") +
+    ylab("") +
+    ggtitle("Biomass per recruit") +
+    theme(plot.margin = margin(c(0, 0, 0, 0))) +
+    scale_y_continuous(labels = ~{f(.x, 2)})
 
-  # make plot of female spawning biomass per recruit
-  plot(0,
-       type = 'n',
-       xlim = xlim,
-       ylim = c(0, 0.12),
-       xaxs = 'r',
-       yaxs = 'i',
-       xlab = "",
-       ylab = "",
-       axes = FALSE)
-  points(ages,
-         0.5 * N_at_age.M * matfec.vec,
-         type = 'h',
-         lwd = 30,
-         lend = 1,
-         col = col2)
-  if(show.fished){
-    points(ages,
-           0.5 * N_at_age.Z * matfec.vec,
-           type = 'h',
-           lwd = 30,
-           lend = 1,
-           col = col1)
-  }
-  axis(1, at = -1:22)
-  axis(2, at = pretty(c(0, 0.1)), las = 1)
-  mtext("Female spawning biomass per recruit",
-        font = 2,
-        cex = 1,
-        at = 0,
-        adj = 0)
-
-  # add label for x-axis (applied to all plots)
-  mtext("Age",
-        side = 1,
-        outer = TRUE,
-        line = 1)
+  p[[3]] <- ggplot(d) +
+    geom_bar(aes(x = age,
+                 y = fsbiomass_pr_unfished),
+             stat = "identity",
+             fill = color,
+             alpha = 0.5) +
+    geom_bar(aes(x = age,
+                 y = fsbiomass_pr_fished),
+             stat = "identity",
+             fill = color,
+             alpha = 1) +
+    xlab("Age") +
+    ylab("") +
+    ggtitle("Female spawning biomass per recruit") +
+    theme(plot.margin = margin(c(0, 0, 6, 0))) +
+    scale_y_continuous(labels = ~{f(.x, 2)})
 
   # calculate spawning potential with and without fishing and SPR
-  spawn.potential.M <- sum(0.5 * N_at_age.M * matfec.vec)
-  spawn.potential.Z <- sum(0.5 * N_at_age.Z * matfec.vec)
-  sum.M <- sum(spawn.potential.M)
-  sum.Z <- sum(spawn.potential.Z)
-  SPR <- sum(spawn.potential.Z) / sum(spawn.potential.M)
+  spawn_potential_m <- sum(0.5 * n_at_age_m * matfec_vec)
+  spawn_potential_z <- sum(0.5 * n_at_age_z * matfec_vec)
+  sum_m <- sum(spawn_potential_m)
+  sum_z <- sum(spawn_potential_z)
+  spr <- sum(spawn_potential_z) / sum(spawn_potential_m)
 
   # format numbers for legend
-  sum.M.txt <- format(sum.M,
-                      digits = 2,
-                      nsmall = 2)
-  sum.Z.txt <- format(sum.Z,
-                      digits = 2,
-                      nsmall = 2)
-  SPR.txt <- format(SPR,
-                    digits = 2,
-                    nsmall = 2)
-  intensity.txt <- format((1 - SPR) / (1 - 0.4),
-                          digits = 2,
-                          nsmall = 2)
+  sum_m_txt <- f(sum_m, 2)
+  sum_z_txt <- f(sum_z, 2)
+  spr_txt <- f(spr, 2)
+  intensity_txt <- f((1 - spr) / (1 - 0.4), 2)
 
-  if(show.legend){
-    legend(x = x.legend,
-           y = 1.05 * par()$usr[4],
-           fill = c(col2, col1, NA),
-           border = NA,
-           legend = c(paste0("Total = ",
-                             sum.M.txt),
-                      paste0("Total = ",
-                             sum.Z.txt)),
-           bty = 'n')
-    text(x = x.legend2,
-         y = 0.90 * par()$usr[4],
-         paste0("SPR = ",
-                sum.Z.txt,
-                "/",
-                sum.M.txt,
-                " = ",
-                SPR.txt),
-         pos = 4)
-    text(x = x.legend2,
-         y = 0.55 * par()$usr[4],
-         paste0("Rel. Fishing Intensity =\n",
-                "     (1 - SPR)/(1 - 0.40) =\n",
-                "          ",
-                intensity.txt),
-         pos = 4)
+  if(show_legend){
+    p[[3]] <- p[[3]] +
+      annotate("point",
+               x = 7.5,
+               y = 0.1,
+               shape = 22,
+               size = 6,
+               fill = color,
+               alpha = 0.5,
+               color = "transparent") +
+      annotate("point",
+               x = 7.5,
+               y = 0.09,
+               shape = 22,
+               size = 6,
+               fill = color,
+               alpha = 1,
+               color = "transparent") +
+      annotate("text",
+               x = 10,
+               y = 0.1,
+               size = 4,
+               label = paste0("Total = ",
+                              sum_m_txt)) +
+      annotate("text",
+               x = 10,
+               y = 0.09,
+               size = 4,
+               label = paste0("Total = ",
+                              sum_z_txt)) +
+      annotate("text",
+               x = 12.5,
+               y = 0.1,
+               size = 4,
+               hjust = 0,
+               label = paste0("SPR = ",
+                              sum_z_txt,
+                              " / ",
+                              sum_m_txt,
+                              " = ",
+                              spr_txt)) +
+      annotate("text",
+               x = 12.5,
+               y = 0.08,
+               size = 4,
+               hjust = 0,
+               label = "Rel. Fishing intensity =") +
+      annotate("text",
+               x = 12.5,
+               y = 0.07,
+               size = 4,
+               hjust = 0,
+               label = "(1 - SPR) / (1 - 0.40) =") +
+    annotate("text",
+             x = 12.5,
+             y = 0.06,
+             size = 4,
+             hjust = 0,
+             label = paste0("(1 - ",
+                            spr_txt,
+                            ") / (1 - 0.40) = ",
+                            intensity_txt))
   }
+
+  plot_grid(plotlist = p, ncol = 1, align = "v")
 }
