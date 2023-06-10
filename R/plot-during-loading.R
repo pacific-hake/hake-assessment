84f2c31b6c603d4ae9ba16a7b93bb2b8aba2f126
Chris Grandin <chris.grandin@gmail.com>
2023-06-10 01:59:19 -0700

    Remove plots during loading (comment out) * Try plotting all during doc build

--- R/plot-during-loading.R
+++ R/plot-during-loading.R
@@ -17,41 +17,41 @@ plot_during_loading <- function(model){
   # Plot the survey fit with many MCMC posterior lines
   # This is done MUST be made here because the MCMC object is not stored
   # in the RDS files
-  out$survey_fit <- plot_survey_fit_mcmc(model,
-                                         type = "acoustic",
-                                         n_posts = 1000,
-                                         glow = TRUE)
-
-  out$age1_index_fit <- plot_survey_fit_mcmc(model,
-                                             type = "age1",
-                                             n_posts = 1000,
-                                             ylim = c(0, 10),
-                                             glow = TRUE)
-
-  out$selex_posteriors <- {
-    plist <- list()
-    plist[[1]] <- plot_selex_posteriors(model,
-                                        type = "survey",
-                                        probs = probs,
-                                        age_range = c(1, 8),
-                                        show_xlab = FALSE,
-                                        post_med_line_color = "red3",
-                                        unc_line_color = "red3",
-                                        glow = TRUE)
-    plist[[2]] <- plot_selex_posteriors(model,
-                                        type = "fishery",
-                                        probs = probs,
-                                        age_range = c(1, 8),
-                                        glow = TRUE)
-    plot_grid(plotlist = plist, nrow = 2, ncol = 1)
-  }
-
-
-  out$stock_recr <- plot_stock_recruitment(model,
-                                           probs = probs)
-
-  out$catch_fore_density <- plot_catch_forecast_density(base_model,
-                                                        yr = end_yr)
+  # out$survey_fit <- plot_survey_fit_mcmc(model,
+  #                                        type = "acoustic",
+  #                                        n_posts = 1000,
+  #                                        glow = TRUE)
+  #
+  # out$age1_index_fit <- plot_survey_fit_mcmc(model,
+  #                                            type = "age1",
+  #                                            n_posts = 1000,
+  #                                            ylim = c(0, 10),
+  #                                            glow = TRUE)
+  #
+  # out$selex_posteriors <- {
+  #   plist <- list()
+  #   plist[[1]] <- plot_selex_posteriors(model,
+  #                                       type = "survey",
+  #                                       probs = probs,
+  #                                       age_range = c(1, 8),
+  #                                       show_xlab = FALSE,
+  #                                       post_med_line_color = "red3",
+  #                                       unc_line_color = "red3",
+  #                                       glow = TRUE)
+  #   plist[[2]] <- plot_selex_posteriors(model,
+  #                                       type = "fishery",
+  #                                       probs = probs,
+  #                                       age_range = c(1, 8),
+  #                                       glow = TRUE)
+  #   plot_grid(plotlist = plist, nrow = 2, ncol = 1)
+  # }
+  #
+  #
+  # out$stock_recr <- plot_stock_recruitment(model,
+  #                                          probs = probs)
+  #
+  # out$catch_fore_density <- plot_catch_forecast_density(base_model,
+  #                                                       yr = end_yr)
 
   # out$prior_posterior <- plot_priors_vs_posts(base_model,
   #                                             key_posteriors,
