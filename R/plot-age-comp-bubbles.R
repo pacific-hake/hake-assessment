7cad6d229727d3793819b941b8e3f4ad93eb4d3d
Chris Grandin <chris.grandin@gmail.com>
2023-06-12 16:04:28 -0700

    Fix age bubble plotting code

--- R/plot-age-comp-bubbles.R
+++ R/plot-age-comp-bubbles.R
@@ -2,8 +2,10 @@
 #'
 #' @param model A model object as returned from [create_rds_file()]
 #' @param type Either `fishery` or `survey`
-#' @param inc_maen_age_line Logical. If `TRUE`, show a line across years
-#' representing the mean age for each year
+#' @param mcmc Logical. If `TRUE`, plt the MCMC median numbers-at-age. If
+#' `FALSE` plot the input data (either fishery or survey as given by `type`)
+#' @param inc_mean_age_line Logical. If `TRUE`, plot the mean age line
+#' over the bubbles
 #' @param ... Additional parameters passed to [plot_bubbles()]
 #'
 #' @return A [ggplot2::ggplot()] object
@@ -22,14 +24,24 @@ plot_age_comp_bubbles <- function(model,
   }
 
   # Ages to show in the plot
+  val_col_nm <- ifelse(mcmc, "Numbers (billions)", "Proportion")
+  val_col_sym <- sym(val_col_nm)
   if(mcmc){
     if(type == "survey"){
       stop("`survey` Numbers-at-age for MCMC not available. You would ",
            "need to add that to the extra-mcmc extraction code first",
            call. = FALSE)
     }
+
     d <- model$extra_mcmc$natage_med |>
-      rename(Year = yr)
+      rename(Year = yr) |>
+      pivot_longer(-Year,
+                   names_to = "Age",
+                   values_to = val_col_nm) |>
+      mutate(Age = as.numeric(Age)) |>
+      mutate(Age = factor(Age)) |>
+      mutate(!!val_col_sym := !!val_col_sym / 1e3)
+
   }else{
     pat <- "^a(\\d+)$"
     age_inds <- grep(pat, names(model$dat$agecomp))
@@ -48,31 +60,35 @@ plot_age_comp_bubbles <- function(model,
       as_tibble() |>
       filter(FltSvy == type_code) |>
       select(Yr, all_of(ages_cols)) |>
-      set_names(nms)
+      set_names(nms) %>%
+      mutate(n = rowSums(.[-1])) %>%
+      mutate_at(vars(-Year), ~(. / n)) |>
+      select(-n) |>
+      pivot_longer(-Year,
+                   names_to = "Age",
+                   values_to = val_col_nm) |>
+      mutate(Age = as.numeric(Age)) |>
+      mutate(Age = factor(Age))
   }
 
-  d <- d %>%
-    mutate(row_sum = rowSums(.[-1])) %>%
-    mutate_at(vars(-Year), ~(. / row_sum)) |>
-    select(-row_sum) |>
-    pivot_longer(-Year,
-                 names_to = "Age",
-                 values_to = "Proportion") |>
-    mutate(Age = as.numeric(Age)) |>
-    mutate(Age = factor(Age))
-
   if(inc_mean_age_line){
+
     d_mean <- d |>
       split(~Year) |>
       map_dbl(~{
         j <- .x |>
-          mutate(val = as.numeric(as.character(Age)) * Proportion)
-        mean_age <- sum(j$val) / sum(j$Proportion)}) |>
+          mutate(val = as.numeric(as.character(Age)) * !!val_col_sym)
+        mean_age <- sum(j$val) / sum(j[[val_col_nm]])}) |>
       enframe(name = "Year", value = "Age") |>
       mutate(Year = as.numeric(Year))
-    g <- plot_bubbles(d, mean_age = d_mean, ...)
+    g <- plot_bubbles(d,
+                      mean_age = d_mean,
+                      val_col_nm = val_col_nm,
+                      ...)
   }else{
-    g <- plot_bubbles(d, ...)
+    g <- plot_bubbles(d,
+                      val_col_nm = val_col_nm,
+                      ...)
   }
 
   g
--- R/plot-bubbles.R
+++ R/plot-bubbles.R
@@ -1,13 +1,15 @@
 #' Make a bubble plot from the given data
 #'
 #' @param d a [tibble::tibble()] of the data in long format with column
-#' names `Year`, `Age`, and `Proportion`
+#' names `Year`, `Age`, and the column name given by `val_col_nm`
 #' @param clines An optional vector of years to draw cohort lines through
+#' @param val_col_nm The name of the column in `d` to use for the values
 #' @param mean_age A two-column tibble with column names `Year` and `Age` where
 #' each row contains a year and `Age` represents the mean age for each year
 #' @param mean_age_line_color The line color for the mean age line
-#' @param mean_age_line_size The line thickness for the mean age line
 #' @param mean_age_line_type The line type for the mean age line
+#' @param diag_line_color The line color for the cohort diagonal lines
+#' @param diag_line_type The line type for the cohort diagonal lines
 #' @param yrs A vector of 2, for the years to show on the plot
 #' @param by How many years between year labels on the x-axis
 #' @param leg_pos See the `legend.position` parameter in
@@ -16,14 +18,17 @@
 #' @param point_fill Color of the bubble fill
 #' @param point_color Color of the bubble outline
 #' @param xlim Limits for the x-axis
-#' @param leg_title The legend title text
 #' @param ... Additional parameters passed to [ggplot2::geom_point()],
 #' [ggplot2::geom_segment()] and [ggplot2::theme()]
+#' @param mean_age_line_width
+#' @param diag_line_width
+#' @param show_x_axis_labels
 #'
 #' @return A [ggplot2::ggplot()] object
 #' @export
 plot_bubbles <- function(d,
                          clines = c(1980, 1984, 1999, 2010, 2014, 2016, 2020),
+                         val_col_nm = "Proportion",
                          mean_age = NULL,
                          mean_age_line_color = "red",
                          mean_age_line_width = 1.5,
@@ -38,27 +43,30 @@ plot_bubbles <- function(d,
                          point_fill = main_fill,
                          point_color = "black",
                          xlim = c(1966, year(Sys.Date())),
-                         leg_title = "Proportion",
+                         show_x_axis_labels = TRUE,
                          ...){
 
-  if(!all(c("Year", "Age", "Proportion") %in% names(d))){
+  if(!all(c("Year", "Age", val_col_nm) %in% names(d))){
     stop("One of the necessary columns is missing. This function requires ",
-         "`Year`, `Age`, and `Proportion` columns",
+         "`Year` and `Age` columns and a column with the name assigned ",
+         "to the argument `val_col_nm` (default name is `Proportion`)",
          call. = FALSE)
   }
+
+  val_col_sym <- sym(val_col_nm)
 
   if(!is.null(xlim[1])){
     d <- d |>
       filter(Year %in% xlim[1]:xlim[2])
   }
 
-  # Start breaks at the last divisible-by-5 year before the first year
-  x_breaks <- seq(xlim[1] - (xlim[1] %% 5), xlim[2], 5)
+  # Start breaks at the last year divisible-by-`by` before the first year
+  x_breaks <- seq(xlim[1] - (xlim[1] %% by), xlim[2], by)
 
   g <- ggplot(d,
               aes(x = Year,
                   y = Age,
-                  size = Proportion)) +
+                  size = !!val_col_sym)) +
     geom_point(shape = 21,
                alpha = point_alpha,
                fill = point_fill,
@@ -66,7 +74,7 @@ plot_bubbles <- function(d,
                ...) +
     scale_x_continuous(breaks = x_breaks,
                        expand = c(0.025, 0)) +
-    coord_cartesian(xlim) +
+    coord_cartesian(xlim = xlim) +
     expand_limits(x = xlim[1]:xlim[2]) +
     scale_size_continuous(range = c(0.5, 10))
 
@@ -94,6 +102,7 @@ plot_bubbles <- function(d,
   }
 
   if(!is.null(mean_age)){
+
     g <- g +
       geom_line(data = mean_age,
                 aes(x = Year,
@@ -118,9 +127,14 @@ plot_bubbles <- function(d,
                  size = mean_age_line_width)
   }
 
+  if(!show_x_axis_labels){
+    g <- g +
+      theme(axis.title.x = element_blank(),
+            axis.text.x = element_blank())
+  }
+
   g <- g +
-    theme(legend.position = leg_pos, ...) +
-    guides(size = guide_legend(title = leg_title))
+    theme(legend.position = leg_pos, ...)
 
   g
 }
