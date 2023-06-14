a32d245233de2996d412de90028ce04d4a185f67
Chris Grandin <chris.grandin@gmail.com>
2023-06-14 09:36:19 -0700

    Fix plot_rel_biomass() to use new major tick code

--- R/plot-rel-biomass.R
+++ R/plot-rel-biomass.R
@@ -1,6 +1,7 @@
 #' Plot relative biomass from MCMC output for one or more models
 #'
 #' @rdname plot_biomass
+#'
 #' @export
 plot_rel_biomass <- function(model_lst = NULL,
                              model_names,
@@ -8,14 +9,13 @@ plot_rel_biomass <- function(model_lst = NULL,
                              x_breaks = xlim[1]:xlim[2],
                              x_labs_mod = 5,
                              x_expansion = 3,
+                             tick_prop = 0.8,
+                             vjust_x_labels = -2,
                              ylim = c(0, 3.5),
                              alpha = 0.1,
                              leg_pos = c(0.65, 0.83),
                              leg_ncol = 1,
                              leg_font_size = 12,
-                             axis_title_font_size = 14,
-                             axis_tick_font_size = 11,
-                             minor_tick_length = 0.1, # centimeters
                              point_size = 2,
                              point_shape = 16,
                              line_width = 1,
@@ -33,37 +33,40 @@ plot_rel_biomass <- function(model_lst = NULL,
            "must be supplied. Both are `NULL`",
            call. = FALSE)
     }
-    d_obj <- create_group_df_biomass(model_lst, model_names, rel = TRUE)
+    d_obj <- create_group_df_biomass(model_lst,
+                                     model_names,
+                                     rel = TRUE)
   }
 
-  # Remove labels for the minor x-axis ticks
-  x_labels <- NULL
-  for(i in x_breaks){
-    if(i %% x_labs_mod == 0){
-      x_labels <- c(x_labels, i)
-    }else{
-      x_labels <- c(x_labels, "")
-    }
-  }
-  # X-axis tick mark lengths adjusted here
-  x_breaks_nth <- x_breaks[x_breaks %% x_labs_mod == 0]
-  top_y_pos = ylim[1]
-  bot_y_pos = ylim[1] - (ylim[2] - ylim[1]) / 25
-  custom_ticks <- tibble(group = x_breaks_nth,
-                         y_end = bot_y_pos)
+  x_labels <- make_major_tick_labels(x_breaks = x_breaks,
+                                     modulo = x_labs_mod)
 
   y_breaks <- c(0, 0.1, 0.4, 0.5, 1.0)
-  y_labels <- expression("0", "0.1B"[0], "0.4B"[0], "0.5", "B"[0])
-  y_colors <- c("black", "red", "green", "black", "blue")
+  y_labels <- expression("0",
+                         "0.1B"[0],
+                         "0.4B"[0],
+                         "0.5",
+                         "B"[0])
+  y_colors <- c("black",
+                "red",
+                "green",
+                "black",
+                "blue")
   if(ylim[2] < 1){
     stop("Relative spawning biomass plot y-axis max must be 1 or greater",
          call. = FALSE)
   }
   if(ylim[2] > 1){
-    seq_above_1 <- seq(1.5, ylim[2], by = 0.5)
-    y_breaks <- c(y_breaks, seq_above_1)
-    y_labels <- c(y_labels, parse(text = as.character(seq_above_1)))
-    y_colors <- c(y_colors, rep("black", length(seq_above_1)))
+    seq_above_1 <- seq(1.5,
+                       ylim[2],
+                       by = 0.5)
+    y_breaks <- c(y_breaks,
+                  seq_above_1)
+    y_labels <- c(y_labels,
+                  parse(text = as.character(seq_above_1)))
+    y_colors <- c(y_colors,
+                  rep("black",
+                      length(seq_above_1)))
   }
 
   d <- d_obj[[1]]
@@ -122,47 +125,30 @@ plot_rel_biomass <- function(model_lst = NULL,
     theme(legend.title = element_blank(),
           legend.text = element_text(size = leg_font_size),
           legend.text.align = 0,
+          # These two commands move the x-axis major tick labels and axis
+          # title down so that the ticks. tick labels, and axis title don't
+          # overlap each other
+          axis.text.x = element_text(vjust = vjust_x_labels),
+          axis.title.x = element_text(vjust = vjust_x_labels),
           axis.text.y = element_text(color = y_colors),
           # plot.margin: top, right,bottom, left
           # Needed to avoid tick labels cutting off
           plot.margin = margin(12, 12, 7, 0)) +
     labs(x = "Year",
          y = ifelse(wrap_y_label,
-                    expression(atop("Relative Spawning Biomass",
+                    expression(atop("Rel. Spawning Biomass",
                                     paste("("~B[t]/B[0]~")"))),
-                    expression(paste("Relative Spawning Biomass ("~B[t]/B[0]~")"))))
+                    expression(paste("Rel. Spawning Biomass ("~B[t]/B[0]~")"))))
 
   # Add major tick marks
-  g <- g +
-    geom_linerange(data = custom_ticks,
-                   aes(x = group,
-                       ymax = top_y_pos,
-                       ymin = y_end),
-                   size = 0.5,
-                   inherit.aes = FALSE)
-
-  g <- g +
-    theme(axis.text.x = element_text(color = "grey20",
-                                     size = axis_tick_font_size,
-                                     angle = 0,
-                                     hjust = 0.5,
-                                     vjust = -3,
-                                     face = "plain"),
-          axis.text.y = element_text(color = "grey20",
-                                     size = axis_tick_font_size,
-                                     hjust = 1,
-                                     vjust = 0.5,
-                                     face = "plain"),
-          axis.title.x = element_text(color = "grey20",
-                                      size = axis_title_font_size,
-                                      angle = 0,
-                                      vjust = -2,
-                                      face = "plain"),
-          axis.title.y = element_text(color = "grey20",
-                                      size = axis_title_font_size,
-                                      angle = 90,
-                                      face = "plain"),
-          axis.ticks.length = unit(0.15, "cm"))
+  g <- g |>
+    add_major_ticks(x_breaks = x_breaks,
+                    modulo = x_labs_mod,
+                    # This proportion must be set by trial and error
+                    # Make sure to change `vjust` value above in the `theme()`
+                    # call so the labels are not overlapping the lines or
+                    # too far away from the lines
+                    prop = tick_prop)
 
   if(is.null(leg_pos[1]) || is.na(leg_pos[1])){
     g <- g +
