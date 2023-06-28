074e6c87a9ceaf2c797b4ffe24d4b469decaf963
Chris Grandin <chris.grandin@gmail.com>
2023-06-28 00:49:40 -0700

    Update forecast age comps

--- R/plot-fore-age-comps.R
+++ R/plot-fore-age-comps.R
@@ -9,25 +9,17 @@
 #' @param x_breaks A vector of values to show on the x-axis
 #' @param y_lim  A vector of 2 representing the minimum and maximum y-axis
 #' values to plot
-#' @param bar_alpha The transparency of the bars from 0 to 1
 #' @param whisker_width The width of the top and bottom crossbars
 #' (whiskers) on the error bars
-#' @param axis_title_font_size Size of the font for the X and Y axis labels
-#' @param axis_tick_font_size Size of the font for the X and Y axis tick labels
-#' @param axis_label_color Color for the axis labels and tick labels
 #'
 #' @return A [cowplot::plot_grid()] object
 #' @export
 plot_fore_age_comps <- function(model,
-                                probs = c(0.025, 0.5, 0.975),
+                                probs = hake::probs,
                                 x_lim = c(1, 15),
                                 x_breaks = seq(x_lim[1], x_lim[2], 2),
                                 y_lim = c(0, 0.4),
-                                bar_alpha = 0.3,
-                                whisker_width = 0.5,
-                                axis_title_font_size = 14,
-                                axis_tick_font_size = 12,
-                                axis_label_color = "black"){
+                                whisker_width = 0.5){
 
   if(length(probs) != 3){
     stop("The `probs` vector must be length 3, composed of the lower CI ",
@@ -64,7 +56,8 @@ plot_fore_age_comps <- function(model,
   plist[[1]] <- ggplot(by_nums,
                        aes(x = age, y = med)) +
     geom_bar(stat = "identity",
-             alpha = bar_alpha) +
+             fill = hake::main_fill,
+             alpha = hake::main_alpha) +
     geom_errorbar(aes(ymin = lower,
                       ymax = upper),
                   width = whisker_width) +
@@ -78,35 +71,13 @@ plot_fore_age_comps <- function(model,
     xlab("Age") +
     ylab(paste0("Expected proportion in ",
                 model$endyr + 1,
-                " catch")) +
-    theme(axis.text.x = element_text(color = axis_label_color,
-                                     size = axis_tick_font_size,
-                                     angle = 0,
-                                     hjust = 0.5,
-                                     vjust = -0.25,
-                                     face = "plain"),
-          axis.text.y = element_text(color = axis_label_color,
-                                     size = axis_tick_font_size,
-                                     hjust = 1,
-                                     vjust = 0.5,
-                                     face = "plain"),
-          axis.title.x = element_text(color = axis_label_color,
-                                      size = axis_title_font_size,
-                                      angle = 0,
-                                      vjust = 0,
-                                      face = "plain"),
-          axis.title.y = element_text(color = axis_label_color,
-                                      size = axis_title_font_size,
-                                      angle = 90,
-                                      face = "plain"),
-          # plot.margin: top, right,bottom, left
-          plot.margin = margin(12, 6, 6, 6))
-
+                " catch"))
 
   plist[[2]] <- ggplot(by_weight,
                        aes(x = age, y = med)) +
     geom_bar(stat = "identity",
-             alpha = bar_alpha) +
+             fill = hake::main_fill,
+             alpha = hake::main_alpha) +
     geom_errorbar(aes(ymin = lower,
                       ymax = upper),
                   width = whisker_width) +
@@ -117,29 +88,7 @@ plot_fore_age_comps <- function(model,
     coord_cartesian(xlim = x_lim,
                     ylim = y_lim) +
     xlab("Age") +
-    ylab("") +
-    theme(axis.text.x = element_text(color = axis_label_color,
-                                     size = axis_tick_font_size,
-                                     angle = 0,
-                                     hjust = 0.5,
-                                     vjust = -0.25,
-                                     face = "plain"),
-          axis.text.y = element_text(color = axis_label_color,
-                                     size = axis_tick_font_size,
-                                     hjust = 1,
-                                     vjust = 0.5,
-                                     face = "plain"),
-          axis.title.x = element_text(color = axis_label_color,
-                                      size = axis_title_font_size,
-                                      angle = 0,
-                                      vjust = 0,
-                                      face = "plain"),
-          axis.title.y = element_text(color = axis_label_color,
-                                      size = axis_title_font_size,
-                                      angle = 90,
-                                      face = "plain"),
-          # plot.margin: top, right,bottom, left
-          plot.margin = margin(12, 6, 6, 0))
+    ylab("")
 
   plot_grid(plotlist = plist, nrow = 1, align = "h")
 }
