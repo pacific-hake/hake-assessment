48406bc13108ce3f82c841b0ff120ca1ae90e541
Chris Grandin <chris.grandin@gmail.com>
2023-06-28 07:45:15 -0700

    Update plot_survey_index_fits()

--- R/plot-survey-index-fits.R
+++ R/plot-survey-index-fits.R
@@ -5,21 +5,21 @@
 #' @export
 plot_survey_index_fits <- function(
     model_lst = NULL,
-    model_names,
+    model_names = NULL,
     survey_type = c("age1",
                     "age2"),
     xlim = c(1995, 2021),
     x_breaks = xlim[1]:xlim[2],
     x_labs_mod = 5,
     x_expansion = 3,
+    tick_prop = 1,
+    vjust_x_labels = -2,
     ylim = c(0, 3),
     y_breaks = seq(ylim[1], ylim[2], by = 0.5),
     alpha = 0.1,
     leg_pos = c(0.65, 0.83),
     leg_ncol = 1,
     leg_font_size = 12,
-    axis_title_font_size = 14,
-    axis_tick_font_size = 11,
     point_size = 1.5,
     line_width = 0.5,
     clip_cover = 2,
@@ -50,22 +50,8 @@ plot_survey_index_fits <- function(
   # Add observed
   colors <- c(colors, "black")
 
-
-  # Remove labels for the minor x-axis ticks
-  x_labels <- NULL
-  for(i in x_breaks){
-    if(i %% x_labs_mod == 0){
-      x_labels <- c(x_labels, i)
-    }else{
-      x_labels <- c(x_labels, "")
-    }
-  }
-  # Major tick mark lengths adjusted here
-  x_breaks_nth <- x_breaks[x_breaks %% x_labs_mod == 0]
-  top_y_pos = ylim[1]
-  bot_y_pos = ylim[1] - (ylim[2] - ylim[1]) / 25
-  custom_ticks <- tibble(group = x_breaks_nth,
-                         y_end = bot_y_pos)
+  x_labels <- make_major_tick_labels(x_breaks = x_breaks,
+                                     modulo = x_labs_mod)
 
   y_title <- ifelse(survey_type == "age1",
                     "Numbers (billions)",
@@ -100,43 +86,24 @@ plot_survey_index_fits <- function(
     theme(legend.title = element_blank(),
           legend.text = element_text(size = leg_font_size),
           legend.text.align = 0,
-          # plot.margin: top, right,bottom, left
           # Needed to avoid tick labels cutting off
-          plot.margin = margin(12, 12, 7, 0)) +
+          # These two commands move the x-axis major tick labels and axis
+          # title down so that the ticks. tick labels, and axis title don't
+          # overlap each other
+          axis.text.x = element_text(vjust = vjust_x_labels),
+          axis.title.x = element_text(vjust = vjust_x_labels)) +
     xlab("Year") +
     ylab(y_title)
 
   # Add major tick marks
-  g <- g +
-    geom_linerange(data = custom_ticks,
-                   aes(x = group,
-                       ymax = top_y_pos,
-                       ymin = y_end),
-                   size = 0.5,
-                   inherit.aes = FALSE)
-
-  g <- g +
-    theme(axis.text.x = element_text(color = "grey20",
-                                     size = axis_tick_font_size,
-                                     angle = 0,
-                                     hjust = 0.5,
-                                     vjust = -3,
-                                     face = "plain"),
-          axis.text.y = element_text(color = "grey20",
-                                     size = axis_tick_font_size,
-                                     hjust = 1,
-                                     vjust = 0.5,
-                                     face = "plain"),
-          axis.title.x = element_text(color = "grey20",
-                                      size = axis_title_font_size,
-                                      angle = 0,
-                                      vjust = -2,
-                                      face = "plain"),
-          axis.title.y = element_text(color = "grey20",
-                                      size = axis_title_font_size,
-                                      angle = 90,
-                                      face = "plain"),
-          axis.ticks.length = unit(0.15, "cm"))
+  g <- g |>
+    add_major_ticks(x_breaks = x_breaks,
+                    modulo = x_labs_mod,
+                    # This proportion must be set by trial and error
+                    # Make sure to change `vjust` value above in the `theme()`
+                    # call so the labels are not overlapping the lines or
+                    # too far away from the lines
+                    prop = tick_prop)
 
   if(is.null(leg_pos[1]) || is.na(leg_pos[1])){
     g <- g +
