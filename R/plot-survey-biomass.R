9e5db30cff1a563453ebb8af8e391236f447b5c9
Chris Grandin <chris.grandin@gmail.com>
2023-07-23 10:28:00 -0700

    Generalize points and lines for plot_survey_biomass()

--- R/plot-survey-biomass.R
+++ R/plot-survey-biomass.R
@@ -9,13 +9,34 @@
 #' for the y-axis on the plot
 #' @param hide_yrs A vector of years to not show the year labels for on the
 #' x-axis. This is to prevent label overlap
+#' @param alpha The transparency for the error bars (non-squid). Transparency
+#' for the squid error bars are 1
+#' @param point_size The size of the points (See [ggplot2::geom_point()] for
+#' more details)
+#' @param point_color The R color for the points (See [ggplot2::geom_point()]
+#' for more details)
+#' @param point_shape The R shape number for the points (See
+#' [ggplot2::geom_point()] for more details)
+#' @param point_stroke The stroke value for the points (See
+#' [ggplot2::geom_point()] for more details)
+#' @param line_width Width of the error bar lines
+#' @param line_color R color for the error bar lines
+#' @param line_type Line type for the error bar lines
 #'
 #' @return A [ggplot2::ggplot()] object
 #' @export
 plot_survey_biomass <- function(model,
                                 index = c("age1", "age2"),
                                 y_lim = c(0, 3),
-                                hide_yrs = 2012){
+                                hide_yrs = 2012,
+                                alpha = 0.3,
+                                point_size = ts_single_model_pointsize,
+                                point_color = ts_single_model_pointcolor,
+                                point_shape = ts_single_model_pointshape,
+                                point_stroke = ts_single_model_pointstroke,
+                                line_width = ts_single_model_linewidth,
+                                line_color = ts_single_model_linecolor,
+                                line_type = ts_single_model_linetype){
 
   index <- match.arg(index)
   y_label <- ifelse(index == "age1",
@@ -43,14 +64,19 @@ plot_survey_biomass <- function(model,
                   xend = year,
                   y = lo,
                   yend = hi,)) +
-    geom_segment(col = "grey",
-                 linewidth = 1.5,
+    geom_segment(alpha = alpha,
+                 linewidth = line_width,
+                 color = line_color,
+                 linetype = line_type,
                  lineend = "round")
 
   if(index == 2){
     g <- g +
       geom_segment(data = ests_squid,
-                   linewidth = 1.5,
+                   alpha = 1,
+                   linewidth = line_width,
+                   color = line_color,
+                   linetype = line_type,
                    lineend = "round")
 
   }
@@ -60,7 +86,11 @@ plot_survey_biomass <- function(model,
   x_labels[x_labels %in% hide_yrs] <- ""
 
   g <- g +
-    geom_point(aes(y = obs), size = 2) +
+    geom_point(aes(y = obs),
+               shape = point_shape,
+               size = point_size,
+               color = point_color,
+               stroke = point_stroke) +
     scale_x_continuous(breaks = x_breaks,
                        labels = x_labels) +
     scale_y_continuous(breaks = seq(y_lim[1], y_lim[2], brk_interval),
