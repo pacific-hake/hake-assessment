3fb937ae963de6b02e9c3d80bd43c0a0f2207364
Chris Grandin <chris.grandin@gmail.com>
2023-10-02 01:54:11 -0700

    Post processor for beamer adds pictures and disclaimer to title slide

--- R/post-process-beamer.R
+++ R/post-process-beamer.R
@@ -1,4 +1,17 @@
-
+#' Post-process Beamer Rmarkdown TeX files
+#'
+#' @details
+#' Inserts the logos and picture on the title slide, and makes the disclaimer
+#' text tiny so it fits on the slide
+#' Creates a backup of the `tex_fn` file before modifying it. The backup file
+#' is called 'backup.tex',
+#' Must run `lualatex beamer-hake-data.tex;lualatex beamer-hake-data.tex` in
+#' a command-line terminal once done to re-create the PDF.
+#'
+#' @param tex_fn The TeX file name
+#'
+#' @return Nothing, overwrites the input file `tex_fn`
+#' @export
 post_process_beamer <- function(tex_fn = "beamer-hake-data.tex"){
 
   d <- readLines(tex_fn)
@@ -20,8 +33,9 @@ post_process_beamer <- function(tex_fn = "beamer-hake-data.tex"){
       "  \\begin{center}",
       "  {\\usebeamerfont{title}\\usebeamercolor[fg]{title}\\inserttitle}\\\\",
       "  \\vskip22pt",
+      "  \\includegraphics[height=1in, width=4in]{../../images/hake-on-board.eps}\\\\",
       "  \\insertauthor",
-      "  \\vskip22pt",
+      "  \\vskip5pt",
       "  \\insertdate",
       "  \\end{center}",
       "  \\usebeamerfont{subtitle}\\usebeamercolor[fg]{subtitle}\\insertsubtitle\\par",
@@ -31,17 +45,32 @@ post_process_beamer <- function(tex_fn = "beamer-hake-data.tex"){
 
   d <- c(pre, dat, post)
 
-  # Remove headline from title page (section buttons)
-  ind <- grep("\\\\begin\\{document\\}", d)
-  pre <- d[1:ind]
-  post <- d[(ind + 1):length(d)]
-  dat <- "\\setbeamertemplate{headline}{}"
-  d <- c(pre, dat, post)
+  # Add theme
+  # ind <- grep("\\\\begin\\{document\\}", d)
+  # pre <- d[1:(ind - 1)]
+  # post <- d[ind:length(d)]
+  # dat <- c("", "\\usetheme{Singapore}", "")
+  # d <- c(pre, dat, post)
 
-  ind <- grep("\\\\frame\\{\\\\titlepage\\}", d)
-  pre <- d[1:ind]
+  # Remove headline from title page (section buttons)
+  # ind <- grep("\\\\begin\\{document\\}", d)
+  # pre <- d[1:ind]
+  # post <- d[(ind + 1):length(d)]
+  # dat <- c("", "\\setbeamertemplate{headline}{}", "")
+  # d <- c(pre, dat, post)
+
+  # ind <- grep("\\\\frame\\{\\\\titlepage\\}", d)
+  # pre <- d[1:ind]
+  # post <- d[(ind + 1):length(d)]
+  # dat <- c("", "\\setbeamertemplate{headline}[miniframes theme]")
+  # d <- c(pre, dat, post)
+
+  # Make the disclaimer on the bottom of the title page tiny
+  ind <- grep("\\\\subtitle\\{", d)
+  pre <- d[1:(ind - 1)]
   post <- d[(ind + 1):length(d)]
-  dat <- "\\setbeamertemplate{headline}[Singapore theme]"
+  dat <- d[ind]
+  dat <- gsub("\\\\subtitle\\{", "\\\\subtitle{\\\\tiny ", dat)
   d <- c(pre, dat, post)
 
   writeLines(d, tex_fn)
