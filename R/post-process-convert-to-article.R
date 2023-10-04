d01f47c83a3f187887e1058bdb7974a762ec2898
Chris Grandin <chris.grandin@gmail.com>
2023-10-04 01:33:42 -0700

    Update doc with full support for hake_pdf cuilod type

--- R/post-process-convert-to-article.R
+++ R/post-process-convert-to-article.R
@@ -1,63 +1,35 @@
-#' Replace book references with article and chapter with section, and
-#' add a "sub" to each section, subsection etc to lower each by one level
+#' Replace chapter with section for all sections, and prepend a "sub" to
+#' each section, subsection etc to lower each by one level
 #'
-#' @param x
+#' @param x Tex code, as a vector of lines read in from a TeX file by
+#' [readLines()]
 #'
-#' @return
+#' @return The modified Tex code, as a vector
 #' @export
-#'
-#' @examples
 post_process_convert_to_article <- function(x){
 
-  section_inds <- grep("\\\\section", x)
-  section_curly_inds <- grep("\\{section\\}", x)
-
-  subsection_inds <- grep("\\\\subsection", x)
-  subsection_curly_inds <- grep("\\{subsection\\}", x)
-
-  subsubsection_inds <- grep("\\\\subsubsection", x)
-  subsubsection_curly_inds <- grep("\\{subsubsection\\}", x)
-
-  subsubsubsection_inds <- grep("\\\\subsubsubsection", x)
-  subsubsubsection_curly_inds <- grep("\\{subsubsubsection\\}", x)
+  section_inds <- grep("\\\\section\\{", x)
+  subsection_inds <- grep("\\\\subsection\\{", x)
+  subsubsection_inds <- grep("\\\\subsubsection\\{", x)
+  subsubsubsection_inds <- grep("\\\\subsubsubsection\\{", x)
 
   # Change sections to subsections etc
   if(length(section_inds))
     x[section_inds] <- gsub("section",
                             "subsection",
                             x[section_inds])
-  if(length(section_curly_inds))
-    x[section_curly_inds] <- gsub("section",
-                                  "subsection",
-                                  x[section_curly_inds])
-
   if(length(subsection_inds))
     x[subsection_inds] <- gsub("subsection",
                                "subsubsection",
                                x[subsection_inds])
-  if(length(subsection_curly_inds))
-    x[subsection_curly_inds] <- gsub("subsection",
-                                     "subsubsection",
-                                     x[subsection_curly_inds])
-
   if(length(subsubsection_inds))
     x[subsubsection_inds] <- gsub("subsubsection",
                                   "subsubsubsection",
                                   x[subsubsection_inds])
-  if(length(subsubsection_curly_inds))
-    x[subsubsection_curly_inds] <- gsub("subsubsection",
-                                        "subsubsubsection",
-                                        x[subsubsection_curly_inds])
-
   if(length(subsubsubsection_inds))
     x[subsubsubsection_inds] <- gsub("subsubsubsection",
                                      "subsubsubsubsection",
                                      x[subsubsubsection_inds])
-  if(length(subsubsubsection_curly_inds))
-    x[subsubsubsection_curly_inds] <- gsub("subsubsubsection",
-                                           "subsubsubsubsection",
-                                           x[subsubsubsection_curly_inds])
-
   x <- gsub("\\\\chapter", "\\\\section", x)
   x <- gsub("\\{chapter\\}", "{section}", x)
 
