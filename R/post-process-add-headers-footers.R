8568b035ab2510943ac81e6fbf1b3014b01bdf0b
Chris Grandin <chris.grandin@gmail.com>
2023-10-28 14:28:09 -0700

    Add header/footer rule customization to YAML

--- R/post-process-add-headers-footers.R
+++ R/post-process-add-headers-footers.R
@@ -2,11 +2,31 @@
 #'
 #' @param x Tex code, as a vector of lines read in from a TeX file by
 #' [readLines()]
+#' @param headrule_width_pt Width of the primary headrule line in pts
+#' @param headrule_double_lines Logical. If `TRUE`, make headrule double lines
+#' @param headrule_double_width_pt Width of the secondary (top) headrule line
+#' in pts if `headrule_double_lines` is `TRUE`
+#' @param headrule_double_space_mm Space between the two headrule lines in mm
+#' if `headrule_double_lines` is `TRUE`
+#' @param footrule_width_pt Width of the primary footrule line in pts
+#' @param footrule_double_lines Logical. If `TRUE`, make footrule double lines
+#' @param footrule_double_width_pt Width of the secondary (top) footrule line
+#' in pts if `footrule_double_lines` is `TRUE`
+#' @param footrule_double_space_mm Space between the two footrule lines in mm
+#' if `footrule_double_lines` is `TRUE`
 #' @param ... Absorbs arguments meant for other functions
 #'
 #' @return The modified Tex code, as a vector
 #' @export
 post_process_add_headers_footers <- function(x,
+                                             headrule_width_pt = 0.25,
+                                             headrule_double_lines = FALSE,
+                                             headrule_double_width_pt = 2,
+                                             headrule_double_space_mm = 1,
+                                             footrule_width_pt = 0.25,
+                                             footrule_double_lines = FALSE,
+                                             footrule_double_width_pt = 2,
+                                             footrule_double_space_mm = 1,
                                              ...){
 
   left_ind <- grep("fancyfoot\\[L\\]\\{\\}", x)
@@ -22,6 +42,50 @@ post_process_add_headers_footers <- function(x,
 
   x[left_ind] <- paste0("\\fancyfoot[L]{", footer_left, "}")
   x[right_ind] <- paste0("\\fancyfoot[R]{", footer_right, "}")
+
+  # Header and Footer lines
+  ind <- grep("INSERT headrule and footrule here", x)
+  if(!length(ind)){
+    stop("'INSERT headrule and footrule here' not found in the preamble. ",
+         "It should be just after the fancyhead and fancyfoot definitions",
+         call. = FALSE)
+  }
+  pre <- x[1:ind]
+  post <- x[(ind + 1):length(x)]
+  # dat <- c(paste0("\\renewcommand{\\headrulewidth}{", headrule_width_pt, "}"),
+  #          paste0("\\renewcommand{\\footrulewidth}{", footrule_width_pt, "}"),
+  #          "")
+  dat <- NULL
+
+  if(headrule_double_lines){
+    dat <- c(dat,
+             "\\renewcommand\\headrule{\\begin{minipage}{1\\textwidth}",
+             paste0("\\hrule width \\hsize height ", headrule_double_width_pt,
+                    "pt \\kern ", headrule_double_space_mm,
+                    "mm \\hrule width \\hsize"),
+             "\\end{minipage}\\par}")
+  }else{
+    dat <- c(dat,
+             "\\renewcommand\\headrule{\\begin{minipage}{1\\textwidth}",
+             paste0("\\hrule width \\hsize height ", headrule_width_pt, "pt"),
+             "\\end{minipage}\\par}")
+  }
+
+  if(footrule_double_lines){
+    dat <- c(dat,
+             "\\renewcommand\\footrule{\\begin{minipage}{1\\textwidth}",
+             paste0("\\hrule width \\hsize height ", footrule_double_width_pt,
+                    "pt \\kern ", footrule_double_space_mm,
+                    "mm \\hrule width \\hsize"),
+             "\\end{minipage}\\par}")
+  }else{
+    dat <- c(dat,
+             "\\renewcommand\\footrule{\\begin{minipage}{1\\textwidth}",
+             paste0("\\hrule width \\hsize height ", footrule_width_pt, "pt"),
+             "\\end{minipage}\\par}")
+  }
+
+  x <- c(pre, dat, post)
 
   x
 }
