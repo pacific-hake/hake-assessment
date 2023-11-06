75f956522d6e0986a650c0360cea729548eccabc
Chris Grandin <chris.grandin@gmail.com>
2023-11-06 14:41:09 -0800

    Update load-ss and create key nuisance file functions

--- R/create-kn-files.R
+++ R/create-kn-files.R
@@ -1,34 +1,29 @@
-#' Create the `nuisance` and `key` posteriors files
+#' Create the `nuisance` and `key` posteriors CSV files in the output
+#' mcmc directory of the model
 #'
 #' @param model A model object representing the output from the SS model
-#' @param posterior_regex A vector of key posterior names in regular
-#' expression formats, which will be glued together with logical OR ('|') to
-#' match the names
-#' @param key_post_fn Filename for the key posterior file
-#' @param nuisance_post_fn Filename for the nuisance posterior file
 #'
-#' @return Nothing
+#' @return Nothing, creates two CSV files
 #' @export
-create_kn_files <- function(model,
-                            posterior_regex,
-                            key_post_fn,
-                            nuisance_post_fn){
+create_kn_files <- function(model){
 
   if(model$extra_mcmc_exists){
-    key_file <- file.path(model$extra_mcmc_path, key_post_fn)
-    nuisance_file <- file.path(model$extra_mcmc_path, nuisance_post_fn)
+    key_file <- file.path(model$extra_mcmc_path, key_posteriors_fn)
+    nuisance_file <- file.path(model$extra_mcmc_path, nuisance_posteriors_fn)
   }else{
     key_file <- file.path(model$mcmc_path, key_post_fn)
     nuisance_file <- file.path(model$mcmc_path, nuisance_post_fn)
   }
 
-  mc <- model$mcmc
-  mc_names <- names(mc)
-  inds <- grep(paste(posterior_regex, collapse = "|"), mc_names) |>
-    unique()
-  mcmc_names <- mc_names[inds]
-  keys <- mc[, inds]
-  nuisances <- mc[, -inds]
-  write.csv(keys, key_file, row.names = FALSE)
-  write.csv(nuisances, nuisance_file, row.names = FALSE)
+  mc <- model$mcmc |>
+    as_tibble()
+  nms <- grep(paste(key_posteriors, collapse = "|"), names(mc), value = TRUE)
+
+  keys <- mc |>
+    select_at(.vars = vars(nms))
+  nuisances <- mc |>
+    select_at(.vars = vars(-nms))
+
+  write_csv(keys, key_file)
+  write_csv(nuisances, nuisance_file)
 }
--- R/load-ss-files.R
+++ R/load-ss-files.R
@@ -20,10 +20,6 @@ load_ss_files <- function(model_path = NA,
                                         "Q_extraSD",
                                         "ln.EffN_mult._1",
                                         "ln.EffN_mult._2"),
-                          posts_fn = "posteriors.sso",
-                          derposts_fn = "derived_posteriors.sso",
-                          key_posts_fn = "keyposteriors.csv",
-                          nuisance_posts_fn = "nuisanceposteriors.csv",
                           printstats = FALSE,
                           ...){
 
@@ -38,37 +34,30 @@ load_ss_files <- function(model_path = NA,
               verbose = FALSE,
               printstats = printstats,
               covar = FALSE,
-              wtfile = "wtatage.ss")
+              wtfile = weight_at_age_fn)
   }, error = function(e){
     SS_output(dir = model_path,
               verbose = FALSE,
               printstats = printstats,
               covar = FALSE,
               forecast = FALSE,
-              wtfile = "wtatage.ss")
+              wtfile = weight_at_age_fn)
   })
 
   # Load the data file and control file for the model
   # Get the file whose name contains "_data.ss" and "_control.ss"
   # If there is not exactly one of each, stop with error.
   model_path_listing <- tolower(dir(model_path))
-  dat_fn_ind <- grep("_data.ss", model_path_listing)
-  ctl_fn_ind <- grep("_control.ss", model_path_listing)
-  par_fn_ind <- grep("ss.par", model_path_listing)
+  dat_fn_ind <- grep(data_fn, model_path_listing)
+  ctl_fn_ind <- grep(control_fn, model_path_listing)
+  par_fn_ind <- grep(par_fn, model_path_listing)
   if(!length(dat_fn_ind)){
-    stop("Error in model ", model_path, ", there is no data file. A data ",
-         "file is any file with a name ending in `_data.ss`",
+    stop("The data file `", file.path(model_path, data_fn), "` is missing",
          call. = FALSE)
-  }
-  if(length(dat_fn_ind) > 1){
-    stop("Error in model ", model_path, ", there is more than one data ",
-         " A data file is any file with a name ending in `_data.ss`",
-         call. = FALSE)
-
   }
   if(!length(ctl_fn_ind)){
-    stop("Error in model ", model_path, ", there is no control file. A ",
-         "control file is any file with a name ending in `_control.ss`",
+    stop("The control file `", file.path(model_path, control_fn),
+         "` is missing",
          call. = FALSE)
 
   }
@@ -79,20 +68,19 @@ load_ss_files <- function(model_path = NA,
          call. = FALSE)
   }
   model$path <- model_path
-  model$dat_file <- file.path(model_path, model_path_listing[dat_fn_ind])
-  model$ctl_file <- file.path(model_path, model_path_listing[ctl_fn_ind])
-  model$par_file <- file.path(model_path, model_path_listing[par_fn_ind])
+  model$dat_file <- file.path(model_path, data_fn)
+  model$ctl_file <- file.path(model_path, control_fn)
+  model$par_file <- file.path(model_path, par_fn)
   model$dat <- SS_readdat(model$dat_file, verbose = FALSE)
-  model$ctl <- readLines(model$ctl_file)
-  model$ctl <- gsub("\t", " ", model$ctl)
-  # model$par <- readLines(par_fn)
+  model$ctl <- SS_readctl(model$ctl_file, verbose = FALSE)
+  model$par <- readLines(model$par_file)
 
   # Set default mcmc members to NA. Later code depends on this.
   model$mcmc <- NA
   # Set the mcmc and extra mcmc paths and record their existence
-  model$mcmc_path <- file.path(model_path, "mcmc")
+  model$mcmc_path <- file.path(model_path, mcmc_path)
   model$mcmc_exists <- dir.exists(model$mcmc_path)
-  model$extra_mcmc_path <- file.path(model$mcmc_path, "sso")
+  model$extra_mcmc_path <- file.path(model$mcmc_path, sso_path)
   model$extra_mcmc_exists <- dir.exists(model$extra_mcmc_path)
   # Save the posterior names from the mcmc output. This is necessary for the
   # function `plot_mcmc_param_stats()`
@@ -100,15 +88,18 @@ load_ss_files <- function(model_path = NA,
                            model$extra_mcmc_path,
                            model$mcmc_path)
 
-  # If it has an mcmc sub-directory, load that as well
+  # If it has an mcmc or mcmc/sso sub-directory, load that as well
   if(dir.exists(posteriors_dir)){
-    tmp <- readLines(file.path(posteriors_dir, posts_fn), n = 1)
+    posteriors_fn <- file.path(posteriors_dir, posts_fn)
+    derposteriors_fn <- file.path(posteriors_dir, derposts_fn)
+    # Read in the column headers only
+    tmp <- readLines(posteriors_fn, n = 1)
     tmp <- str_split(tmp, "[:space:]")[[1]]
     # Remove Empty string, `Iter` and `Objective_function` as they are not
     # parameters
     model$post_names <- tmp[!tmp %in% c("", "Iter", "Objective_function")]
-    fix_posteriors(file.path(posteriors_dir, posts_fn))
-    fix_posteriors(file.path(posteriors_dir, derposts_fn))
+    fix_posteriors(posteriors_fn)
+    fix_posteriors(derposteriors_fn)
     model$mcmc <- SSgetMCMC(dir = posteriors_dir,
                             writecsv = FALSE,
                             verbose = FALSE)
@@ -118,10 +109,7 @@ load_ss_files <- function(model_path = NA,
                               names(model$mcmc))
 
     # Create the key and nuisance posteriors csv files
-    create_kn_files(model,
-                    key_posts,
-                    key_posts_fn,
-                    nuisance_posts_fn)
+    create_kn_files(model)
 
     # Do the mcmc calculations, e.g. quantiles for SB, SSB, DEPL, RECR, RECRDEVS
     model$mcmccalcs <- calc_mcmc(model$mcmc,
