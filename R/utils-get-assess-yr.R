8fdce67df316efbb4ccf4bec2e31f9436e2042e0
Chris Grandin <chris.grandin@gmail.com>
2023-11-07 19:01:53 -0800

    Add get_assess_yr()

---
+++ R/utils-get-assess-yr.R
@@ -0,0 +1,24 @@
+#' Get the current assessment year
+#'
+#' @param assess_yr_month_cutoff The month where 1 will be added to
+#' the assessment year. So if this is 12 and the current date is in December,
+#' the assessment year will be the next year, which starts in January
+#'
+#' @return A year
+#' @export
+get_assess_yr <- function(assess_yr_month_cutoff = 12){
+
+  dt <- Sys.Date()
+  yr <- dt |>
+    format("%Y") |>
+    as.numeric()
+  mn <- dt |>
+    format("%m") |>
+    as.numeric()
+
+  if(mn == assess_yr_month_cutoff){
+    yr <- yr + 1
+  }
+
+  yr
+}
