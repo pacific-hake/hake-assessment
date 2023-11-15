ab31a6a76b7c661d2b50bef3f9be514c8e6cc951
Chris Grandin <chris.grandin@gmail.com>
2023-11-15 12:47:01 -0800

    Add check_catch_levels() function to assure the catch levels routine was run so forecasting can go ahead

---
+++ R/utils-check-catch-levels.R
@@ -0,0 +1,80 @@
+#' Ensure that the [run_catch_levels()] function has been run successfully
+#' by looking in to the catch levels directory and checking values in the
+#' forecast files
+#'
+#' @param model The SS model output as loaded by [create_rds_file()]
+#' @param model_path The directory the models are located in
+#' @param forecast_yrs A vector of forecast years
+#' @param ... Absorbs arguments meant for other functions
+#'
+#' @return Logical. `TRUE` if the catch levels appears to have been run
+#' successfully and `FALSE` otherwise
+#' @export
+check_catch_levels <- function(model = NULL,
+                               model_path = NULL,
+                               forecast_yrs = get_assess_yr():(get_assess_yr() + 3),
+                               ...){
+
+  if(is.null(model)){
+    if(is.null(model_path)){
+      stop("Either `model` or `model_path` must be supplied")
+    }
+    model <- load_ss_files(model_path, ...)
+  }else{
+    if(is.null(model_path)){
+      model_path <- model$path
+    }else{
+      if(model$path != model_path){
+        stop("You provided both `model` and `model_path` and `model$path` ",
+             "does not math `model_path`")
+      }
+    }
+  }
+
+  ct_levels_fullpath <- file.path(model_path, ct_levels_path)
+  if(!dir.exists(ct_levels_fullpath)){
+    return(FALSE)
+  }
+
+  lvls <- set_ct_levels()
+  dir_nms <- map_chr(lvls$ct_levels, ~{.x[[3]]})
+  dir_nms <- gsub("[0-9]+-(.*)", "\\1", dir_nms)
+  dir_nms <- dir_nms[grep("[a-zA-Z]+", dir_nms)]
+  dir_nms_fullpath <- file.path(model_path, ct_levels_path, dir_nms)
+  if(!all(dir.exists(dir_nms_fullpath))){
+    return(FALSE)
+  }
+
+  fore_fns_fullpath <- file.path(dir_nms_fullpath, forecast_fn)
+  if(!all(file.exists(fore_fns_fullpath))){
+    return(FALSE)
+  }
+
+  yr_ind_lst <- map(fore_fns_fullpath, \(fn){
+    x <- readLines(fn)
+    map_dbl(forecast_yrs, \(yr){
+      j <- grep(paste0("[[:space:]]", yr, "[[:space:]]*1[[:space:]]*1"), x)
+      if(!length(j)){
+        return(FALSE)
+      }
+      if(length(j) > 1){
+        stop("Found more than one match for year ", yr, " in the file ",
+             fn)
+      }
+      j
+    })
+  })
+
+  length_vector <- lengths(yr_ind_lst)
+  if(var(length_vector)){
+    # If not all the number of years in each forecast file are the same, FALSE
+    # Uses variance as a bit of trickiness
+    return(FALSE)
+  }
+  # Check that the correct number of years appears in the forecast files
+  if(length_vector[1] != length(forecast_yrs)){
+    return(FALSE)
+  }
+
+  TRUE
+}
