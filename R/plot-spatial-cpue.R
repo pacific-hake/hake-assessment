55817bc07c12f945678177bef9fab4556d0bd6af
Chris Grandin <chris.grandin@gmail.com>
2023-11-26 02:24:10 -0800

    Plot spatial CPUE in JTC pres

--- R/plot-spatial-cpue.R
+++ R/plot-spatial-cpue.R
@@ -22,28 +22,47 @@
 #' @param bin_width Bin width as defined in [ggplot2::stat_summary_hex()]
 #' @param n_minimum_vessels The minimum number of vessels that must be present
 #' in a hexagon for the hexagon to be shown
+#' @param coast_line_color The ljne color for the coastline
+#' @param coast_line_thickness The line thickness for the coastline
+#' @param coast_fill The fill color for the land areas
+#' @param bath_alpha The transparency for the bathymetric raster layer
 #' @param bath_contours A vector of contours to show contour lines for.
 #' These can be either positive or negative; they will all be made negative
 #' in the function
+#' @param bath_contour_color The line color for the bathymetric contours
+#' @param bath_contour_thickness  The line thickness for the bathymetric
+#' contours
 #' @param hexagon_border_color The color of the hexagon border lines
 #' @param hexagon_border_thickness The thickness of the hexagon border lines
+#' @param hex_fill_breaks The breaks to show in the color bar that describes
+#' the colors of the hexagons. Must be the same length as `hex_fill_labels`
+#' @param hex_fill_labels The labels to show in the color bar that describes
+#' the colors of the hexagons. Must be the same length as `hex_fill_breaks`
 #'
 #' @return A [ggplot2::ggplot()] object
 #' @export
 plot_spatial_cpue <- function(
     d,
     crs_ll = 4326,
-    crs_utm = 32609, # Zone 9 = 32609
-    #crs_utm = 32610, # For Washington, Oregon, California (zone 10)
-    #crs_utm = 32611, # For Washington, Oregon, California (zone 11)
+    crs_utm = 32609, # For BC (zone 9)
+    #crs_utm = 32610, # For Washington, Oregon, Northern California (zone 10)
+    #crs_utm = 32611, # For Southern California (zone 11)
     bathy_resolution = 1,
     x_lim = c(-131.25, -124.5),
     y_lim = c(48, 52.5),
     bin_width = 7,
     n_minimum_vessels = 3,
-    bath_contours = c(100, 200, 500),
+    coast_line_color = "black",
+    coast_line_thickness = 0.5,
+    coast_fill = "grey",
+    bath_alpha = 0.8,
+    bath_contours = c(100, 200, 500, 1000, 1500),
+    bath_contour_color = "black",
+    bath_contour_thickness = 0.25,
     hexagon_border_color = "black",
-    hexagon_border_thickness = 0.5){
+    hexagon_border_thickness = 0.25,
+    hex_fill_breaks = c(0, 250, 1000, seq(3000, 14000, 3000)),
+    hex_fill_labels = comma(hex_fill_breaks)){
 
   bath_contours <- -abs(bath_contours)
 
@@ -57,15 +76,6 @@ plot_spatial_cpue <- function(
             ymin = y_lim[1],
             ymax = y_lim[2])
 
-  # coast_utm <- coast |>
-  #   # sf::st_as_sf(coords = c("longitude", "latitude"),
-  #   #          crs = crs_ll) |>
-  #   st_transform(crs_utm) |>
-  #   st_crop(xmin = x_lim[1],
-  #           xmax = x_lim[2],
-  #           ymin = y_lim[1],
-  #           ymax = y_lim[2])
-
   # Bathymetry ----
   bath <- getNOAA.bathy(x_lim[1],
                         x_lim[2],
@@ -77,9 +87,6 @@ plot_spatial_cpue <- function(
   # Convert 'bathy' type to regular data frame, with UTMs
   bath_df <- fortify.bathy(bath) |>
     as_tibble()
-    #rename(lon = x, lat = y) |>
-    #add_utm_columns(utm_crs = crs_utm) |>
-    #mutate(across(c(x, y), ~{.x = .x * 1e3}))
 
   # CPUE hexagons ----
   d <- add_utm_columns(d)
@@ -96,7 +103,6 @@ plot_spatial_cpue <- function(
   public_dat <- compute_hexagon_xy(privacy_out$data,
                                    bin_width = bin_width) |>
     mutate(hex_id = as.numeric(hex_id))
-    #filter(X >= lims$X[1] & X <= lims$X[2] & Y >= lims$Y[1] & Y <= lims$Y[2])
 
   x <- public_dat |>
     mutate(across(c(X, Y), ~{.x <- .x * 1e3})) |>
@@ -108,7 +114,7 @@ plot_spatial_cpue <- function(
 
   public_dat <- public_dat |>
     bind_cols(x)
-browser()
+
   polygon <- public_dat |>
     st_as_sf(coords = c("lon", "lat"), crs =crs_ll) |>
     group_by(hex_id) |>
@@ -130,29 +136,47 @@ browser()
     geom_raster(data = bath_df,
                 aes(x = x,
                     y = y,
-                    fill = z)) +
+                    fill = z),
+                alpha = bath_alpha,
+                show.legend = FALSE) +
     geom_contour(data = bath_df,
                  aes(x = x,
                      y = y,
                      z = z),
                  breaks = bath_contours,
-                 color = "grey",
-                 size = 0.5) +
-    geom_sf(linewidth = 0.5,
-            fill = "grey") +
-    scale_fill_continuous(type = "viridis") +
-    theme_bw() +
+                 color = bath_contour_color,
+                 size = bath_contour_thickness) +
+    # Fill in the coastline. Must come after the bathymetry raster
+    geom_sf(linewidth = coast_line_thickness,
+            color = coast_line_color,
+            fill = coast_fill) +
+    # Fill the bathymetry raster
+    scale_fill_continuous(type = "gradient") +
     new_scale_fill() +
     geom_sf(data = polygon,
             aes(fill = cpue),
             color = hexagon_border_color,
             linewidth = hexagon_border_thickness,
             inherit.aes = FALSE) +
-    scale_fill_gradientn(colors = colors) +
+    # Fill in the hexagons
+    #scale_fill_gradientn(colors = colors) +
+    #scale_fill_viridis_c(option = "magma", direction = -1) +
+    scale_fill_viridis_c(trans = "sqrt",
+                         option = "D",
+                         breaks = hex_fill_breaks,
+                         labels = hex_fill_labels) +
     coord_sf(datum = st_crs(crs_ll),
              xlim = x_lim,
              ylim = y_lim,
-             expand = FALSE)
+             expand = FALSE) +
+    guides(fill = guide_colorbar(barwidth = 0.5,
+                                 barheight = 5,
+                                 title = "CPUE",
+                                 title.hjust = 0,
+                                 label.position = "left")) +
+    xlab("Longitude (°)") +
+    ylab("Latitude (°)") +
+    scale_y_continuous(labels = comma)
 
   g
 }
