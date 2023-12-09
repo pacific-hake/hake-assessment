3ec5cffdaf6bd7a2975dc090acccdefa96c97179
Chris Grandin <chris.grandin@gmail.com>
2023-12-09 01:09:21 -0800

    Add digits arg to catch by year csv file creation function

--- R/utils-canada-create-fleet-year-catch-csv-files.R
+++ R/utils-canada-create-fleet-year-catch-csv-files.R
@@ -7,13 +7,15 @@
 #' @param write_file If `TRUE`, write the output to the data file given by
 #' the package data variable `can_catch_by_year_fn`. If `FALSE`, return the
 #' data frame
+#' @params digits The number of decimal places to include in the table output
 #'
 #' @return If `write_file` is `TRUE`, nothing. If `write_file` is `FALSE`,
 #' a data frame containing the catch by year (rows) and fishery type (columns)
 #' @export
 canada_create_fleet_year_catch_csv_files <- function(lst,
                                                      catch_scale = 1000,
-                                                     write_file = TRUE){
+                                                     write_file = TRUE,
+                                                     digits = 2){
 
   if(length(lst) != 3){
     stop("The length of the input list `lst` does not equal 3")
@@ -37,7 +39,7 @@ canada_create_fleet_year_catch_csv_files <- function(lst,
   ct[is.na(ct)] <- 0
   ct <- ct |>
     mutate(total_weight = ft_weight + ss_weight +jv_weight) |>
-    mutate(across(-year, ~{.x / catch_scale}))
+    mutate(across(-year, ~{round(.x / catch_scale, digits)}))
 
   if(!write_file){
     return(ct)
