0e8a114a6ea508ade9041c81294193bd0a3457ef
Chris Grandin <chris.grandin@gmail.com>
2023-12-09 22:02:25 -0800

    Add run_sql() to list of imported functions

--- R/hake.R
+++ R/hake.R
@@ -23,7 +23,7 @@
 #' @importFrom future availableCores plan
 #' @importFrom furrr future_map future_imap furrr_options
 #' @importFrom furrr future_walk future_walk2
-#' @importFrom gfdata get_commercial_samples
+#' @importFrom gfdata get_commercial_samples run_sql
 #' @importFrom gfplot tidy_lengths_raw
 #' @importFrom ggnewscale new_scale_fill
 #' @importFrom glue glue
