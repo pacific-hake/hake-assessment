a5806d647a11f143bf3dfa1bb487d28220ac80c6
Chris Grandin <chris.grandin@gmail.com>
2023-12-20 14:28:34 -0800

    Remove version requirement for r4ss and add other packages to Docker script

--- docker/install_r_packages.R
+++ docker/install_r_packages.R
@@ -11,7 +11,7 @@ github_pac_lst <- c("cgrandin/adnuts",
                     "pbs-assess/gfdata",
                     "pbs-assess/gfplot",
                     "ropensci/rnaturalearthhires",
-                    "r4ss/r4ss@bioscale",
+                    "r4ss/r4ss",
                     "tylermorganwall/rayshader")
 
 walk(github_pac_lst, \(pkg){
@@ -22,13 +22,21 @@ walk(github_pac_lst, \(pkg){
 # tidyverse packages here
 
 pac_lst <- c(
-  "bookdown", "clipr", "coda", "cowplot", "crayon", "data.table",
-  "fs", "future", "furrr", "gfplot", "GGally", "ggh4x",
-  "ggnewscale", "ggrepel", "glue", "grDevices", "grid",
-  "gridExtra", "gtable", "here", "knitr", "marmap",
-  "microbenchmark", "minpack.lm", "pacman", "parallelly",
-  "RColorBrewer", "readr", "rgl", "rnaturalearth",
-  "scales", "sf", "tidyselect", "tools",
-  "utils", "with")
+  "bookdown",
+  "car", "carData", "clipr", "coda", "corrplot", "cowplot",
+  "crayon", "data.table", "data.tree", "date",
+  "fs", "future", "furrr",
+  "gfplot", "GGally", "ggh4x", "ggnewscale", "ggpubr", "ggrepel", "ggsci",
+  "ggsignif", "glue", "grDevices", "grid", "gridExtra", "gridGraphics", "gtable",
+  "here",
+  "knitr",
+  "marmap", "MatrixModels", "microbenchmark", "minpack.lm",
+  "pacman", "parallelly", "pbkrtest", "polynom",
+  "quantreg",
+  "RColorBrewer", "readr", "rgl", "rnaturalearth", "rstatix",
+  "scales", "SparseM", "sf",
+  "tidyselect", "tictoc", "tools",
+  "utils",
+  "with")
 
 install.packages(pac_lst)
