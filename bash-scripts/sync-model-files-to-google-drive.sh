f50cb84651efc1b48cdea3f3c48f3445df7d942f
Chris Grandin <chris.grandin@gmail.com>
2023-12-26 01:29:50 -0800

    Update Google drive sync script

--- bash-scripts/sync-model-files-to-google-drive.sh
+++ bash-scripts/sync-model-files-to-google-drive.sh
@@ -10,13 +10,23 @@
 #
 # `get-assess-year.sh` creates the variable $assess_year containing the
 # current year unless it is currently December, in which case it will be
-# the current year + 1. Enter a year as an argument here to force it to be
-# that year, even if December.
-. ./get-assess-year.sh
+# the current year + 1. Enter a year as an argument to this script to force
+# it to be that year, even if December.
+if [ "$#" -eq 1 ]
+then
+  . ./get-assess-year.sh $1
+else
+  . ./get-assess-year.sh
+fi
+
 printf "Syncing files for model year $assess_year\n\n"
 
 # Remote directory on Google drive to sync
-gd="googledrive:/hake-data/models/$assess_year/"
+# For some reason cannot use this variable in the rclone command below,
+# must use direct string in the command or it will continue to sync to the
+# last known sync drive
+# gd="googledrive:/hake-data/mod/$assess_year/"
+
 # Local directory to sync with Google drive directory above
 ld="/srv/hake/models/$assess_year/"
 
@@ -25,6 +35,7 @@ ld="/srv/hake/models/$assess_year/"
 file_list_fn="file_list_for_google_sync.txt"
 . ./create-file-list-for-google-sync.sh
 
-rclone sync $ld $gd --files-from $file_list_fn
+rclone sync $ld "googledrive:/hake-data/srv/hake/models/$assess_year/" \
+  --files-from $file_list_fn
 
 rm -f $file_list_fn
