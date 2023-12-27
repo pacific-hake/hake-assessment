8b81d2cfba9e6a2b29f4865ba27333e3d7d8fa4b
Chris Grandin <chris.grandin@gmail.com>
2023-12-27 11:07:26 -0800

    Remove chmod from git-pull script for Docker

--- docker/run-git-pull.sh
+++ docker/run-git-pull.sh
@@ -9,12 +9,8 @@ git stash clear
 
 git pull
 
-# `git pull` resets the file permissions so we have to set them back to
-# fully open again after pulling
-chmod -R 777 .
-
 # Return control back to the main Dockerfile script. If this is missing,
 # then the ENTRYPOINT command that called this script will exit the
-# container once this script is finished. Don't removeor the container will
-# never open up for you.
+# container once this script is finished. Don't remove or the container will
+# open and close again instantly when you launch it.
 exec "$@"
