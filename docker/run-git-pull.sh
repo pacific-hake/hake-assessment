921fc14a4ca8329e73bd155fb52b48312cfd8384
Chris Grandin <chris.grandin@gmail.com>
2023-12-27 13:56:58 -0800

    Add to git-pull to make the repo writable to user rstudio * Stop git from reporting permission changes as file modifications

--- docker/run-git-pull.sh
+++ docker/run-git-pull.sh
@@ -6,6 +6,18 @@
 
 git pull
 
+# Tell git to ignore permission chages as differences. If we don't do this,
+# then changing the permissions on the next line will cause git to report
+# every file and directory in the repo as modified and they will have to be
+# added again - we do not want that!
+git config core.fileMode false
+
+# Need to change permission because when the container is launched, the repo
+# is completely read-only to group and user (has 744 chmod code). It is
+# writable to root only, but the Rstudio server is launched as user `rstudio`
+# so it has to be changed.
+chmod -R 777 .
+
 # Return control back to the main Dockerfile script. If this is missing,
 # then the ENTRYPOINT command that called this script will exit the
 # container once this script is finished. Don't remove or the container will
