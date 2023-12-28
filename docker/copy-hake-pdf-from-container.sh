11175701196a0e8ae810adcdb57e8ed6f460a7d8
Chris Grandin <chris.grandin@gmail.com>
2023-12-27 22:17:05 -0800

    Comments in copy hake pdf from container script

--- docker/copy-hake-pdf-from-container.sh
+++ docker/copy-hake-pdf-from-container.sh
@@ -3,14 +3,14 @@
 # Copy the hake.pdf file from within a Docker container out to your
 # local machine.
 #
-# The conatiner ID or cointainer name is passed as an argument to this script
+# The container ID or container name is passed as an argument to this script
 # and the file will be copied to your current directory
-# To find the container name and ID, run `docker ps` on the local machine
+# To find the container name and ID, run 'docker ps' on the local machine
 
 printf "Attempting to copy hake.pdf from the container to the local machine.\n\n"
 if [ $# -eq 0 ]
   then
-    echo "You must supply a container ID or name. Run `docker ps` for these"
+    echo "You must supply a container ID or name. Run 'docker ps' for these"
     exit 1
 else
   docker cp $1:/home/rstudio/github/pacific-hake/hake-assessment/doc/hake.pdf hake.pdf
