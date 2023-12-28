89b035b709f48f91ea9a57384e4c2d21f525f61f
Chris Grandin <chris.grandin@gmail.com>
2023-12-28 00:46:45 -0800

    Place the git config command to ignore permissions above the git pull * In the git-pull.sh script which is run each time the container starts

--- docker/run-git-pull.sh
+++ docker/run-git-pull.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+# Tell git to ignore permission chages as differences. If we don't do this,
+# then pulling files with different permissions and changing the
+# permissions later will cause git to report every file and directory in
+# the repo as modified and they will have to be added again - we do not
+# want that!
+git config core.fileMode false
+
 # Pull a repository. Used for a starting script for Docker container
 # so that you are starting with the most current commit in the repo
 # each time you start the container
 git pull
-
-# Tell git to ignore permission chages as differences. If we don't do this,
-# then changing the permissions on the next line will cause git to report
-# every file and directory in the repo as modified and they will have to be
-# added again - we do not want that!
-git config core.fileMode false
 
 # Need to change permission because when the container is launched, the repo
 # is completely read-only to group and user (has 744 chmod code). It is
