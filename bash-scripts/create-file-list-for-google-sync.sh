40cf7ad94c615c1f68e9450dc5f88c2cfeec57f8
Chris Grandin <chris.grandin@gmail.com>
2024-01-09 02:40:38 -0800

    Update the Google Drive sync scripts

--- bash-scripts/create-file-list-for-google-sync.sh
+++ bash-scripts/create-file-list-for-google-sync.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-[[ -z $assess_year ]] && { printf "\nVariable 'assess_year' has not been \
-set, bailing out.\n"; exit 1; }
-
 [[ -z $file_list_fn ]] && { printf "\nVariable 'file_list_fn' has not been \
 set, bailing out.\n"; exit 1; }
 
 # Creates a list of files to synchronize. The list includes only the file
 # matches below in the $files variable
-gd="googledrive:/hake-data/models/$assess_year"
+gd="googledrive:/hake-data/"
+
 # Make sure this covers all the models you want to be backed up
 # ld = local directory
-ld="/srv/hake/models/$assess_year/"
-remove_path_regex="\\/srv\\/hake\\/models\\/$assess_year\\/"
+ld="/srv/hake/"
+remove_path_regex="\\/srv\\/hake\\/"
 
 files=(*.rds \
        starter.ss \
@@ -28,4 +26,5 @@ for file in ${files[@]}; do
   fns=$(find "$ld" -type f -name "$file" | sort -n)
   echo "$fns" >> $file_list_fn
 done
+
 sed -i "s/$remove_path_regex//g" $file_list_fn
--- bash-scripts/sync-model-files-to-google-drive.sh
+++ bash-scripts/sync-model-files-to-google-drive.sh
@@ -1,43 +1,29 @@
 #!/bin/bash
 
-# Synchronize all model input files and RDS files to Google drive,
-# preserving directory structure for given year, which is passed as an
-# argument. If no argument is supplied, the current year (as calculated by
-# `get-assess-year.sh`) will be synced.
+# Synchronize all model input files and RDS files to the following Google
+#  drive:
+# https://drive.google.com/drive/folders/19xcCDlYYE9Uj94Ns9S9aXZKPtPIz0O0O?tid=0B5lSVg5Eq86uQllqNmdmczFVU2c
+# preserving directory structure
 #
-# rclone config must have been run before this, with a new remote called
-# `googledrive`. To see if it is set up properly, run the following:
+# rclone config must have been run one time before this, with a new remote
+# called `googledrive`. To see if it is set up properly, run the following:
+#
 # `rclone listremotes`
-# Which should return `googledrive:`
 #
-# `get-assess-year.sh` creates the variable $assess_year containing the
-# current year unless it is currently December, in which case it will be
-# the current year + 1. Enter a year as an argument to this script to force
-# it to be that year, even if December.
-if [ "$#" -eq 1 ]
-then
-  . ./get-assess-year.sh $1
-else
-  . ./get-assess-year.sh
-fi
-
-printf "Syncing files for model year $assess_year\n\n"
-
-# Remote directory on Google drive to sync
-# For some reason cannot use this variable in the rclone command below,
-# must use direct string in the command or it will continue to sync to the
-# last known sync drive
-# gd="googledrive:/hake-data/mod/$assess_year/"
-
-# Local directory to sync with Google drive directory above
-ld="/srv/hake/models/$assess_year/"
+# Which should return:
+# `googledrive:`
 
 # `file_list_fn` is needed by create-file-list.sh as well as being used
 # in this script
 file_list_fn="file_list_for_google_sync.txt"
-. ./create-file-list-for-google-sync.sh
 
-rclone sync $ld "googledrive:/hake-data/srv/hake/models/$assess_year/" \
+. ~/github/pacific-hake/hake-assessment/bash-scripts/create-file-list-for-google-sync.sh
+
+# Local directory to sync with Google drive directory above
+ld="/srv/hake"
+
+rclone sync --transfers=20 --progress --delete-excluded \
+  --track-renames $ld "googledrive:/hake-data/srv/hake" \
   --files-from $file_list_fn
 
 rm -f $file_list_fn
