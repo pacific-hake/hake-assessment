8467028bbc444645225c92c312f2bfdeffacc428
aaronmberger-nwfsc <1474026481113302@mil>
2024-01-09 13:35:34 -0800

    update age-1 index caption and legend

--- R/plot-survey-fit-mcmc.R
+++ R/plot-survey-fit-mcmc.R
@@ -394,7 +394,7 @@ plot_survey_fit_mcmc <- function(model,
                                      "survey biomass",
                                      "age-1 index"),
                               " with input (thick)\n",
-                              "and estimated (thin) 95% intervals"),
+                              "and additionally estimated (median MCMC; thin) standard deviations"),
                size = leg_font_size,
                hjust = 0)
 
