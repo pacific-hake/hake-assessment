075321f74570edf5abda04dcb44e592e82b9dc83
Chris Grandin <chris.grandin@gmail.com>
2024-01-12 23:42:53 -0800

    Update sync bash script to make a backup to the /srv2 drive as well * Local /srv2 backup is 100% backup, that is ALL files including SS3 intermediary files, 8000 Report files etc. * This script is run at automatically 5:00AM every day

--- bash-scripts/sync-model-files.sh
+++ bash-scripts/sync-model-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Synchronize all model input files and RDS files to the following Google
-#  drive:
+#  drive and to the /srv2 drive on the server (as a local backup):
 # https://drive.google.com/drive/folders/19xcCDlYYE9Uj94Ns9S9aXZKPtPIz0O0O?tid=0B5lSVg5Eq86uQllqNmdmczFVU2c
 # preserving directory structure
 #
@@ -12,18 +12,32 @@
 #
 # Which should return:
 # `googledrive:`
-
-# `file_list_fn` is needed by create-file-list.sh as well as being used
-# in this script
-file_list_fn="file_list_for_google_sync.txt"
-
+#
+# The backup of /srv/hake to /srv2/hake will be a 100% backup meaning ALL
+# files including intermediary SS3 files will be backed up there
 . ~/github/pacific-hake/hake-assessment/bash-scripts/create-file-list-for-google-sync.sh
 
 # Local directory to sync with Google drive directory above
 ld="/srv/hake"
 
-rclone sync --transfers=20 --progress --delete-excluded \
-  --track-renames $ld "googledrive:/hake-data/srv/hake" \
-  --files-from $file_list_fn
+rclone sync \
+  --transfers=20 \
+  --progress \
+  --delete-excluded \
+  --track-renames \
+  --files-from $file_list_fn \
+  $ld \
+  "googledrive:/hake-data/srv/hake"
 
 rm -f $file_list_fn
+
+# Sync the /srv2 backup, note no --files-from argument so ALL files
+# are backed up:
+rclone sync \
+  --transfers=20 \
+  --progress \
+  --delete-excluded \
+  --track-renames \
+  /srv/hake \
+  /srv2/hake
+
