89738e54eda3af734a6791345e74ecfea5d3a570
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-01-16 14:59:21 -0800

    Add n_posts subsampling option for tv selectivity curves; cuts build time in half, closes #1095.

--- R/plot-selex-posteriors.R
+++ R/plot-selex-posteriors.R
@@ -3,6 +3,9 @@
 #'
 #' @param model A model list, created by [create_rds_file()]
 #' @param type One of 1 or 2 where 1 = Fishery and 2 = Acoustic age 2+ survey
+#' @param n_posts The number of posterior lines to plot. The lines are
+#' randomly drawn from all posteriors. if this is larger than the number
+#' of posteriors, all posterior lines will be shown
 #' @param probs A vector of two quantiles to include for the uncertainty
 #' @param age_range A vector of two for the min and max ages to plot. If `NULL`,
 #' all ages in the data will be plotted
@@ -31,6 +34,7 @@
 plot_selex_posteriors <- function(
     model = NULL,
     type = c("fishery", "survey"),
+    n_posts = NULL,
     age_range = NULL,
     show_xlab = TRUE,
     post_line_width = 0.1,
@@ -64,9 +68,24 @@ plot_selex_posteriors <- function(
                    med = apply(selex, 2, quantile, prob = probs[2]),
                    upper = apply(selex, 2, quantile, prob = probs[3]))
 
+  # Total number of samples available, and the number of posteriors requested
+  # which, if `NULL` becomes the number of samples. Adapted from plot_survey_fit_mcmc().
+  n_samp <- model$extra_mcmc$num_posts
+  n_posts <- n_posts %||% n_samp
+  if(n_samp < n_posts){
+    warning("the number of posteriors available (", n_samp, ") is less than ",
+            "the requested number of samples to use in the plot (", n_posts,
+            "). Using all available samples (", n_samp, ")")
+    n_posts <- n_samp
+  }
+  subsample <- sample(n_samp, n_posts)
+
   selex <- selex |>
     mutate(iter = row_number()) |>
+    # Take the random subsample of posterior numbers
+    filter(iter %in% subsample) |>
     select(iter, everything())
+
   sel <- selex |>
     pivot_longer(-iter, names_to = "age", values_to = "med") |>
     mutate(age = factor(age, ages))
