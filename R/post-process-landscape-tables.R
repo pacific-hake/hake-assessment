f058d74361f23a208e003635c613a77fde980924
Chris Grandin <chris.grandin@gmail.com>
2024-01-23 13:21:09 -0800

    Center super-wide table by increasing margins beyond margins

--- R/post-process-landscape-tables.R
+++ R/post-process-landscape-tables.R
@@ -11,6 +11,7 @@ post_process_landscape_tables <- function(x,
 
   # Add fancy landscape page type to landscape pages to remove the sideways
   # headers and footers, and add a page number at the bottom
+
   lscape_inds <- grep("^\\\\begin\\{landscape\\}", x)
   if(!length(lscape_inds)){
     return(x)
@@ -18,8 +19,28 @@ post_process_landscape_tables <- function(x,
 
   lst <- post_process_extract_chunks(x, lscape_inds, lscape_inds)
   # Replace the begin landscape line with the KOMA options lines
-  lst$between <- map(lst$between, \(lscape_line){
+  lst$between <- map2(lst$between, lscape_inds, \(lscape_line, ind){
     # Extract font information
+    adj_val <- "1in"
+    adj_ind <- grep("ADJUST TABLE", x[(ind - 5):(ind - 1)])
+    if(length(adj_ind)){
+      adj_ind_in_x <- ind - 5 + adj_ind - 1
+      adj_line <- x[adj_ind_in_x]
+      adj_val <- gsub("ADJUST TABLE (-?[0-9]+)",
+                      "\\1",
+                      adj_line)
+      adj_val <- as.numeric(adj_val)
+      if(is.na(adj_val)){
+        stop("ADJUST TABLE macro on line ", adj_ind_in_x, " of the ",
+             "Tex file is not of the correct format.\nLine with error:\n",
+             adj_line, "\n\n",
+             "Format:\n",
+             "ADJUST TABLE (-)[decimal number in inches]\n\n",
+             "Examples:\n",
+             "ADJUST TABLE 2\nADJUST TABLE -0.5\n")
+      }
+      adj_val <- paste0(adj_val, "in")
+    }
     fnt_line <- gsub("^\\\\begin\\{landscape\\}\\\\begingroup(.*)$",
                      "\\1",
                      lscape_line)
@@ -29,7 +50,7 @@ post_process_landscape_tables <- function(x,
       "%",
       "\\KOMAoptions{paper = landscape, DIV = last}",
       paste0("\\newgeometry{",
-             "hmargin = 1in, ",
+             paste0("hmargin = ", adj_val, ", "),
              "top = 0.25in, ",
              "bottom = 1in, ",
              "height = 7in, ",
@@ -66,6 +87,10 @@ post_process_landscape_tables <- function(x,
   })
 
   x <- post_process_interlace_chunks(lst)
+
+  # Remove the ADJUST TABLE text from the doc
+  inds <- grep("ADJUST TABLE", x)
+  x[inds] <- ""
 
   x
 }
