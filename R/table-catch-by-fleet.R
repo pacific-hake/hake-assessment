49c98040a33d0db2de237a02faa4952ddfbafb45
Chris Grandin <chris.grandin@gmail.com>
2024-02-07 01:44:06 -0800

    Finish request 7, catch-at-age tables

---
+++ R/table-catch-by-fleet.R
@@ -0,0 +1,111 @@
+#' Create a table containing the catch-at-age for one of the 3 Canadian
+#' or 3 U.S. fleets
+#'
+#' @param flt The fleet index. 1 = CAN FT, 2 = CAN SS, 3 = CAN JV,
+#' 4 = US MS, 5 = US CP, 6 = US SB
+#' @param font_size The size of the font in the table cells
+#'
+#' @return A [kableExtra::kbl()] table
+#' @export
+table_catch_by_fleet <- function(flt = 1,
+                                 font_size  = 4){
+
+  catch_raw_lst <- list(
+    can_ft_catch_by_month_df,
+    can_ss_catch_by_month_df,
+    can_jv_catch_by_month_df,
+    us_ss_catch_by_month_df,
+    us_cp_catch_by_month_df,
+    us_ms_catch_by_month_df)
+
+  nms <- c("CAN FT",
+           "CAN SS",
+           "CAN JV",
+           "US MS",
+           "US CP",
+           "US SB")
+
+  # Merge age props
+  age_raw_lst <- list(can_ft_age_df,
+                  can_ss_age_df,
+                  can_jv_age_df,
+                  us_ms_age_df,
+                  us_cp_age_df,
+                  us_sb_age_df)
+  age_lst <- map2(age_raw_lst, nms, ~{
+    x <- .x |>
+      select(year, grep("^[0-9]+$", names(.x))) |>
+      mutate(fleet = .y)
+
+    if(length(grep("^CAN", .y))){
+      x <- x |>
+        mutate(country = "Canada")
+    }else{
+      x <- x |>
+        mutate(country = "U.S.")
+    }
+    x |>
+      select(country, fleet, everything())
+  })
+    # map_df(~{.x}) |>
+    # select(country, fleet, everything())
+
+  catch_lst <- map2(catch_raw_lst, nms, ~{
+    x <- .x |>
+      group_by(year) |>
+      summarize(year, catch = sum(catch)) |>
+      ungroup() |>
+      mutate(fleet = .y)
+
+    if(length(grep("^CAN", .y))){
+      x <- x |>
+        mutate(country = "Canada")
+    }else{
+      x <- x |>
+        mutate(country = "U.S.")
+    }
+    x |>
+      distinct() |>
+      select(country, fleet, everything())
+  })
+  # map_df(~{.x}) |>
+  # select(country, fleet, everything())
+
+  # Truncate age_df to catch_df by years
+  age_lst <- map2(age_lst, catch_lst, ~{
+    .x |>
+      filter(year %in% unique(.y$year))
+  })
+  # Truncate catch_df to age_df by years
+  catch_lst <- map2(catch_lst, age_lst, ~{
+    .x |>
+      filter(year %in% unique(.y$year))
+  })
+
+  joined_lst <- map2(catch_lst, age_lst, ~{
+    xx <- .x |>
+      full_join(.y, by = c("year", "fleet", "country"))
+  })
+
+  catch_age_lst <- map(joined_lst, ~{
+    .x |>
+      mutate(across(grep("^[0-9]+$", names(.x)), ~{.x * catch} )) |>
+      mutate(across(c(catch, grep("^[0-9]+$", names(.x))), ~{f(.x)})) |>
+      select(-country, -fleet, -catch) |>
+      rename(Year = year)
+  })
+
+  num_cols <- ncol(catch_age_lst[[flt]])
+
+  k <- kbl(catch_age_lst[[flt]],
+           format = "latex",
+           booktabs = TRUE,
+           align = c("l",
+                     rep("r", num_cols - 1)),
+           linesep = "",
+           escape = FALSE) |>
+    kable_styling(font_size = font_size) |>
+    row_spec(0, bold = TRUE)
+
+  k
+}
