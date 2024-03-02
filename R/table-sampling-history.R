e17b081a67bca38519365e13e8d0cd286ee793e6
Chris Grandin <chris.grandin@gmail.com>
2024-03-02 02:09:49 -0800

    Fix sampling history table * Update all values 2010 and later * Each year, those values will be updated in the csv file automatically * See table 4 in the 13-tables.rmd file (replace_start_yr val) * Resolves #1147

--- R/table-sampling-history.R
+++ R/table-sampling-history.R
@@ -3,6 +3,10 @@
 #' @param d Data frame as found in [sampling_history_df]
 #' @param start_yr Start year in table
 #' @param end_yr End year in table
+#' @param replace_start_yr The first year to replace in the csv file using the
+#' age data frames for each fleet. All data for this year to `end_yr` will be
+#' replaced in the "data-tables/fishery-sampling-history.csv" file. If
+#' `NULL`, the file is not loaded and `sampling_history_df` is used instead.
 #' @param font_size The table data and header font size in points
 #' @param header_font_size The font size for the headers only. If `NULL`,
 #' the headers will have the same font size as the table cell data
@@ -14,14 +18,61 @@
 #'
 #' @return A [knitr::kable()] object
 #' @export
-table_sampling_history <- function(d,
-                                   start_yr,
+table_sampling_history <- function(start_yr,
                                    end_yr,
+                                   replace_start_yr = NULL,
                                    font_size = 10,
                                    header_font_size = 10,
                                    header_vert_spacing = 12,
                                    header_vert_scale = 1.2,
                                    ...){
+
+  if(is.null(replace_start_yr)){
+    d <- sampling_history_df
+  }else{
+    d <- read_csv(here("data-tables/fishery-sampling-history.csv"),
+                  col_types = cols(),
+                  show_col_types = FALSE)
+
+    if(!nrow(d)){
+      stop("Problem reading the sampling CSV file: ",
+           "`data-tables/fishery-sampling-history.csv`. No rows were loaded")
+    }
+    row_template <- d |>
+      slice(1) |>
+      mutate(across(everything(), ~{.x = NA_real_}))
+
+    lst <- list(us_ms_age_df,
+                us_cp_age_df,
+                us_sb_age_df,
+                can_ss_age_df,
+                can_ft_age_df) |>
+      map(~{.x |>
+          filter(year %in% replace_start_yr:end_yr) |>
+          select(year, num_samples) |>
+          complete(year = replace_start_yr:end_yr,
+                   fill = list(num_samples = NA))})
+
+    d <- d |>
+      filter(!Year %in% replace_start_yr:end_yr)
+    rows <- row_template
+    for(row in seq_along(replace_start_yr:end_yr)[-1]){
+      rows <- rows |>
+        bind_rows(row_template)
+    }
+    rows$Year <- replace_start_yr:end_yr
+    rows$`U.S. Mothership (hauls)` <- lst[[1]]$num_samples
+    rows$`U.S. Catcher-processor (hauls)` <- lst[[2]]$num_samples
+    rows$`U.S. Shore-based (trips)` <- lst[[3]]$num_samples
+    rows$`Canada Shoreside (trips)` <- lst[[4]]$num_samples
+    rows$`Canada Freezer trawlers (hauls)` <- lst[[5]]$num_samples
+
+    d <- d |>
+      bind_rows(rows)
+
+    write_csv(d, here("data-tables/fishery-sampling-history.csv"),
+              na = "")
+  }
 
   yrs <- d$Year
   start_yr <- ifelse(start_yr < min(yrs), min(yrs), start_yr)
