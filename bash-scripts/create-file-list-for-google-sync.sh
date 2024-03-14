003f54e56dd22c7baca42eae31310c80effc610d
Chris Grandin <chris.grandin@gmail.com>
2024-03-14 09:56:41 -0700

    Add some output files for the base model to the sync functions

--- bash-scripts/create-file-list-for-google-sync.sh
+++ bash-scripts/create-file-list-for-google-sync.sh
@@ -7,6 +7,13 @@ file_list_fn="file_list_for_google_sync.txt"
 # December.
 . ./get-assess-year.sh
 
+# Version used for the final base model. Usually 01-version but some years
+# may be 02-version
+version="02-version"
+
+base_dir="/srv/hake/models/$assess_year/$version/01-base-models/01-base"
+base_mcmc_dir="$base_dir/mcmc"
+base_sso_dir="$base_mcmc_dir/sso"
 
 # Creates a list of files to synchronize. The list includes only the file
 # matches below in the $files variable
@@ -30,8 +37,13 @@ touch $file_list_fn
 for file in ${files[@]}; do
   fns=$(find "$ld" -type f -name "$file" | sort -n)
   echo "$fns" >> $file_list_fn
-  echo "/srv/hake/models/$assess_year/01-version/01-base-models/01-base/mcmc/hake.Rdata" \
-   >> $file_list_fn
+  echo "$base_mcmc_dir/hake.Rdata" >> $file_list_fn
+  echo "$base_sso_dir/CompReport.sso" >> $file_list_fn
+  echo "$base_sso_dir/derived_posteriors.sso" >> $file_list_fn
+  echo "$base_sso_dir/posteriors.sso" >> $file_list_fn
+  echo "$base_sso_dir/posterior_vectors.sso" >> $file_list_fn
+  echo "$base_sso_dir/Report.sso" >> $file_list_fn
+  echo "$base_sso_dir/Report.sso" >> $file_list_fn
 done
 
 sed -i "s/$remove_path_regex//g" $file_list_fn
