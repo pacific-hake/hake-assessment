12a55e8a88b191b2eca654622338f66f9c15992a
Chris Grandin <chris.grandin@gmail.com>
2024-03-27 20:40:50 -0700

    Change run test model bash script

--- bash-scripts/run-test-model.sh
+++ bash-scripts/run-test-model.sh
@@ -4,9 +4,6 @@
 # To fix the permissions for the hake group:
 # chmod -R u+rwx,g+rwx,o= 01-base-models
 
-# Start timer
-SECONDS=0
-
 # Create the variable $assess_year containing the current year unless it
 # is currently December, in which case it will be the current year + 1
 # Enter a year as an argument here to force it to be that year, even if
@@ -33,25 +30,28 @@ repo_path=`Rscript -e "cat(here::here())"`
 models_path="/srv/hake/models"
 # *Never* change `year_path` manually - See `get-assess-year.sh` call above
 year_path=$assess_year
-version_path="01-version"
+version_path="02-version"
 type_path="05-test-models"
-model_name="03-time-varying-weight-at-age"
+model_name="14-long-base"
 
 ss_exe="ss3_2024"
-num_chains=4
-num_samples=1000
-num_warmup_samples=250
-adapt_delta=0.95
-run_extra_mcmc=TRUE
+num_chains=1
+num_samples=32000
+num_warmup_samples=50
+adapt_delta=0.99
+run_extra_mcmc=FALSE
 
 model_path=$models_path/$year_path/$version_path/$type_path/$model_name
 
 [[ ! -d $model_path ]] && { echo "Error: Directory $model_path does not \
 exist, bailing out." ; exit 1; }
 
+# Start timer
+SECONDS=0
+
 (
   trap 'kill 0' SIGINT; \
-    Rscript -e " \
+  Rscript -e " \
     setwd('$repo_path'); \
     devtools::load_all(); \
     run_adnuts_timed('$model_path', \
