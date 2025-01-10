d013d4ca56a3acb973dbde5772afcc0bbd43f0b0
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2025-01-10 13:17:47 -0800

    fix(create-sens-dirs): Fleet is fleet
    Adds some formatting changes

--- R/create-sens-dirs.R
+++ R/create-sens-dirs.R
@@ -34,14 +34,23 @@
 #'
 create_sens_dirs <- function(dir_version,
                              sens_dir_name = "03-sensitivity-models") {
+  # Inputs and directories ----
   main <- normalizePath(dir_version)
   dir_base <- fs::path(dir_version, "01-base-models", "01-base")
   sens_dir <- fs::path(dir_version, sens_dir_name)
-  if(dir.exists(sens_dir)){
-    fns <- list.files(sens_dir, all.files = TRUE, recursive = TRUE, no.. = TRUE, include.dirs = TRUE)
-    if(length(fns)){
-      stop("\nDirectory `", sens_dir, "` exists and contains files and/or directories. ",
-           "Delete these or the whole directory and try again\n")
+  if (dir.exists(sens_dir)) {
+    fns <- list.files(
+      sens_dir,
+      all.files = TRUE,
+      recursive = TRUE,
+      no.. = TRUE,
+      include.dirs = TRUE
+    )
+    if (length(fns) > 0) {
+      cli::cli_abort(c(
+        "x" = "Directory {sens_dir} exists and is populated.",
+        "i" = "Delete these files or the whole directory and try again."
+      ))
     }
   }
   fs::dir_create(sens_dir)
@@ -50,17 +59,19 @@ create_sens_dirs <- function(dir_version,
   starter <- input[["starter"]]
   dat <- input[["dat"]]
   ctl <- input[["ctl"]]
+  rm(input)
 
+  # Helper functions ----
+  # Set up the directory for a single sensitivity and copy inputs into it and
+  # return the path name of the directory
   setup_sensitivity <- function(prefix_number,
                                 path_version = dir_version,
-                                suffix_string) {
-    main <- fs::path(path_version, sens_dir_name)
-    # Make the sensitivity path name
+                                suffix_string,
+                                the_name = sens_dir_name) {
     path_new <- fs::path(
-      main,
+      fs::path(path_version, the_name),
       sprintf("%02d-%s", prefix_number, suffix_string)
     )
-    # Copy inputs
     r4ss::copy_SS_inputs(
       dir.old = fs::path(dir_version, "01-base-models", "01-base"),
       dir.new = path_new,
@@ -72,117 +83,16 @@ create_sens_dirs <- function(dir_version,
     path_new
   }
 
-  setup_ctl <- function(path_sens, return = c("ctl", "dat")) {
-    return <- match.arg(return)
+  # It is easier to use r4ss::SS_read() and read in the entire model than to use
+  # r4ss::SS_readctl because it relies on the data file. This function reads in
+  # the entire model, which takes a little bit longer when you only need the dat
+  # file but that is okay, and then just returns the portions that you want.
+  # The default is to return the control file.
+  setup_ctl <- function(path_sens, return_section = c("ctl", "dat")) {
+    return_section <- match.arg(return_section)
     input_list <- r4ss::SS_read(dir = path_sens)
-    return(input_list[[return]])
+    return(input_list[[return_section]])
   }
-
-  aa <- setup_sensitivity(prefix_number = 1, suffix_string = "h-prior-mean-low")
-  ctl <- setup_ctl(aa)
-  ctl[["SR_parms"]]["SR_BH_steep", "PRIOR"] <- 0.5
-  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-
-  aa <- setup_sensitivity(prefix_number = 2, suffix_string = "h-fix-high")
-  ctl <- setup_ctl(aa)
-  ctl[["SR_parms"]]["SR_BH_steep", "INIT"] <- 1
-  ctl[["SR_parms"]]["SR_BH_steep", "HI"] <- 1
-  ctl[["SR_parms"]]["SR_BH_steep", "PHASE"] <- -4
-  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-
-  # profile over sigmaR
-  sigmavals <- c(1.0, 1.6)
-  aa <- mapply(setup_sensitivity,
-    suffix_string = paste0("sigma-r-fix-", c("low", "high")),
-    prefix_number = 3:4
-    )
-  for (ii in seq_along(aa)) {
-    ctl <- setup_ctl(aa[ii])
-    ctl[["SR_parms"]]["SR_sigmaR", "INIT"] <- sigmavals[ii]
-    ctl[["SR_parms"]]["SR_sigmaR", "HI"] <- sigmavals[ii] * 1.5
-    r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-  }
-
-  aa <- setup_sensitivity(prefix_number = 5, suffix_string = "m-02-sd")
-  ctl <- setup_ctl(aa)
-  ctl[["MG_parms"]]["NatM_p_1_Fem_GP_1", "PR_SD"] <- 0.2
-  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-  aa <- setup_sensitivity(prefix_number = 6, suffix_string = "m-03-sd")
-  ctl <- setup_ctl(aa)
-  ctl[["MG_parms"]]["NatM_p_1_Fem_GP_1", "PR_SD"] <- 0.3
-  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-  aa <- setup_sensitivity(prefix_number = 7, suffix_string = "m-hamel-prior")
-  ctl <- setup_ctl(aa)
-  ctl[["MG_parms"]]["NatM_p_1_Fem_GP_1", c("PRIOR", "PR_SD")] <- c(-1.53245582, 0.31)
-  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-
-  aa <- setup_sensitivity(prefix_number = 8, suffix_string = "age-1-survey")
-  ctl <- setup_ctl(aa)
-  dat <- setup_ctl(aa, return = "dat")
-  dat[["Nfleets"]] <- 2
-  dat[["fleetinfo"]] <- dat[["fleetinfo"]][1:dat[["Nfleets"]], ]
-  dat[["CPUEinfo"]] <- dat[["CPUEinfo"]][1:dat[["Nfleets"]], ]
-  dat[["Nsurveys"]] <- sum(dat[["fleetinfo"]][, "type"] == 3)
-  dat[["fleetinfo1"]] <- dat[["fleetinfo1"]][, 1:dat[["Nfleets"]]]
-  dat[["fleetinfo2"]] <- dat[["fleetinfo2"]][, 1:dat[["Nfleets"]]]
-  dat[["CPUE"]] <- dat[["CPUE"]] %>%
-    dplyr::filter(abs(index) %in% seq_along(dat[["fleetinfo"]][, 1])) %>%
-    data.frame
-  dat[["len_info"]] <- dat[["len_info"]][1:dat[["Nfleets"]], ]
-  dat[["age_info"]] <- dat[["age_info"]][1:dat[["Nfleets"]], ]
-  r4ss::SS_writedat(dat, dat[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-  ctl[["Nfleets"]] <- dat[["Nfleets"]]
-  ctl[["Q_options"]] <- ctl[["Q_options"]][1:dat[["Nsurveys"]], ]
-  ctl[["Q_parms"]] <- ctl[["Q_parms"]][1:(dat[["Nsurveys"]] * 2), ]
-  ctl[["size_selex_types"]] <- ctl[["size_selex_types"]][1:dat[["Nfleets"]], ]
-  ctl[["age_selex_types"]] <- ctl[["age_selex_types"]][1:dat[["Nfleets"]], ]
-  ctl[["size_selex_types"]] <- ctl[["size_selex_types"]][1:dat[["Nfleets"]], ]
-  ctl[["age_selex_parms"]] <- ctl[["age_selex_parms"]][
-    !grepl("Age1", row.names(ctl[["age_selex_parms"]])),
-  ]
-  ctl[["dirichlet_parms"]] <- ctl[["dirichlet_parms"]][1:dat[["Nfleets"]], ]
-  wtfilename <- file.path(dirname(ctl[["sourcefile"]]), "wtatage.ss")
-  wtatage <- r4ss::SS_readwtatage(wtfilename) %>%
-    dplyr::filter(Fleet != 3)
-  r4ss::SS_writewtatage(wtatage, dirname(wtfilename),
-    overwrite = TRUE,
-    verbose = FALSE
-  )
-  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-
-  aa <- setup_sensitivity(
-    prefix_number = 9,
-    suffix_string = "comp-weight-harmonic-mean"
-  )
-  ctl <- setup_ctl(aa)
-  dat <- setup_ctl(aa, return = "dat")
-  dat[["age_info"]][, "CompError"] <- 0
-  dat[["age_info"]][, "ParmSelect"] <- 0
-  ctl[["dirichlet_parms"]] <- NULL
-  ctl[["Variance_adjustment_list"]] <- data.frame(
-    Factor = c(5, 5),
-    Fleet = seq(1:2),
-    Value = c(0.14, 0.46)
-  )
-  ctl[["DoVar_adjust"]] <- 1
-  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-  r4ss::SS_writedat(dat, dat[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-
-  aa <- setup_sensitivity(prefix_number = 10, suffix_string = "tv-select-phi-extra-low")
-  ctl <- setup_ctl(aa)
-  rows <- grep("dev_se", rownames(ctl[["age_selex_parms_tv"]]))
-  ctl[["age_selex_parms_tv"]][rows, "INIT"] <- 0.21
-  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-  aa <- setup_sensitivity(prefix_number = 11, suffix_string = "tv-select-phi-low")
-  ctl <- setup_ctl(aa)
-  rows <- grep("dev_se", rownames(ctl[["age_selex_parms_tv"]]))
-  ctl[["age_selex_parms_tv"]][rows, "INIT"] <- 0.70
-  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
-  aa <- setup_sensitivity(prefix_number = 12, suffix_string = "tv-select-phi-high")
-  ctl <- setup_ctl(aa)
-  rows <- grep("dev_se", rownames(ctl[["age_selex_parms_tv"]]))
-  ctl[["age_selex_parms_tv"]][rows, "INIT"] <- 2.10
-  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
 
   change_max_age_selectivity <- function(ctl_list, age_max, fishery = 1) {
     max_parameter_int <- ctl_list[["age_selex_parms"]] |>
@@ -261,6 +171,144 @@ create_sens_dirs <- function(dir_version,
       tibble::column_to_rownames()
     return(ctl_list)
   }
+
+  # Set up each sensitivity run by manipulating an object called `aa` and then
+  # use r4ss::SS_write*() to write the manipulated portion back to the
+  # appropriate folder.
+
+  # Steepness ----
+  aa <- setup_sensitivity(prefix_number = 1, suffix_string = "h-prior-mean-low")
+  ctl <- setup_ctl(aa)
+  ctl[["SR_parms"]]["SR_BH_steep", "PRIOR"] <- 0.5
+  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+
+  aa <- setup_sensitivity(prefix_number = 2, suffix_string = "h-fix-high")
+  ctl <- setup_ctl(aa)
+  ctl[["SR_parms"]]["SR_BH_steep", "INIT"] <- 1
+  ctl[["SR_parms"]]["SR_BH_steep", "HI"] <- 1
+  ctl[["SR_parms"]]["SR_BH_steep", "PHASE"] <- -4
+  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+
+  # sigma_r ----
+  sigmavals <- c(1.0, 1.6)
+  aa <- purrr::map2(
+    .x = 3:4,
+    .y = paste0("sigma-r-fix-", c("low", "high")),
+    .f = \(x, y) setup_sensitivity(prefix_number = x, suffix_string = y)
+  )
+  for (ii in seq_along(aa)) {
+    ctl <- setup_ctl(aa[[ii]])
+    ctl[["SR_parms"]]["SR_sigmaR", "LO"] <- sigmavals[ii] * 0.5
+    ctl[["SR_parms"]]["SR_sigmaR", "INIT"] <- sigmavals[ii]
+    ctl[["SR_parms"]]["SR_sigmaR", "HI"] <- sigmavals[ii] * 1.5
+    r4ss::SS_writectl(
+      ctl,
+      ctl[["sourcefile"]],
+      verbose = FALSE,
+      overwrite = TRUE
+    )
+  }
+
+  # Natural mortality ----
+  aa <- setup_sensitivity(prefix_number = 5, suffix_string = "m-02-sd")
+  ctl <- setup_ctl(aa)
+  ctl[["MG_parms"]]["NatM_p_1_Fem_GP_1", "PR_SD"] <- 0.2
+  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+  aa <- setup_sensitivity(prefix_number = 6, suffix_string = "m-03-sd")
+  ctl <- setup_ctl(aa)
+  ctl[["MG_parms"]]["NatM_p_1_Fem_GP_1", "PR_SD"] <- 0.3
+  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+  aa <- setup_sensitivity(prefix_number = 7, suffix_string = "m-hamel-prior")
+  ctl <- setup_ctl(aa)
+  ctl[["MG_parms"]]["NatM_p_1_Fem_GP_1", c("PRIOR", "PR_SD")] <- c(
+    -1.53245582,
+    0.31
+  )
+  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+
+  # Age-one survey ----
+  aa <- setup_sensitivity(prefix_number = 8, suffix_string = "age-1-survey")
+  ctl <- setup_ctl(aa)
+  dat <- setup_ctl(aa, return = "dat")
+  dat[["Nfleets"]] <- 2
+  dat[["fleetinfo"]] <- dat[["fleetinfo"]][1:dat[["Nfleets"]], ]
+  dat[["CPUEinfo"]] <- dat[["CPUEinfo"]][1:dat[["Nfleets"]], ]
+  dat[["Nsurveys"]] <- sum(dat[["fleetinfo"]][, "type"] == 3)
+  dat[["fleetinfo1"]] <- dat[["fleetinfo1"]][, 1:dat[["Nfleets"]]]
+  dat[["fleetinfo2"]] <- dat[["fleetinfo2"]][, 1:dat[["Nfleets"]]]
+  dat[["CPUE"]] <- dat[["CPUE"]] %>%
+    dplyr::filter(abs(index) %in% seq_along(dat[["fleetinfo"]][, 1])) %>%
+    data.frame
+  dat[["len_info"]] <- dat[["len_info"]][1:dat[["Nfleets"]], ]
+  dat[["age_info"]] <- dat[["age_info"]][1:dat[["Nfleets"]], ]
+  r4ss::SS_writedat(dat, dat[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+  ctl[["Nfleets"]] <- dat[["Nfleets"]]
+  ctl[["Q_options"]] <- ctl[["Q_options"]][1:dat[["Nsurveys"]], ]
+  ctl[["Q_parms"]] <- ctl[["Q_parms"]][1:(dat[["Nsurveys"]] * 2), ]
+  ctl[["size_selex_types"]] <- ctl[["size_selex_types"]][1:dat[["Nfleets"]], ]
+  ctl[["age_selex_types"]] <- ctl[["age_selex_types"]][1:dat[["Nfleets"]], ]
+  ctl[["size_selex_types"]] <- ctl[["size_selex_types"]][1:dat[["Nfleets"]], ]
+  ctl[["age_selex_parms"]] <- ctl[["age_selex_parms"]][
+    !grepl("Age1", row.names(ctl[["age_selex_parms"]])),
+  ]
+  ctl[["dirichlet_parms"]] <- ctl[["dirichlet_parms"]][1:dat[["Nfleets"]], ]
+  wtfilename <- file.path(dirname(ctl[["sourcefile"]]), "wtatage.ss")
+  wtatage <- r4ss::SS_readwtatage(wtfilename) |>
+    dplyr::filter(fleet <= 2)
+  r4ss::SS_writewtatage(
+    wtatage,
+    dirname(wtfilename),
+    overwrite = TRUE,
+    verbose = FALSE
+  )
+  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+
+  # Data weighting ----
+  aa <- setup_sensitivity(
+    prefix_number = 9,
+    suffix_string = "comp-weight-harmonic-mean"
+  )
+  ctl <- setup_ctl(aa)
+  dat <- setup_ctl(aa, return = "dat")
+  dat[["age_info"]][, "CompError"] <- 0
+  dat[["age_info"]][, "ParmSelect"] <- 0
+  ctl[["dirichlet_parms"]] <- NULL
+  # TODO: this one might need to change because there are three fleets
+  ctl[["Variance_adjustment_list"]] <- data.frame(
+    Factor = c(5, 5),
+    Fleet = seq(1:2),
+    Value = c(0.14, 0.46)
+  )
+  ctl[["DoVar_adjust"]] <- 1
+  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+  r4ss::SS_writedat(dat, dat[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+
+  # Selectivity ----
+  aa <- setup_sensitivity(
+    prefix_number = 10,
+    suffix_string = "tv-select-phi-extra-low"
+  )
+  ctl <- setup_ctl(aa)
+  rows <- grep("dev_se", rownames(ctl[["age_selex_parms_tv"]]))
+  ctl[["age_selex_parms_tv"]][rows, "INIT"] <- 0.21
+  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+  aa <- setup_sensitivity(
+    prefix_number = 11,
+    suffix_string = "tv-select-phi-low"
+  )
+  ctl <- setup_ctl(aa)
+  rows <- grep("dev_se", rownames(ctl[["age_selex_parms_tv"]]))
+  ctl[["age_selex_parms_tv"]][rows, "INIT"] <- 0.70
+  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+  aa <- setup_sensitivity(
+    prefix_number = 12,
+    suffix_string = "tv-select-phi-high"
+  )
+  ctl <- setup_ctl(aa)
+  rows <- grep("dev_se", rownames(ctl[["age_selex_parms_tv"]]))
+  ctl[["age_selex_parms_tv"]][rows, "INIT"] <- 2.10
+  r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
+
   aa <- setup_sensitivity(prefix_number = 13, suffix_string = "max-sel-age-5")
   ctl <- setup_ctl(aa) |>
     change_max_age_selectivity(age_max = 5)
@@ -275,11 +323,15 @@ create_sens_dirs <- function(dir_version,
   r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
 
   # zero sum rec dev constraint
-  aa <- setup_sensitivity(prefix_number = 16, suffix_string = "zero-sum-constraint")
+  aa <- setup_sensitivity(
+    prefix_number = 16,
+    suffix_string = "zero-sum-constraint"
+  )
   ctl <- setup_ctl(aa)
   ctl[["do_recdev"]] <- 1
   r4ss::SS_writectl(ctl, ctl[["sourcefile"]], verbose = FALSE, overwrite = TRUE)
 
-  message("Created new sensitivity directories in ",
-          sens_dir)
+  cli::cli_alert(c(
+    "i" = "Created new sensitivity directories in {sens_dir}"
+  ))
 }
