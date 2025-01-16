8e91fa9ee343ce792590735099dc06a050a4bd7a
Chris Grandin <chris.grandin@gmail.com>
2025-01-16 09:25:32 -0800

    Fix spelling in comments

--- bash-scripts/get-assess-year.sh
+++ bash-scripts/get-assess-year.sh
@@ -3,7 +3,7 @@
 # Creates the $assess_year variable for insertion into year_path variable
 # for other run and create scripts. If an argument is supplied, `assess_yr`
 # will be set to that, otherwise it will be the current year, unless the
-# current month is Decembver, then it wilol be the current year plus 1
+# current month is December, then it will be the current year plus 1
 
 if [ "$#" -eq 1 ]
 then
