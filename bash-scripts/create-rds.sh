074ecd6cb23c5c1cacb38e8c85563272ee1b0b7d
Chris Grandin <chris.grandin@gmail.com>
2025-01-21 16:25:19 -0800

    Add bash script to create an RDS for a standalone model (eDNA in this case)

---
+++ bash-scripts/create-rds.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Build RDS file for the base model
+# The path structure is as follows
+# /srv/hake/models/2023/01-version/01-base-models/01-base/
+#  ^   ^    ^      ^    ^          ^              ^
+#  |   |    |      |    |          |              |
+#  |   |    |      |    |          |              $models (in this script)
+#  |   |    |      |    |          $type_path (in this script)
+#  |   |    |      |    $version_path (in generic-create-rds.sh)
+#  |   |    |      $year (in generic-create-rds.sh)
+#  |   |    $models_path (in generic-create-rds.sh)
+#  \  /
+#   ||
+#   $project_path (in this script)
+
+models=(17-eDNA-index)
+
+type_path="03-sensitivity-models"
+verbose=TRUE
+overwrite=TRUE
+keep_index_fit_posts=TRUE
+# If `first` is zero or negative, load all the posteriors. If any positive
+# number, load the poateriors 1 through `first` only for speed when testing
+# the function `create_rds_file()` and the functions it calls
+first=0
+
+. ./generic-create-rds.sh
