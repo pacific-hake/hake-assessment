807ae1b2479861865252949ce34941aeeb0f3887
Chris Grandin <chris.grandin@gmail.com>
2026-01-21 14:49:57 -0800

    Use load_pakages from doc folder instead of copying it

---
+++ bash-scripts/run-forecasts-test.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# The path structure is as follows
+# /srv/hake/models/2023/01-version/01-base-models/01-base/
+#  ^   ^    ^      ^    ^          ^              ^
+#  |   |    |      |    |          |              |
+#  |   |    |      |    |          |              $model_name
+#  |   |    |      |    |          $type_path
+#  |   |    |      |    $version_path
+#  |   |    |      $year
+#  |   |    $models_path
+#  \  /
+#   ||
+#   $project_path
+
+# Run base model forecasts only
+
+# Create the variable $assess_year containing the current year unless it
+# is currently December, in which case it will be the current year + 1
+# Enter a year as an argument here to force it to be that year, even if
+# December.
+. ./get-assess-year.sh
+
+# The path structure is as follows
+# /srv/hake/models/2023/01-version/01-base-models/01-base/
+#  ^   ^    ^      ^    ^          ^              ^
+#  |   |    |      |    |          |              |
+#  |   |    |      |    |          |              $model_name
+#  |   |    |      |    |          $type_path
+#  |   |    |      |    $version_path
+#  |   |    |      $year
+#  |   |    $models_path
+#  \  /
+#   ||
+#   $project_path
+
+# The following seems to introduce a leading newline when tried in 2026
+repo_path=`Rscript -e "cat(here::here())"`
+# Remove leading newline
+repo_path=`echo $repo_path | tr -d '\n'`
+
+models_path="/srv/hake/models"
+# *Never* change `year_path` manually - See `get-assess-year.sh` call above
+year_path=$assess_year
+version_path="01-version"
+type_path="05-test-models"
+model_name="04-base-with-high-recruitment"
+
+model_path=$models_path/$year_path/$version_path/$type_path/$model_name
+
+[[ ! -d $model_path ]] && { echo "Error: Directory $model_path \
+does not exist, bailing out." ; exit 1; }
+
+# Run the base models forecasts
+(trap 'kill 0' SIGINT; \
+  echo; \
+  Rscript -e " \
+    setwd('$repo_path'); \
+    devtools::load_all(); \
+    run_forecasts(model_path = '$model_path',
+                  run_spr_100 = FALSE,
+                  run_stable_catch = FALSE,
+                  is_catch_proj_model = TRUE,#
+                  forecast_yrs = 2026:2035,
+                  ss_exe = 'ss3_2024')" #\
+  #> /dev/null 2>&1; \
+  printf "\nBase model forecasts complete\n" \
+)
